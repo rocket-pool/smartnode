b4e591cd027c167eb944e5b31da0baba2ae44c29
Jake Pospischil <moles1@gmail.com>
2018-08-27 10:05:35 +1000

    implementing CLI commands

--- rocketpool/smartnode-cli.go
+++ rocketpool/smartnode-cli.go
@@ -224,15 +224,18 @@ func main() {
                     Name:      "required",
                     Aliases:   []string{"r"},
                     Usage:     "Check resources required based on current network utilisation",
-                    UsageText: "rocketpool resource required [type]" + "\n   " +
-                               "- valid types are 'rpl'",
+                    UsageText: "rocketpool resource required [type, ether amount]" + "\n   " +
+                               "- valid types are 'rpl'" + "\n   " +
+                               "- ether amount must be a decimal number",
                     Action: func(c *cli.Context) error {
 
                         // Arguments
                         var resourceType string
+                        var etherAmount float64
 
                         // Validate arguments
-                        err := validateArgs(c, 1, func(messages *[]string) {
+                        err := validateArgs(c, 2, func(messages *[]string) {
+                            var err error
 
                             // Parse type
                             resourceType = c.Args().Get(0)
@@ -242,13 +245,19 @@ func main() {
                                     *messages = append(*messages, "Invalid type - valid types are 'rpl'")
                             }
 
+                            // Parse ether amount
+                            etherAmount, err = strconv.ParseFloat(c.Args().Get(1), 64)
+                            if err != nil {
+                                *messages = append(*messages, "Invalid ether amount - must be a decimal number")
+                            }
+
                         });
                         if err != nil {
                             return err;
                         }
 
                         // Run command
-                        fmt.Printf("Required %v: 0\n", resourceType)
+                        fmt.Printf("Required %v for %v eth: 0\n", resourceType, etherAmount)
                         return nil
 
                     },
