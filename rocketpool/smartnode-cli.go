000ee4d934d18216f896f47cf2094eca473632cc
Jake Pospischil <moles1@gmail.com>
2018-08-27 10:32:09 +1000

    implementing cli commands

--- rocketpool/smartnode-cli.go
+++ rocketpool/smartnode-cli.go
@@ -284,6 +284,74 @@ func main() {
             },
         },
 
+        // Fee management commands
+        cli.Command{
+            Name:      "fee",
+            Aliases:   []string{"f"},
+            Usage:     "Manage user fees",
+            Subcommands: []cli.Command{
+
+                // Display
+                cli.Command{
+                    Name:      "display",
+                    Aliases:   []string{"d"},
+                    Usage:     "Display the current user fee percentage",
+                    UsageText: "rocketpool fee display",
+                    Action: func(c *cli.Context) error {
+
+                        // Validate arguments
+                        err := validateArgs(c, 0, nil)
+                        if err != nil {
+                            return err;
+                        }
+
+                        // Run command
+                        fmt.Println("User fee:")
+                        return nil
+
+                    },
+                },
+
+                // Vote
+                cli.Command{
+                    Name:      "vote",
+                    Aliases:   []string{"v"},
+                    Usage:     "Vote on the user fee percentage to be charged",
+                    UsageText: "rocketpool fee vote [fee percentage]" + "\n   " +
+                               "- fee percentage must be a decimal number between 0 and 100",
+                    Action: func(c *cli.Context) error {
+
+                        // Arguments
+                        var feePercent float64
+
+                        // Validate arguments
+                        err := validateArgs(c, 1, func(messages *[]string) {
+                            var err error
+
+                            // Parse fee percentage
+                            feePercent, err = strconv.ParseFloat(c.Args().Get(0), 64)
+                            if err != nil {
+                                *messages = append(*messages, "Invalid fee percentage - must be a decimal number")
+                            }
+                            if feePercent < 0 || feePercent > 100 {
+                                *messages = append(*messages, "Invalid fee percentage - must be between 0 and 100")
+                            }
+
+                        });
+                        if err != nil {
+                            return err;
+                        }
+
+                        // Run command
+                        fmt.Println("Voting:", feePercent)
+                        return nil
+
+                    },
+                },
+
+            },
+        },
+
     }
 
     // Run application
