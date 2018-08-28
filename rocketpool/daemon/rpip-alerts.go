0477bffbbb994c9880e32c9ece5fdbd74acb7152
Jake Pospischil <moles1@gmail.com>
2018-08-28 15:38:01 +1000

    cleanup

--- rocketpool/daemon/rpip-alerts.go
+++ rocketpool/daemon/rpip-alerts.go
@@ -2,7 +2,6 @@ package daemon
 
 import (
     "fmt"
-    "log"
     "time"
 )
 
@@ -49,41 +48,6 @@ func sendRPIPAlert(proposalId uint64) {
     // TODO: implement
     // - send alert email
     // - mark rpip as notified
-
-}
-
-
-// Check RPIP votes periodically
-func startCheckRPIPVotes(interval string) {
-
-    // Parse check interval
-    duration, err := time.ParseDuration(interval)
-    if err != nil {
-        log.Fatal("Couldn't parse check RPIP votes interval: ", err)
-    }
-
-    // Check RPIP votes on interval
-    ticker := time.NewTicker(duration)
-    defer ticker.Stop()
-    for _ = range ticker.C {
-        go checkRPIPVotes()
-    }
-
-}
-
-
-// Check RPIP votes
-func checkRPIPVotes() {
-
-    // TODO: implement -
-    // - get "ready to reveal" proposals
-    // - for each "ready to reveal" proposal:
-    //     - check for stored vote; cancel if not set
-    //     - reveal vote
-    //     - delete stored vote
-
-    // Log
-    fmt.Println("Checking RPIP votes...")
 
 }
 
