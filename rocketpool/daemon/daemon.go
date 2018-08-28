74f6daad044246da87f9b9d200fe5f6143eea0bd
Jake Pospischil <moles1@gmail.com>
2018-08-28 15:08:14 +1000

    implementing daemon logic

--- rocketpool/daemon/daemon.go
+++ rocketpool/daemon/daemon.go
@@ -10,6 +10,10 @@ const checkRPIPVoteInterval string = "10s"
 // Run daemon
 func Run() {
 
+    // Check for node exit on minipool removed
+    go startCheckNodeExit()
+    go checkNodeExit()
+
     // Check RPIP votes periodically
     go startCheckRPIPVotes(checkRPIPVoteInterval)
     go checkRPIPVotes()
---
+++ rocketpool/daemon/node.go
@@ -0,0 +1,36 @@
+package daemon
+
+import (
+    "fmt"
+    "time"
+)
+
+
+// Check for node exit on minipool removed
+func startCheckNodeExit() {
+
+    // Check for node exit on interval
+    // TODO: implement event listener instead
+    ticker := time.NewTicker(10 * time.Second)
+    defer ticker.Stop()
+    for _ = range ticker.C {
+        go checkNodeExit()
+    }
+
+}
+
+
+// Check for node exit
+func checkNodeExit() {
+
+    // TODO: implement -
+    // - check for exit flag; cancel if not set
+    //     - get minipool count; cancel if > 0
+    //     - exit
+    //     - delete exit flag
+
+    // Log
+    fmt.Println("Checking for node exit...")
+
+}
+
