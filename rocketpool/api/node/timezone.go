752902668e170ca4bdc03c56dd6b709505b65b9b
Jake Pospischil <moles1@gmail.com>
2019-02-05 11:15:23 +1000

    refactored timezone prompt logic

---
+++ rocketpool/api/node/timezone.go
@@ -0,0 +1,48 @@
+package node
+
+import (
+    "fmt"
+    "os/exec"
+    "regexp"
+    "strings"
+
+    "github.com/rocket-pool/smartnode-cli/rocketpool/utils/cli"
+)
+
+
+// Prompt user for a time zone string
+func promptTimezone() string {
+
+    // Time zone value
+    var timezone string
+
+    // Get system time zone
+    timeOutput, _ := exec.Command("timedatectl").Output()
+    if len(timeOutput) > 0 {
+        tzMatches := regexp.MustCompile("(?i)zone:\\s*(\\w{2,}\\/\\w{2,})").FindStringSubmatch(string(timeOutput[:]))
+        if len(tzMatches) > 1 {
+            timezone = tzMatches[1]
+        }
+    }
+
+    // Confirm system time zone
+    if timezone != "" {
+        response := cli.Prompt(fmt.Sprintf("Your system timezone is '%s', would you like to register using this timezone? [y/n]", timezone), "(?i)^(y|yes|n|no)$", "Please answer 'y' or 'n'")
+        if strings.ToLower(response[:1]) == "n" {
+            timezone = ""
+        }
+    }
+
+    // Prompt for time zone
+    for timezone == "" {
+        timezone = cli.Prompt("Please enter a timezone to register with in the format 'Region/City':", "^\\w{2,}\\/\\w{2,}$", "Please enter a timezone in the format 'Region/City'")
+        response := cli.Prompt(fmt.Sprintf("You have chosen to register with the timezone '%s', is this correct? [y/n]", timezone), "(?i)^(y|yes|n|no)$", "Please answer 'y' or 'n'")
+        if strings.ToLower(response[:1]) == "n" {
+            timezone = ""
+        }
+    }
+
+    // Return
+    return timezone
+
+}
