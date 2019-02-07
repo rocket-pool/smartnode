190a6e728feff4c27967866c0d3d415047dd3dc1
Jake Pospischil <moles1@gmail.com>
2019-02-07 11:00:48 +1000

    minor keccak256 helper update

--- rocketpool/utils/eth/eth.go
+++ rocketpool/utils/eth/eth.go
@@ -5,12 +5,12 @@ import (
 )
 
 
-// Make a keccak256 hash of a source string and return as a 32-byte array
-func KeccakStr(src string) [32]byte {
+// Make a keccak256 hash of a source byte slice and return as a 32-byte array
+func KeccakBytes(src []byte) [32]byte {
 
     // Hash source data
     hash := sha3.NewKeccak256()
-    hash.Write([]byte(src)[:])
+    hash.Write(src[:])
 
     // Copy hashed data to byte array
     var bytes [32]byte
@@ -19,5 +19,11 @@ func KeccakStr(src string) [32]byte {
     // Return
     return bytes
 
+}
+
+
+// Make a keccak256 hash of a source string and return as a 32-byte array
+func KeccakStr(src string) [32]byte {
+    return KeccakBytes([]byte(src))
 }
 
