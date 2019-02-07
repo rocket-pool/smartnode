61e41d03578cb0c15cf33735817a107e49f083b4
Jake Pospischil <moles1@gmail.com>
2019-02-07 11:05:48 +1000

    implementing depositInput serialisation

--- rocketpool/api/deposits/reservations.go
+++ rocketpool/api/deposits/reservations.go
@@ -2,16 +2,27 @@ package deposits
 
 import (
     "bytes"
+    "encoding/hex"
     "errors"
     "fmt"
 
     "github.com/ethereum/go-ethereum/common"
     "github.com/ethereum/go-ethereum/ethclient"
+    "github.com/prysmaticlabs/prysm/shared/ssz"
     "github.com/urfave/cli"
 
     "github.com/rocket-pool/smartnode-cli/rocketpool/services/accounts"
     "github.com/rocket-pool/smartnode-cli/rocketpool/services/rocketpool"
+    "github.com/rocket-pool/smartnode-cli/rocketpool/utils/eth"
 )
+
+
+// DepositInput data
+type DepositInput struct {
+    pubkey [48]byte
+    withdrawalCredentials [32]byte
+    proofOfPossession [96]byte
+}
 
 
 // Reserve a node deposit
@@ -87,6 +98,44 @@ func reserveDeposit(c *cli.Context, durationId string) error {
         fmt.Println("Node has an existing deposit reservation, please cancel or finalize it")
         return nil
     }
+
+    // Get node's validator pubkey
+    // :TODO: implement once BLS library is available
+    pubkeyHex := []byte("0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcd01")
+    pubkey := make([]byte, hex.DecodedLen(len(pubkeyHex)))
+    _,_ = hex.Decode(pubkey, pubkeyHex)
+
+    // Get RP withdrawal pubkey
+    // :TODO: replace with correct withdrawal pubkey once available
+    withdrawalPubkeyHex := []byte("0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef")
+    withdrawalPubkey := make([]byte, hex.DecodedLen(len(withdrawalPubkeyHex)))
+    _,_ = hex.Decode(withdrawalPubkey, withdrawalPubkeyHex)
+
+    // Build withdrawal credentials
+    withdrawalCredentials := eth.KeccakBytes(withdrawalPubkey) // Withdrawal pubkey hash
+    withdrawalCredentials[0] = 0 // Replace first byte with BLS_WITHDRAWAL_PREFIX_BYTE
+
+    // Build proof of possession
+    // :TODO: implement once BLS library is available
+    proofOfPossessionHex := []byte(
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef" +
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef" +
+        "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef")
+    proofOfPossession := make([]byte, hex.DecodedLen(len(proofOfPossessionHex)))
+    _,_ = hex.Decode(proofOfPossession, proofOfPossessionHex)
+
+    // Build DepositInput
+    depositInputData := &DepositInput{}
+    copy(depositInputData.pubkey[:], pubkey)
+    copy(depositInputData.withdrawalCredentials[:], withdrawalCredentials[:])
+    copy(depositInputData.proofOfPossession[:], proofOfPossession)
+    depositInput := new(bytes.Buffer)
+    err = ssz.Encode(depositInput, depositInputData)
+    if err != nil {
+        return errors.New("Error encoding DepositInput for deposit reservation: " + err.Error())
+    }
+
+    // Create deposit reservation
 
     // Log & return
     fmt.Println("")
