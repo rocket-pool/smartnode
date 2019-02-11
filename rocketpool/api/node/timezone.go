9d8a450709aab2bdb501c40560cd59e76d04a72e
Jake Pospischil <moles1@gmail.com>
2019-02-12 09:43:16 +1000

    refactoring

--- rocketpool/api/node/timezone.go
+++ rocketpool/api/node/timezone.go
@@ -4,9 +4,6 @@ import (
     "bytes"
     "errors"
     "fmt"
-    "os/exec"
-    "regexp"
-    "strings"
 
     "github.com/ethereum/go-ethereum/common"
     "github.com/ethereum/go-ethereum/ethclient"
@@ -14,7 +11,6 @@ import (
 
     "github.com/rocket-pool/smartnode-cli/rocketpool/services/accounts"
     "github.com/rocket-pool/smartnode-cli/rocketpool/services/rocketpool"
-    cliutils "github.com/rocket-pool/smartnode-cli/rocketpool/utils/cli"
 )
 
 
@@ -85,44 +81,6 @@ func setNodeTimezone(c *cli.Context) error {
     // Log & return
     fmt.Println("Node timezone successfully updated to:", *nodeTimezone)
     return nil
-
-}
-
-
-// Prompt user for a time zone string
-func promptTimezone() string {
-
-    // Time zone value
-    var timezone string
-
-    // Get system time zone
-    timeOutput, _ := exec.Command("timedatectl").Output()
-    if len(timeOutput) > 0 {
-        tzMatches := regexp.MustCompile("(?i)zone:\\s*(\\w{2,}\\/\\w{2,})").FindStringSubmatch(string(timeOutput[:]))
-        if len(tzMatches) > 1 {
-            timezone = tzMatches[1]
-        }
-    }
-
-    // Confirm system time zone
-    if timezone != "" {
-        response := cliutils.Prompt(fmt.Sprintf("Your system timezone is '%s', would you like to register using this timezone? [y/n]", timezone), "(?i)^(y|yes|n|no)$", "Please answer 'y' or 'n'")
-        if strings.ToLower(response[:1]) == "n" {
-            timezone = ""
-        }
-    }
-
-    // Prompt for time zone
-    for timezone == "" {
-        timezone = cliutils.Prompt("Please enter a timezone to register with in the format 'Country/City':", "^\\w{2,}\\/\\w{2,}$", "Please enter a timezone in the format 'Country/City'")
-        response := cliutils.Prompt(fmt.Sprintf("You have chosen to register with the timezone '%s', is this correct? [y/n]", timezone), "(?i)^(y|yes|n|no)$", "Please answer 'y' or 'n'")
-        if strings.ToLower(response[:1]) == "n" {
-            timezone = ""
-        }
-    }
-
-    // Return
-    return timezone
 
 }
 
