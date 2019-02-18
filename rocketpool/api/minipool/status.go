714965beceaa16ccd4d894a6f5ccf3ba949b09ff
Jake Pospischil <moles1@gmail.com>
2019-02-18 14:47:17 +1000

    implementing minipool commands

---
+++ rocketpool/api/minipool/status.go
@@ -0,0 +1,20 @@
+package minipool
+
+import (
+    "fmt"
+
+    "github.com/urfave/cli"
+)
+
+
+// Get the node's minipool statuses
+func getMinipoolStatus(c *cli.Context) error {
+
+    
+
+    // Log status & return
+    fmt.Println("")
+    return nil
+
+}
+
