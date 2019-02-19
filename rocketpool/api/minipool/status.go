dcf343a9d31fb91ebf69e979b8b2110d0e52513a
Jake Pospischil <moles1@gmail.com>
2019-02-19 11:25:30 +1000

    implementing minipool commands

--- rocketpool/api/minipool/status.go
+++ rocketpool/api/minipool/status.go
@@ -9,6 +9,7 @@ import (
     "github.com/ethereum/go-ethereum/common"
     "github.com/urfave/cli"
 
+    "github.com/rocket-pool/smartnode-cli/rocketpool/services/rocketpool/minipool"
     "github.com/rocket-pool/smartnode-cli/rocketpool/utils/eth"
 )
 
@@ -17,7 +18,7 @@ import (
 func getMinipoolStatus(c *cli.Context) error {
 
     // Command setup
-    if message, err := setup(c, []string{"utilAddressSetStorage"}, []string{}); message != "" {
+    if message, err := setup(c, []string{"rocketPoolToken", "utilAddressSetStorage"}, []string{"rocketMinipool"}); message != "" {
         fmt.Println(message)
         return nil
     } else if err != nil {
@@ -34,26 +35,53 @@ func getMinipoolStatus(c *cli.Context) error {
     }
     minipoolCount := (*minipoolCountV).Int64()
 
-    // Get minipool addresses
-    addressChannel := make(chan common.Address)
+    // Minipool data channels
+    addressChannels := make([]chan *common.Address, minipoolCount)
+    detailsChannels := make([]chan *minipool.Details, minipoolCount)
     errorChannel := make(chan error)
+
+    // Get minipool addresses
     for mi := int64(0); mi < minipoolCount; mi++ {
+        addressChannels[mi] = make(chan *common.Address)
         go (func(mi int64) {
             minipoolAddress := new(common.Address)
             if err := cm.Contracts["utilAddressSetStorage"].Call(nil, minipoolAddress, "getItem", minipoolListKey, big.NewInt(mi)); err != nil {
                 errorChannel <- errors.New("Error retrieving node minipool address: " + err.Error())
             } else {
-                addressChannel <- *minipoolAddress
+                addressChannels[mi] <- minipoolAddress
             }
         })(mi)
     }
 
     // Receive minipool addresses
-    addresses := make([]common.Address, 0)
+    minipoolAddresses := make([]*common.Address, minipoolCount)
     for mi := int64(0); mi < minipoolCount; mi++ {
         select {
-            case address := <-addressChannel:
-                addresses = append(addresses, address)
+            case address := <-addressChannels[mi]:
+                minipoolAddresses[mi] = address
+            case err := <-errorChannel:
+                return err
+        }
+    }
+
+    // Get minipool details
+    for mi := int64(0); mi < minipoolCount; mi++ {
+        detailsChannels[mi] = make(chan *minipool.Details)
+        go (func(mi int64) {
+            if details, err := minipool.GetDetails(cm, minipoolAddresses[mi]); err != nil {
+                errorChannel <- err
+            } else {
+                detailsChannels[mi] <- details
+            }
+        })(mi)
+    }
+
+    // Receive minipool details
+    minipoolDetails := make([]*minipool.Details, minipoolCount)
+    for mi := int64(0); mi < minipoolCount; mi++ {
+        select {
+            case details := <-detailsChannels[mi]:
+                minipoolDetails[mi] = details
             case err := <-errorChannel:
                 return err
         }
---
+++ rocketpool/services/rocketpool/minipool/info.go
@@ -0,0 +1,162 @@
+package minipool
+
+import (
+    "errors"
+    "math/big"
+    "time"
+
+    "github.com/ethereum/go-ethereum/common"
+
+    "github.com/rocket-pool/smartnode-cli/rocketpool/services/rocketpool"
+)
+
+
+// Minipool detail data
+type Details struct {
+    Address *common.Address
+    Status uint8
+    StatusTime time.Time
+    StakingDurationId string
+    NodeEtherBalanceWei *big.Int
+    NodeRplBalanceWei *big.Int
+    UserCount *big.Int
+    UserDepositCapacityWei *big.Int
+    UserDepositTotalWei *big.Int
+}
+
+
+// Get a minipool's details
+// Requires rocketMinipool and rocketPoolToken contracts to be loaded with contract manager
+func GetDetails(cm *rocketpool.ContractManager, minipoolAddress *common.Address) (*Details, error) {
+
+    // Minipool details
+    details := &Details{
+        Address: minipoolAddress,
+    }
+
+    // Initialise minipool contract
+    minipoolContract, err := cm.NewContract(minipoolAddress, "rocketMinipool")
+    if err != nil {
+        return nil, errors.New("Error initialising minipool contract: " + err.Error())
+    }
+
+    // Data channels
+    statusChannel := make(chan uint8)
+    statusTimeChannel := make(chan time.Time)
+    stakingDurationIdChannel := make(chan string)
+    nodeEtherBalanceChannel := make(chan *big.Int)
+    nodeRplBalanceChannel := make(chan *big.Int)
+    userCountChannel := make(chan *big.Int)
+    userDepositCapacityChannel := make(chan *big.Int)
+    userDepositTotalChannel := make(chan *big.Int)
+    errorChannel := make(chan error)
+
+    // Get status
+    go (func() {
+        status := new(uint8)
+        if err := minipoolContract.Call(nil, status, "getStatus"); err != nil {
+            errorChannel <- errors.New("Error retrieving minipool status: " + err.Error())
+        } else {
+            statusChannel <- *status
+        }
+    })()
+
+    // Get status time
+    go (func() {
+        statusChangedTime := new(*big.Int)
+        if err := minipoolContract.Call(nil, statusChangedTime, "getStatusChangedTime"); err != nil {
+            errorChannel <- errors.New("Error retrieving minipool status changed time: " + err.Error())
+        } else {
+            statusTimeChannel <- time.Unix((*statusChangedTime).Int64(), 0)
+        }
+    })()
+
+    // Get staking duration ID
+    go (func() {
+        stakingDurationId := new(string)
+        if err := minipoolContract.Call(nil, stakingDurationId, "getStakingDurationID"); err != nil {
+            errorChannel <- errors.New("Error retrieving minipool staking duration ID: " + err.Error())
+        } else {
+            stakingDurationIdChannel <- *stakingDurationId
+        }
+    })()
+
+    // Get node ETH balance
+    go (func() {
+        nodeEtherBalanceWei := new(*big.Int)
+        if err := minipoolContract.Call(nil, nodeEtherBalanceWei, "getNodeBalance"); err != nil {
+            errorChannel <- errors.New("Error retrieving minipool node ETH balance: " + err.Error())
+        } else {
+            nodeEtherBalanceChannel <- *nodeEtherBalanceWei
+        }
+    })()
+
+    // Get node RPL balance
+    go (func() {
+        nodeRplBalanceWei := new(*big.Int)
+        if err := cm.Contracts["rocketPoolToken"].Call(nil, nodeRplBalanceWei, "balanceOf", minipoolAddress); err != nil {
+            errorChannel <- errors.New("Error retrieving minipool node RPL balance: " + err.Error())
+        } else {
+            nodeRplBalanceChannel <- *nodeRplBalanceWei
+        }
+    })()
+
+    // Get user count
+    go (func() {
+        userCount := new(*big.Int)
+        if err := minipoolContract.Call(nil, userCount, "getUserCount"); err != nil {
+            errorChannel <- errors.New("Error retrieving minipool user count: " + err.Error())
+        } else {
+            userCountChannel <- *userCount
+        }
+    })()
+
+    // Get user deposit capacity
+    go (func() {
+        userDepositCapacityWei := new(*big.Int)
+        if err := minipoolContract.Call(nil, userDepositCapacityWei, "getUserDepositCapacity"); err != nil {
+            errorChannel <- errors.New("Error retrieving minipool user deposit capacity: " + err.Error())
+        } else {
+            userDepositCapacityChannel <- *userDepositCapacityWei
+        }
+    })()
+
+    // Get user deposit total
+    go (func() {
+        userDepositTotalWei := new(*big.Int)
+        if err := minipoolContract.Call(nil, userDepositTotalWei, "getUserDepositTotal"); err != nil {
+            errorChannel <- errors.New("Error retrieving minipool user deposit total: " + err.Error())
+        } else {
+            userDepositTotalChannel <- *userDepositTotalWei
+        }
+    })()
+
+    // Receive minipool data
+    for received := 0; received < 8; {
+        select {
+            case details.Status = <-statusChannel:
+                received++
+            case details.StatusTime = <-statusTimeChannel:
+                received++
+            case details.StakingDurationId = <-stakingDurationIdChannel:
+                received++
+            case details.NodeEtherBalanceWei = <-nodeEtherBalanceChannel:
+                received++
+            case details.NodeRplBalanceWei = <-nodeRplBalanceChannel:
+                received++
+            case details.UserCount = <-userCountChannel:
+                received++
+            case details.UserDepositCapacityWei = <-userDepositCapacityChannel:
+                received++
+            case details.UserDepositTotalWei = <-userDepositTotalChannel:
+                received++
+            case err := <-errorChannel:
+                return nil, err
+        }
+    }
+
+    // Return
+    return details, nil
+
+}
+
