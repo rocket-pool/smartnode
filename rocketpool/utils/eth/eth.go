f878dbb61c3905d0d4403224baa8e75d138afeb5
Jake Pospischil <moles1@gmail.com>
2019-03-07 15:48:36 +1000

    implemented wei to gwei conversion helpers

--- rocketpool/utils/eth/eth.go
+++ rocketpool/utils/eth/eth.go
@@ -7,8 +7,9 @@ import (
 )
 
 
-// Conversion factor from wei to eth
-const WEI_PER_ETH float64 = 1000000000000000000
+// Conversion factors
+const WEI_PER_ETH float64 =  1000000000000000000
+const WEI_PER_GWEI float64 = 1000000000
 
 
 // Convert wei to eth
@@ -27,6 +28,27 @@ func EthToWei(eth float64) *big.Int {
     var weiFloat big.Float
     var wei big.Int
     weiFloat.Mul(big.NewFloat(eth), big.NewFloat(WEI_PER_ETH))
+    weiFloat.Int(&wei)
+    return &wei
+}
+
+
+// Convert wei to gigawei
+func WeiToGwei(wei *big.Int) float64 {
+    var weiFloat big.Float
+    var gwei big.Float
+    weiFloat.SetInt(wei)
+    gwei.Quo(&weiFloat, big.NewFloat(WEI_PER_GWEI))
+    gwei64, _ := gwei.Float64()
+    return gwei64
+}
+
+
+// Convert gigawei to wei
+func GweiToWei(gwei float64) *big.Int {
+    var weiFloat big.Float
+    var wei big.Int
+    weiFloat.Mul(big.NewFloat(gwei), big.NewFloat(WEI_PER_GWEI))
     weiFloat.Int(&wei)
     return &wei
 }
