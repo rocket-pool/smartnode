c0a973990f10ffa02c5d139b8ec0144fef54f451
Jake Pospischil <moles1@gmail.com>
2019-05-29 10:56:11 +1000

    indentation

--- rocketpool/daemons/validator/beacon/watchtower.go
+++ rocketpool/daemons/validator/beacon/watchtower.go
@@ -1,19 +1,19 @@
 package beacon
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"log"
-	"time"
+    "encoding/json"
+    "errors"
+    "fmt"
+    "log"
+    "time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/fatih/color"
+    "github.com/ethereum/go-ethereum/common"
+    "github.com/fatih/color"
 
-	"github.com/rocket-pool/smartnode-cli/rocketpool/services"
-	beaconchain "github.com/rocket-pool/smartnode-cli/rocketpool/services/beacon-chain"
-	"github.com/rocket-pool/smartnode-cli/rocketpool/services/rocketpool/minipool"
-	"github.com/rocket-pool/smartnode-cli/rocketpool/utils/eth"
+    "github.com/rocket-pool/smartnode-cli/rocketpool/services"
+    beaconchain "github.com/rocket-pool/smartnode-cli/rocketpool/services/beacon-chain"
+    "github.com/rocket-pool/smartnode-cli/rocketpool/services/rocketpool/minipool"
+    "github.com/rocket-pool/smartnode-cli/rocketpool/utils/eth"
 )
 
 // Config
@@ -26,12 +26,12 @@ var getActiveMinipoolsInterval, _ = time.ParseDuration(GET_ACTIVE_MINIPOOLS_INTE
 
 // Watchtower process
 type WatchtowerProcess struct {
-	c                      func(a ...interface{}) string
-	p                      *services.Provider
-	updatingMinipools      bool
-	getActiveMinipoolsStop chan bool
-	beaconMessageChannel   chan interface{}
-	activeMinipools        map[string]common.Address
+    c                      func(a ...interface{}) string
+    p                      *services.Provider
+    updatingMinipools      bool
+    getActiveMinipoolsStop chan bool
+    beaconMessageChannel   chan interface{}
+    activeMinipools        map[string]common.Address
 }
 
 /**
@@ -39,18 +39,18 @@ type WatchtowerProcess struct {
  */
 func StartWatchtowerProcess(p *services.Provider) {
 
-	// Initialise process
-	process := &WatchtowerProcess{
-		c:                      color.New(WATCHTOWER_LOG_COLOR).SprintFunc(),
-		p:                      p,
-		updatingMinipools:      false,
-		getActiveMinipoolsStop: make(chan bool),
-		beaconMessageChannel:   make(chan interface{}),
-		activeMinipools:        make(map[string]common.Address),
-	}
+    // Initialise process
+    process := &WatchtowerProcess{
+        c:                      color.New(WATCHTOWER_LOG_COLOR).SprintFunc(),
+        p:                      p,
+        updatingMinipools:      false,
+        getActiveMinipoolsStop: make(chan bool),
+        beaconMessageChannel:   make(chan interface{}),
+        activeMinipools:        make(map[string]common.Address),
+    }
 
-	// Start
-	process.start()
+    // Start
+    process.start()
 
 }
 
@@ -59,25 +59,25 @@ func StartWatchtowerProcess(p *services.Provider) {
  */
 func (p *WatchtowerProcess) start() {
 
-	// Check if node is trusted on interval
-	go (func() {
-		p.checkTrusted()
-		checkTrustedTimer := time.NewTicker(checkTrustedInterval)
-		for _ = range checkTrustedTimer.C {
-			p.checkTrusted()
-		}
-	})()
+    // Check if node is trusted on interval
+    go (func() {
+        p.checkTrusted()
+        checkTrustedTimer := time.NewTicker(checkTrustedInterval)
+        for _ = range checkTrustedTimer.C {
+            p.checkTrusted()
+        }
+    })()
 
-	// Handle beacon chain events
-	go (func() {
-		for eventData := range p.beaconMessageChannel {
-			event := (eventData).(struct {
-				Client  *beaconchain.Client
-				Message []byte
-			})
-			p.onBeaconClientMessage(event.Message)
-		}
-	})()
+    // Handle beacon chain events
+    go (func() {
+        for eventData := range p.beaconMessageChannel {
+            event := (eventData).(struct {
+                Client  *beaconchain.Client
+                Message []byte
+            })
+            p.onBeaconClientMessage(event.Message)
+        }
+    })()
 
 }
 
@@ -86,19 +86,19 @@ func (p *WatchtowerProcess) start() {
  */
 func (p *WatchtowerProcess) checkTrusted() {
 
-	// Get trusted status
-	trusted := new(bool)
-	if err := p.p.CM.Contracts["rocketNodeAPI"].Call(nil, trusted, "getTrusted", p.p.AM.GetNodeAccount().Address); err != nil {
-		log.Println(p.c(errors.New("Error retrieving node trusted status: " + err.Error())))
-		return
-	}
+    // Get trusted status
+    trusted := new(bool)
+    if err := p.p.CM.Contracts["rocketNodeAPI"].Call(nil, trusted, "getTrusted", p.p.AM.GetNodeAccount().Address); err != nil {
+        log.Println(p.c(errors.New("Error retrieving node trusted status: " + err.Error())))
+        return
+    }
 
-	// Start/stop minipool updates
-	if *trusted {
-		p.startUpdateMinipools()
-	} else {
-		p.stopUpdateMinipools()
-	}
+    // Start/stop minipool updates
+    if *trusted {
+        p.startUpdateMinipools()
+    } else {
+        p.stopUpdateMinipools()
+    }
 
 }
 
@@ -107,32 +107,32 @@ func (p *WatchtowerProcess) checkTrusted() {
  */
 func (p *WatchtowerProcess) startUpdateMinipools() {
 
-	// Cancel if already updating minipools
-	if p.updatingMinipools {
-		return
-	}
-	p.updatingMinipools = true
+    // Cancel if already updating minipools
+    if p.updatingMinipools {
+        return
+    }
+    p.updatingMinipools = true
 
-	// Log
-	log.Println(p.c("Node is trusted, starting watchtower process..."))
+    // Log
+    log.Println(p.c("Node is trusted, starting watchtower process..."))
 
-	// Get active minipools on interval
-	go (func() {
-		p.getActiveMinipools()
-		getActiveMinipoolsTimer := time.NewTicker(getActiveMinipoolsInterval)
-		for {
-			select {
-			case <-getActiveMinipoolsTimer.C:
-				p.getActiveMinipools()
-			case <-p.getActiveMinipoolsStop:
-				getActiveMinipoolsTimer.Stop()
-				return
-			}
-		}
-	})()
+    // Get active minipools on interval
+    go (func() {
+        p.getActiveMinipools()
+        getActiveMinipoolsTimer := time.NewTicker(getActiveMinipoolsInterval)
+        for {
+            select {
+            case <-getActiveMinipoolsTimer.C:
+                p.getActiveMinipools()
+            case <-p.getActiveMinipoolsStop:
+                getActiveMinipoolsTimer.Stop()
+                return
+            }
+        }
+    })()
 
-	// Subscribe to beacon chain events
-	p.p.Publisher.AddSubscriber("beacon.client.message", p.beaconMessageChannel)
+    // Subscribe to beacon chain events
+    p.p.Publisher.AddSubscriber("beacon.client.message", p.beaconMessageChannel)
 
 }
 
@@ -141,20 +141,20 @@ func (p *WatchtowerProcess) startUpdateMinipools() {
  */
 func (p *WatchtowerProcess) stopUpdateMinipools() {
 
-	// Cancel if not updating minipools
-	if !p.updatingMinipools {
-		return
-	}
-	p.updatingMinipools = false
+    // Cancel if not updating minipools
+    if !p.updatingMinipools {
+        return
+    }
+    p.updatingMinipools = false
 
-	// Log
-	log.Println(p.c("Node is untrusted, stopping watchtower process..."))
+    // Log
+    log.Println(p.c("Node is untrusted, stopping watchtower process..."))
 
-	// Stop getting active minipools
-	p.getActiveMinipoolsStop <- true
+    // Stop getting active minipools
+    p.getActiveMinipoolsStop <- true
 
-	// Unsubscribe from beacon chain events
-	p.p.Publisher.RemoveSubscriber("beacon.client.message", p.beaconMessageChannel)
+    // Unsubscribe from beacon chain events
+    p.p.Publisher.RemoveSubscriber("beacon.client.message", p.beaconMessageChannel)
 
 }
 
@@ -163,38 +163,38 @@ func (p *WatchtowerProcess) stopUpdateMinipools() {
  */
 func (p *WatchtowerProcess) getActiveMinipools() {
 
-	// Get active minipools
-	if activeMinipools, err := minipool.GetActiveMinipoolsByValidatorPubkey(p.p.CM); err != nil {
-		log.Println(p.c(errors.New("Error getting active minipools: " + err.Error())))
-		return
-	} else {
-		p.activeMinipools = *activeMinipools
-	}
+    // Get active minipools
+    if activeMinipools, err := minipool.GetActiveMinipoolsByValidatorPubkey(p.p.CM); err != nil {
+        log.Println(p.c(errors.New("Error getting active minipools: " + err.Error())))
+        return
+    } else {
+        p.activeMinipools = *activeMinipools
+    }
 
-	// Request validator statuses for active minipools
-	for pubkey, minipoolAddress := range p.activeMinipools {
-		go (func(pubkey string, minipoolAddress common.Address) {
+    // Request validator statuses for active minipools
+    for pubkey, minipoolAddress := range p.activeMinipools {
+        go (func(pubkey string, minipoolAddress common.Address) {
 
-			// Check minipool status
-			if status, err := getMinipoolStatus(p.p, &minipoolAddress); err != nil {
-				log.Println(p.c(err))
-				return
-			} else if *status < minipool.STAKING || *status > minipool.LOGGED_OUT {
-				return
-			}
+            // Check minipool status
+            if status, err := getMinipoolStatus(p.p, &minipoolAddress); err != nil {
+                log.Println(p.c(err))
+                return
+            } else if *status < minipool.STAKING || *status > minipool.LOGGED_OUT {
+                return
+            }
 
-			// Request validator status
-			if payload, err := json.Marshal(beaconchain.ClientMessage{
-				Message: "get_validator_status",
-				Pubkey:  pubkey,
-			}); err != nil {
-				log.Println(p.c(errors.New("Error encoding get validator status payload: " + err.Error())))
-			} else if err := p.p.Beacon.Send(payload); err != nil {
-				log.Println(p.c(errors.New("Error sending get validator status message: " + err.Error())))
-			}
+            // Request validator status
+            if payload, err := json.Marshal(beaconchain.ClientMessage{
+                Message: "get_validator_status",
+                Pubkey:  pubkey,
+            }); err != nil {
+                log.Println(p.c(errors.New("Error encoding get validator status payload: " + err.Error())))
+            } else if err := p.p.Beacon.Send(payload); err != nil {
+                log.Println(p.c(errors.New("Error sending get validator status message: " + err.Error())))
+            }
 
-		})(pubkey, minipoolAddress)
-	}
+        })(pubkey, minipoolAddress)
+    }
 
 }
 
@@ -203,97 +203,97 @@ func (p *WatchtowerProcess) getActiveMinipools() {
  */
 func (p *WatchtowerProcess) onBeaconClientMessage(messageData []byte) {
 
-	// Parse message
-	message := new(beaconchain.ServerMessage)
-	if err := json.Unmarshal(messageData, message); err != nil {
-		log.Println(p.c(errors.New("Error decoding beacon message: " + err.Error())))
-		return
-	}
+    // Parse message
+    message := new(beaconchain.ServerMessage)
+    if err := json.Unmarshal(messageData, message); err != nil {
+        log.Println(p.c(errors.New("Error decoding beacon message: " + err.Error())))
+        return
+    }
 
-	// Handle exit and withdrawal validator status messages only
-	if message.Message != "validator_status" {
-		return
-	}
-	if !(message.Status.Code == "exited" || message.Status.Code == "withdrawable") {
-		return
-	}
+    // Handle exit and withdrawal validator status messages only
+    if message.Message != "validator_status" {
+        return
+    }
+    if !(message.Status.Code == "exited" || message.Status.Code == "withdrawable") {
+        return
+    }
 
-	// Get associated minipool
-	minipoolAddress, ok := p.activeMinipools[message.Pubkey]
-	if !ok {
-		return
-	}
+    // Get associated minipool
+    minipoolAddress, ok := p.activeMinipools[message.Pubkey]
+    if !ok {
+        return
+    }
 
-	// Get minipool status
-	status, err := getMinipoolStatus(p.p, &minipoolAddress)
-	if err != nil {
-		log.Println(p.c(err))
-		return
-	}
+    // Get minipool status
+    status, err := getMinipoolStatus(p.p, &minipoolAddress)
+    if err != nil {
+        log.Println(p.c(err))
+        return
+    }
 
-	// Log minipool out if staking
-	if *status == minipool.STAKING {
+    // Log minipool out if staking
+    if *status == minipool.STAKING {
 
-		// Log
-		log.Println(p.c(fmt.Sprintf("Minipool %s is ready for logout...", minipoolAddress.Hex())))
+        // Log
+        log.Println(p.c(fmt.Sprintf("Minipool %s is ready for logout...", minipoolAddress.Hex())))
 
-		// Log out
-		if txor, err := p.p.AM.GetNodeAccountTransactor(); err != nil {
-			log.Println(p.c(err))
-		} else {
-			txor.GasLimit = 250000 // Gas estimates on this method are incorrect
-			if _, err := p.p.CM.Contracts["rocketNodeWatchtower"].Transact(txor, "logoutMinipool", minipoolAddress); err != nil {
-				log.Println(p.c(errors.New("Error logging out minipool: " + err.Error())))
-			} else {
-				log.Println(p.c(fmt.Sprintf("Minipool %s was successfully logged out", minipoolAddress.Hex())))
-			}
-		}
+        // Log out
+        if txor, err := p.p.AM.GetNodeAccountTransactor(); err != nil {
+            log.Println(p.c(err))
+        } else {
+            txor.GasLimit = 250000 // Gas estimates on this method are incorrect
+            if _, err := p.p.CM.Contracts["rocketNodeWatchtower"].Transact(txor, "logoutMinipool", minipoolAddress); err != nil {
+                log.Println(p.c(errors.New("Error logging out minipool: " + err.Error())))
+            } else {
+                log.Println(p.c(fmt.Sprintf("Minipool %s was successfully logged out", minipoolAddress.Hex())))
+            }
+        }
 
-		return
-	}
+        return
+    }
 
-	// Withdraw minipool if logged out and withdrawable
-	if *status == minipool.LOGGED_OUT && message.Status.Code == "withdrawable" {
+    // Withdraw minipool if logged out and withdrawable
+    if *status == minipool.LOGGED_OUT && message.Status.Code == "withdrawable" {
 
-		// Log
-		log.Println(p.c(fmt.Sprintf("Minipool %s is ready for withdrawal...", minipoolAddress.Hex())))
+        // Log
+        log.Println(p.c(fmt.Sprintf("Minipool %s is ready for withdrawal...", minipoolAddress.Hex())))
 
-		// Get balance to withdraw
-		balanceWei := eth.GweiToWei(float64(message.Balance))
+        // Get balance to withdraw
+        balanceWei := eth.GweiToWei(float64(message.Balance))
 
-		// Withdraw
-		if txor, err := p.p.AM.GetNodeAccountTransactor(); err != nil {
-			log.Println(p.c(err))
-		} else {
-			txor.GasLimit = 250000 // Gas estimates on this method are incorrect
-			if _, err := p.p.CM.Contracts["rocketNodeWatchtower"].Transact(txor, "withdrawMinipool", minipoolAddress, balanceWei); err != nil {
-				log.Println(p.c(errors.New("Error withdrawing minipool: " + err.Error())))
-			} else {
-				log.Println(p.c(fmt.Sprintf("Minipool %s was successfully withdrawn with a balance of %.2f ETH", minipoolAddress.Hex(), eth.WeiToEth(balanceWei))))
-			}
-		}
+        // Withdraw
+        if txor, err := p.p.AM.GetNodeAccountTransactor(); err != nil {
+            log.Println(p.c(err))
+        } else {
+            txor.GasLimit = 250000 // Gas estimates on this method are incorrect
+            if _, err := p.p.CM.Contracts["rocketNodeWatchtower"].Transact(txor, "withdrawMinipool", minipoolAddress, balanceWei); err != nil {
+                log.Println(p.c(errors.New("Error withdrawing minipool: " + err.Error())))
+            } else {
+                log.Println(p.c(fmt.Sprintf("Minipool %s was successfully withdrawn with a balance of %.2f ETH", minipoolAddress.Hex(), eth.WeiToEth(balanceWei))))
+            }
+        }
 
-		return
-	}
+        return
+    }
 
 }
 
 // Get a minipool's status
 func getMinipoolStatus(p *services.Provider, address *common.Address) (*uint8, error) {
 
-	// Initialise minipool contract
-	minipoolContract, err := p.CM.NewContract(address, "rocketMinipool")
-	if err != nil {
-		return nil, errors.New("Error initialising minipool contract: " + err.Error())
-	}
+    // Initialise minipool contract
+    minipoolContract, err := p.CM.NewContract(address, "rocketMinipool")
+    if err != nil {
+        return nil, errors.New("Error initialising minipool contract: " + err.Error())
+    }
 
-	// Get minipool's current status
-	status := new(uint8)
-	if err := minipoolContract.Call(nil, status, "getStatus"); err != nil {
-		return nil, errors.New("Error retrieving minipool status: " + err.Error())
-	}
+    // Get minipool's current status
+    status := new(uint8)
+    if err := minipoolContract.Call(nil, status, "getStatus"); err != nil {
+        return nil, errors.New("Error retrieving minipool status: " + err.Error())
+    }
 
-	// Return
-	return status, nil
+    // Return
+    return status, nil
 
 }
--- rocketpool/smartnode-cli.go
+++ rocketpool/smartnode-cli.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
+    "fmt"
+    "log"
+    "os"
 
-	"gopkg.in/urfave/cli.v1"
-	"gopkg.in/urfave/cli.v1/altsrc"
+    "gopkg.in/urfave/cli.v1"
+    "gopkg.in/urfave/cli.v1/altsrc"
 
-	"github.com/rocket-pool/smartnode-cli/rocketpool/api/deposit"
-	"github.com/rocket-pool/smartnode-cli/rocketpool/api/fee"
-	"github.com/rocket-pool/smartnode-cli/rocketpool/api/minipool"
-	"github.com/rocket-pool/smartnode-cli/rocketpool/api/node"
-	"github.com/rocket-pool/smartnode-cli/rocketpool/api/service"
+    "github.com/rocket-pool/smartnode-cli/rocketpool/api/deposit"
+    "github.com/rocket-pool/smartnode-cli/rocketpool/api/fee"
+    "github.com/rocket-pool/smartnode-cli/rocketpool/api/minipool"
+    "github.com/rocket-pool/smartnode-cli/rocketpool/api/node"
+    "github.com/rocket-pool/smartnode-cli/rocketpool/api/service"
 )
 
 // Run
 func main() {
 
-	// Add logo to application help template
-	cli.AppHelpTemplate = fmt.Sprintf(`
+    // Add logo to application help template
+    cli.AppHelpTemplate = fmt.Sprintf(`
 ______           _        _    ______           _ 
 | ___ \         | |      | |   | ___ \         | |
 | |_/ /___   ___| | _____| |_  | |_/ /__   ___ | |
@@ -29,79 +29,79 @@ ______           _        _    ______           _
 
 %s`, cli.AppHelpTemplate)
 
-	// Initialise application
-	app := cli.NewApp()
+    // Initialise application
+    app := cli.NewApp()
 
-	// Configure application
-	app.Name = "rocketpool"
-	app.Usage = "Rocket Pool node operator utilities"
-	app.Version = "0.0.1"
-	app.Authors = []cli.Author{
-		cli.Author{
-			Name:  "Darren Langley",
-			Email: "darren@rocketpool.net",
-		},
-		cli.Author{
-			Name:  "David Rugendyke",
-			Email: "david@rocketpool.net",
-		},
-		cli.Author{
-			Name:  "Jake Pospischil",
-			Email: "jake@rocketpool.net",
-		},
-	}
-	app.Copyright = "(c) 2019 Rocket Pool Pty Ltd"
+    // Configure application
+    app.Name = "rocketpool"
+    app.Usage = "Rocket Pool node operator utilities"
+    app.Version = "0.0.1"
+    app.Authors = []cli.Author{
+        cli.Author{
+            Name:  "Darren Langley",
+            Email: "darren@rocketpool.net",
+        },
+        cli.Author{
+            Name:  "David Rugendyke",
+            Email: "david@rocketpool.net",
+        },
+        cli.Author{
+            Name:  "Jake Pospischil",
+            Email: "jake@rocketpool.net",
+        },
+    }
+    app.Copyright = "(c) 2019 Rocket Pool Pty Ltd"
 
-	// Register global application options & defaults
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "config",
-			Usage: "Rocket Pool CLI config file absolute `path`",
-			Value: os.Getenv("HOME") + "/.rocketpool/config.yml",
-		},
-		altsrc.NewStringFlag(cli.StringFlag{
-			Name:  "database",
-			Usage: "Rocket Pool CLI database absolute `path`",
-			Value: os.Getenv("HOME") + "/.rocketpool/rocketpool-cli.db",
-		}),
-		altsrc.NewStringFlag(cli.StringFlag{
-			Name:  "keychain",
-			Usage: "PoW chain account keychain absolute `path`",
-			Value: os.Getenv("HOME") + "/.rocketpool/accounts",
-		}),
-		altsrc.NewStringFlag(cli.StringFlag{
-			Name:  "providerPow",
-			Usage: "PoW chain provider `url`",
-			Value: "http://localhost:8545",
-		}),
-		altsrc.NewStringFlag(cli.StringFlag{
-			Name:  "providerBeacon",
-			Usage: "Beacon chain provider `url`",
-			Value: "ws://localhost:9545", // Local simulator
-		}),
-		altsrc.NewStringFlag(cli.StringFlag{
-			Name:  "storageAddress",
-			Usage: "PoW chain Rocket Pool storage contract `address`",
-			Value: "0x70a5F2eB9e4C003B105399b471DAeDbC8d00B1c5", // Ganache
-		}),
-	}
+    // Register global application options & defaults
+    app.Flags = []cli.Flag{
+        cli.StringFlag{
+            Name:  "config",
+            Usage: "Rocket Pool CLI config file absolute `path`",
+            Value: os.Getenv("HOME") + "/.rocketpool/config.yml",
+        },
+        altsrc.NewStringFlag(cli.StringFlag{
+            Name:  "database",
+            Usage: "Rocket Pool CLI database absolute `path`",
+            Value: os.Getenv("HOME") + "/.rocketpool/rocketpool-cli.db",
+        }),
+        altsrc.NewStringFlag(cli.StringFlag{
+            Name:  "keychain",
+            Usage: "PoW chain account keychain absolute `path`",
+            Value: os.Getenv("HOME") + "/.rocketpool/accounts",
+        }),
+        altsrc.NewStringFlag(cli.StringFlag{
+            Name:  "providerPow",
+            Usage: "PoW chain provider `url`",
+            Value: "http://localhost:8545",
+        }),
+        altsrc.NewStringFlag(cli.StringFlag{
+            Name:  "providerBeacon",
+            Usage: "Beacon chain provider `url`",
+            Value: "ws://localhost:9545", // Local simulator
+        }),
+        altsrc.NewStringFlag(cli.StringFlag{
+            Name:  "storageAddress",
+            Usage: "PoW chain Rocket Pool storage contract `address`",
+            Value: "0x70a5F2eB9e4C003B105399b471DAeDbC8d00B1c5", // Ganache
+        }),
+    }
 
-	// Register api commands
-	deposit.RegisterCommands(app, "deposit", []string{"d"})
-	fee.RegisterCommands(app, "fee", []string{"f"})
-	minipool.RegisterCommands(app, "minipool", []string{"m"})
-	node.RegisterCommands(app, "node", []string{"n"})
-	service.RegisterCommands(app, "service", []string{"s"})
+    // Register api commands
+    deposit.RegisterCommands(app, "deposit", []string{"d"})
+    fee.RegisterCommands(app, "fee", []string{"f"})
+    minipool.RegisterCommands(app, "minipool", []string{"m"})
+    node.RegisterCommands(app, "node", []string{"n"})
+    service.RegisterCommands(app, "service", []string{"s"})
 
-	// Load external config, squelch load errors
-	app.Before = func(c *cli.Context) error {
-		altsrc.InitInputSourceWithContext(app.Flags, altsrc.NewYamlSourceFromFlagFunc("config"))(c)
-		return nil
-	}
+    // Load external config, squelch load errors
+    app.Before = func(c *cli.Context) error {
+        altsrc.InitInputSourceWithContext(app.Flags, altsrc.NewYamlSourceFromFlagFunc("config"))(c)
+        return nil
+    }
 
-	// Run application
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
+    // Run application
+    if err := app.Run(os.Args); err != nil {
+        log.Fatal(err)
+    }
 
 }
