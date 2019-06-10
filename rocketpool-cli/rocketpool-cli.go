ce08f104dc5078b5c7d02e313e32313a731248a9
Jake Pospischil <moles1@gmail.com>
2019-06-10 11:37:58 +1000

    moved application config to shared module

--- rocketpool-cli/rocketpool-cli.go
+++ rocketpool-cli/rocketpool-cli.go
@@ -6,12 +6,12 @@ import (
     "os"
 
     "gopkg.in/urfave/cli.v1"
-    "gopkg.in/urfave/cli.v1/altsrc"
 
     "github.com/rocket-pool/smartnode-cli/rocketpool-cli/deposit"
     "github.com/rocket-pool/smartnode-cli/rocketpool-cli/fee"
     "github.com/rocket-pool/smartnode-cli/rocketpool-cli/minipool"
     "github.com/rocket-pool/smartnode-cli/rocketpool-cli/node"
+    cliutils "github.com/rocket-pool/smartnode-cli/shared/utils/cli"
 )
 
 // Run
@@ -31,8 +31,8 @@ ______           _        _    ______           _
     // Initialise application
     app := cli.NewApp()
 
-    // Configure application
-    app.Name = "rocketpool"
+    // Set application info
+    app.Name = "rocketpool-cli"
     app.Usage = "Rocket Pool node operator utilities"
     app.Version = "0.0.1"
     app.Authors = []cli.Author{
@@ -51,61 +51,14 @@ ______           _        _    ______           _
     }
     app.Copyright = "(c) 2019 Rocket Pool Pty Ltd"
 
-    // Register global application options & defaults
-    app.Flags = []cli.Flag{
-        cli.StringFlag{
-            Name:  "config",
-            Usage: "Rocket Pool CLI config file absolute `path`",
-            Value: os.Getenv("HOME") + "/.rocketpool/config.yml",
-        },
-        altsrc.NewStringFlag(cli.StringFlag{
-            Name:  "database",
-            Usage: "Rocket Pool CLI database absolute `path`",
-            Value: os.Getenv("HOME") + "/.rocketpool/rocketpool-cli.db",
-        }),
-        altsrc.NewStringFlag(cli.StringFlag{
-            Name:  "password",
-            Usage: "Rocket Pool CLI keystore password `path`",
-            Value: os.Getenv("HOME") + "/.rocketpool/password",
-        }),
-        altsrc.NewStringFlag(cli.StringFlag{
-            Name:  "keychainPow",
-            Usage: "PoW chain account keychain absolute `path`",
-            Value: os.Getenv("HOME") + "/.rocketpool/accounts",
-        }),
-        altsrc.NewStringFlag(cli.StringFlag{
-            Name:  "keychainBeacon",
-            Usage: "Beacon chain account keychain absolute `path`",
-            Value: os.Getenv("HOME") + "/.rocketpool/validators",
-        }),
-        altsrc.NewStringFlag(cli.StringFlag{
-            Name:  "providerPow",
-            Usage: "PoW chain provider `url`",
-            Value: "http://localhost:8545",
-        }),
-        altsrc.NewStringFlag(cli.StringFlag{
-            Name:  "providerBeacon",
-            Usage: "Beacon chain provider `url`",
-            Value: "ws://localhost:9545", // Local simulator
-        }),
-        altsrc.NewStringFlag(cli.StringFlag{
-            Name:  "storageAddress",
-            Usage: "PoW chain Rocket Pool storage contract `address`",
-            Value: "0x70a5F2eB9e4C003B105399b471DAeDbC8d00B1c5", // Ganache
-        }),
-    }
+    // Configure application
+    cliutils.Configure(app)
 
-    // Register api commands
+    // Register commands
     deposit.RegisterCommands(app, "deposit", []string{"d"})
     fee.RegisterCommands(app, "fee", []string{"f"})
     minipool.RegisterCommands(app, "minipool", []string{"m"})
     node.RegisterCommands(app, "node", []string{"n"})
-
-    // Load external config, squelch load errors
-    app.Before = func(c *cli.Context) error {
-        altsrc.InitInputSourceWithContext(app.Flags, altsrc.NewYamlSourceFromFlagFunc("config"))(c)
-        return nil
-    }
 
     // Run application
     if err := app.Run(os.Args); err != nil {
---
+++ shared/utils/cli/config.go
@@ -0,0 +1,65 @@
+package cli
+
+import (
+    "os"
+
+    "gopkg.in/urfave/cli.v1"
+    "gopkg.in/urfave/cli.v1/altsrc"
+)
+
+
+// Configure the application options
+func Configure(app *cli.App) {
+
+    // Register global application options & defaults
+    app.Flags = []cli.Flag{
+        cli.StringFlag{
+            Name:  "config",
+            Usage: "Rocket Pool CLI config file absolute `path`",
+            Value: os.Getenv("HOME") + "/.rocketpool/config.yml",
+        },
+        altsrc.NewStringFlag(cli.StringFlag{
+            Name:  "database",
+            Usage: "Rocket Pool CLI database absolute `path`",
+            Value: os.Getenv("HOME") + "/.rocketpool/rocketpool.db",
+        }),
+        altsrc.NewStringFlag(cli.StringFlag{
+            Name:  "password",
+            Usage: "Rocket Pool CLI keystore password `path`",
+            Value: os.Getenv("HOME") + "/.rocketpool/password",
+        }),
+        altsrc.NewStringFlag(cli.StringFlag{
+            Name:  "keychainPow",
+            Usage: "PoW chain account keychain absolute `path`",
+            Value: os.Getenv("HOME") + "/.rocketpool/accounts",
+        }),
+        altsrc.NewStringFlag(cli.StringFlag{
+            Name:  "keychainBeacon",
+            Usage: "Beacon chain account keychain absolute `path`",
+            Value: os.Getenv("HOME") + "/.rocketpool/validators",
+        }),
+        altsrc.NewStringFlag(cli.StringFlag{
+            Name:  "providerPow",
+            Usage: "PoW chain provider `url`",
+            Value: "http://localhost:8545",
+        }),
+        altsrc.NewStringFlag(cli.StringFlag{
+            Name:  "providerBeacon",
+            Usage: "Beacon chain provider `url`",
+            Value: "ws://localhost:9545", // Local simulator
+        }),
+        altsrc.NewStringFlag(cli.StringFlag{
+            Name:  "storageAddress",
+            Usage: "PoW chain Rocket Pool storage contract `address`",
+            Value: "0x70a5F2eB9e4C003B105399b471DAeDbC8d00B1c5", // Ganache
+        }),
+    }
+
+    // Load external config, squelch load errors
+    app.Before = func(c *cli.Context) error {
+        altsrc.InitInputSourceWithContext(app.Flags, altsrc.NewYamlSourceFromFlagFunc("config"))(c)
+        return nil
+    }
+
+}
+
