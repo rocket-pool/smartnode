29dcc0fa453c9b31df66188baa3728615d0f4c4f
Jake Pospischil <moles1@gmail.com>
2019-06-25 15:55:27 +1000

    implementing minipool management daemon

--- rocketpool-minipools/minipools/management.go
+++ rocketpool-minipools/minipools/management.go
@@ -1,9 +1,14 @@
 package minipools
 
 import (
+    "context"
+    "errors"
+    "fmt"
     "log"
     "time"
 
+    "github.com/docker/docker/api/types"
+    "github.com/docker/docker/api/types/container"
     "github.com/ethereum/go-ethereum/common"
 
     "github.com/rocket-pool/smartnode/shared/services"
@@ -13,6 +18,8 @@ import (
 
 
 // Config
+const CONTAINER_IMAGE_NAME string = "rocketpool-minipool"
+const CONTAINER_NAME_PREFIX string = "rocketpool_minipool_"
 const CHECK_MINIPOOLS_INTERVAL string = "15s"
 var checkMinipoolsInterval, _ = time.ParseDuration(CHECK_MINIPOOLS_INTERVAL)
 
@@ -63,6 +70,7 @@ func (p *ManagementProcess) checkMinipools() {
 
     // Data channels
     stakingMinipoolAddressesChannel := make(chan []*common.Address)
+    minipoolContainersChannel := make(chan []types.Container)
     errorChannel := make(chan error)
 
     // Get staking minipool addresses
@@ -107,16 +115,108 @@ func (p *ManagementProcess) checkMinipools() {
 
     })()
 
+    // Get active minipool containers
+    go (func() {
+
+        // Get docker containers
+        containers, err := p.p.Docker.ContainerList(context.Background(), types.ContainerListOptions{All: true})
+        if err != nil {
+            errorChannel <- errors.New("Error retrieving docker containers: " + err.Error())
+            return
+        }
+
+        // Filter by minipool container image name
+        minipoolContainers := []types.Container{}
+        for _, container := range containers {
+            if container.Image == CONTAINER_IMAGE_NAME { minipoolContainers = append(minipoolContainers, container) }
+        }
+
+        // Send minipool containers
+        minipoolContainersChannel <- minipoolContainers
+
+    })()
+
     // Receive minipool data
     var stakingMinipoolAddresses []*common.Address
-    for received := 0; received < 1; {
+    var minipoolContainers []types.Container
+    for received := 0; received < 2; {
         select {
             case stakingMinipoolAddresses = <-stakingMinipoolAddressesChannel:
+                received++
+            case minipoolContainers = <-minipoolContainersChannel:
                 received++
             case err := <-errorChannel:
                 log.Println(err)
                 return
         }
+    }
+
+    // Run minipool containers
+    for _, minipoolAddress := range stakingMinipoolAddresses {
+        go p.runMinipoolContainer(minipoolAddress, minipoolContainers)
+    }
+
+}
+
+
+/**
+ * Run minipool container
+ */
+func (p *ManagementProcess) runMinipoolContainer(minipoolAddress *common.Address, minipoolContainers []types.Container) {
+
+    // Get name for minipool container
+    containerName := CONTAINER_NAME_PREFIX + minipoolAddress.Hex()
+
+    // Get existing minipool container ID
+    var containerId string
+    for _, container := range minipoolContainers {
+        if "/" + containerName == container.Names[0] {
+            containerId = container.ID
+            break
+        }
+    }
+
+    // Create minipool container if it doesn't exist
+    if containerId == "" {
+
+        // Log
+        log.Println(fmt.Sprintf("Creating minipool container %s...", containerName))
+
+        // Create container
+        if response, err := p.p.Docker.ContainerCreate(context.Background(), &container.Config{
+            Image: CONTAINER_IMAGE_NAME,
+        }, &container.HostConfig{
+            Binds: []string{"/Users/moles/.rocketpool:/.rocketpool"},
+            Links: []string{"rocketpool_geth_1:geth", "rocketpool_beacon_1:beacon"},
+            NetworkMode: "rocketpool_net",
+            RestartPolicy: container.RestartPolicy{Name: "on-failure"},
+        }, nil, containerName); err != nil {
+            log.Println(errors.New(fmt.Sprintf("Error creating minipool container %s: " + err.Error(), containerName)))
+            return
+        } else {
+            log.Println(fmt.Sprintf("Created minipool container %s successfully", containerName))
+            containerId = response.ID
+        }
+
+    }
+
+    // Start minipool container if not running
+    if container, err := p.p.Docker.ContainerInspect(context.Background(), containerId); err != nil {
+        log.Println(errors.New(fmt.Sprintf("Error inspecting minipool container %s: " + err.Error(), containerName)))
+        return
+    } else if !container.State.Running {
+
+        // Log
+        log.Println(fmt.Sprintf("Starting minipool container %s...", containerName))
+
+        // Start container
+        if err := p.p.Docker.ContainerStart(context.Background(), containerId, types.ContainerStartOptions{}); err != nil {
+            log.Println(errors.New(fmt.Sprintf("Error starting minipool container %s: " + err.Error(), containerName)))
+            return
+        } else {
+            log.Println(fmt.Sprintf("Started minipool container %s successfully", containerName))
+        }
+
     }
 
 }
