fbcc70a77d2cb0167406704916728c1fae384193
Jake Pospischil <moles1@gmail.com>
2019-06-26 14:33:56 +1000

    implemented eth1 client setup shell script; added eth1 and beacon client data volumes

---
+++ docker/pow/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# This script configures ETH1 clients for Rocket Pool's scalable docker stack; only edit if you know what you're doing ;)
+
+# Arguments
+CLIENT=$1          # Client type (geth/parity)
+NETWORKID=$2       # ID of network to join
+BOOTNODE=$3        # Initial bootnode(s) to connect to
+ETHSTATSLABEL=$4   # The ID to show on the ETHSTATS page (with container ID appended)
+ETHSTATSLOGIN=$5   # Login credentials for ethstats
+
+# Get container ID
+CONTAINERID="${HOSTNAME}"
+
+# Create a container data directory if it doesn't exist
+DATADIR="/ethclient/$CONTAINERID"
+mkdir -p "$DATADIR"
+
+# Geth startup
+if [ $CLIENT == "geth" ]; then
+
+    # Initialise
+    CMD="/usr/local/bin/geth --datadir $DATADIR init /pow/genesis77.json"
+
+    # Run
+    CMD="$CMD && /usr/local/bin/geth --datadir $DATADIR --networkid $NETWORKID --syncmode full --rpc --rpcport 8545 --rpcaddr 0.0.0.0 --rpcvhosts '*' --rpcapi db,eth,net,web3,personal --bootnodes $BOOTNODE"
+
+    # Add Ethstats to run
+    if [ ! -z "$ETHSTATSLABEL" ] && [ ! -z "$ETHSTATSLOGIN" ]; then
+        CMD="$CMD --ethstats $ETHSTATSLABEL-$CONTAINERID:$ETHSTATSLOGIN"
+    fi
+
+    # Run command
+    eval "$CMD"
+
+fi
+
+# Parity startup
+# TODO: implement
