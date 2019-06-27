2130474b0a26deab753fb17183f02182780af8f8
Jake Pospischil <moles1@gmail.com>
2019-06-27 12:18:54 +1000

    formatting fix

--- docker/pow/setup.sh
+++ docker/pow/setup.sh
@@ -22,7 +22,9 @@ if [ $CLIENT == "geth" ]; then
     CMD="/usr/local/bin/geth --datadir $DATADIR init /pow/genesis77.json"
 
     # Run
-    CMD="$CMD && /usr/local/bin/geth --datadir $DATADIR --networkid $NETWORKID --syncmode full --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --rpcapi db,eth,net,web3,personal --rpcvhosts '*' --ws --wsaddr 0.0.0.0 --wsport 8546 --wsapi db,eth,net,web3,personal --wsorigins '*' --bootnodes $BOOTNODE"
+    CMD="$CMD && /usr/local/bin/geth --datadir $DATADIR --networkid $NETWORKID --syncmode full --bootnodes $BOOTNODE"
+    CMD="$CMD --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --rpcapi db,eth,net,web3,personal --rpcvhosts '*'"
+    CMD="$CMD --ws --wsaddr 0.0.0.0 --wsport 8546 --wsapi db,eth,net,web3,personal --wsorigins '*'"
 
     # Add Ethstats to run
     if [ ! -z "$ETHSTATSLABEL" ] && [ ! -z "$ETHSTATSLOGIN" ]; then
