ed9cb631fd94ae4cdb2436a5181fcb6b917a7b83
Jake Pospischil <moles1@gmail.com>
2019-07-30 12:56:08 +1000

    updated CLI prompt function for testing

--- shared/utils/cli/prompt.go
+++ shared/utils/cli/prompt.go
@@ -3,6 +3,7 @@ package cli
 import (
     "bufio"
     "fmt"
+    "io"
     "os"
     "regexp"
 )
@@ -14,14 +15,25 @@ func Prompt(input *os.File, initialPrompt string, expectedFormat string, incorre
     // Read from stdin by default
     if input == nil { input = os.Stdin }
 
+    // Get initial offset
+    var offset int64 = 0
+    if input != os.Stdin { offset, _ = input.Seek(0, io.SeekCurrent) }
+
     // Print initial prompt
     fmt.Println(initialPrompt)
 
-    // Get valid user input
+    // Get valid user input, increment offset
     scanner := bufio.NewScanner(input)
-    for scanner.Scan(); !regexp.MustCompile(expectedFormat).MatchString(scanner.Text()); scanner.Scan() {
+    scanner.Scan()
+    offset += int64(len(scanner.Bytes()) + 1)
+    for !regexp.MustCompile(expectedFormat).MatchString(scanner.Text()) {
         fmt.Println(incorrectFormatPrompt)
+        scanner.Scan()
+        offset += int64(len(scanner.Bytes()) + 1)
     }
+
+    // Seek input to offset
+    if input != os.Stdin { input.Seek(offset, io.SeekStart) }
 
     // Return user input
     return scanner.Text()
