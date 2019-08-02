3f0f5229d51eabb1361f3902a921291b852ea109
Jake Pospischil <moles1@gmail.com>
2019-08-02 10:17:21 +1000

    implementing node service unit tests

--- tests/shared/services/rocketpool/node/info_test.go
+++ tests/shared/services/rocketpool/node/info_test.go
@@ -127,7 +127,58 @@ func TestGetRequiredBalances(t *testing.T) {
 
     // Check required balances
     expectedEth := eth.EthToWei(16)
-    if balances.EtherWei.String() != expectedEth.String() { t.Errorf("Incorrect required balance ETH value: expected %s, got %s", expectedEth.String(), balances.EtherWei.String()) }
+    if balances.EtherWei.String() != expectedEth.String() { t.Errorf("Incorrect required ETH value: expected %s, got %s", expectedEth.String(), balances.EtherWei.String()) }
+
+}
+
+
+// Test node deposit reservation details getter
+func TestGetReservationDetails(t *testing.T) {
+
+    // Create account manager
+    am, err := test.NewInitAccountManager("foobarbaz")
+    if err != nil { t.Fatal(err) }
+
+    // Create key manager
+    km, err := test.NewInitKeyManager("foobarbaz")
+    if err != nil { t.Fatal(err) }
+
+    // Initialise ethereum client
+    client, err := ethclient.Dial(test.POW_PROVIDER_URL)
+    if err != nil { t.Fatal(err) }
+
+    // Initialise contract manager & load contracts / ABIs
+    cm, err := rocketpool.NewContractManager(client, test.ROCKET_STORAGE_ADDRESS)
+    if err != nil { t.Fatal(err) }
+    if err := cm.LoadContracts([]string{"rocketNodeAPI", "rocketNodeSettings"}); err != nil { t.Fatal(err) }
+    if err := cm.LoadABIs([]string{"rocketNodeContract"}); err != nil { t.Fatal(err) }
+
+    // Register node
+    nodeContract, nodeContractAddress, err := rp.RegisterNode(client, cm, am)
+    if err != nil { t.Fatal(err) }
+
+    // Get reservation details before deposit reservation
+    details, err := node.GetReservationDetails(nodeContract, cm)
+    if err != nil { t.Fatal(err) }
+
+    // Check reservation details
+    if details.Exists { t.Errorf("Incorrect deposit exists value: expected %t, got %t", false, details.Exists) }
+    if details.StakingDurationID != "" { t.Error("Staking duration ID should be undefined") }
+    if details.EtherRequiredWei != nil { t.Error("Required ETH value should be undefined") }
+    if details.RplRequiredWei != nil { t.Error("Required ETH value should be undefined") }
+
+    // Reserve node deposit
+    if err := rp.ReserveNodeDeposit(client, cm, am, km, nodeContractAddress, "3m"); err != nil { t.Fatal(err) }
+
+    // Get reservation details
+    details, err = node.GetReservationDetails(nodeContract, cm)
+    if err != nil { t.Fatal(err) }
+
+    // Check reservation details
+    expectedEth := eth.EthToWei(16)
+    if !details.Exists { t.Errorf("Incorrect deposit exists value: expected %t, got %t", true, details.Exists) }
+    if details.StakingDurationID != "3m" { t.Errorf("Incorrect staking duration ID: expected %s, got %s", "3m", details.StakingDurationID) }
+    if details.EtherRequiredWei.String() != expectedEth.String() { t.Errorf("Incorrect required ETH value: expected %s, got %s", expectedEth.String(), details.EtherRequiredWei.String()) }
 
 }
 
