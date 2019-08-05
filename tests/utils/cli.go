d9bf81bc072f56ed1736ab8619e2e0beb211e753
Jake Pospischil <moles1@gmail.com>
2019-08-05 12:45:18 +1000

    implemented CLI test utils

---
+++ tests/utils/cli.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+    "gopkg.in/urfave/cli.v1"
+
+    "github.com/rocket-pool/smartnode/rocketpool-cli/deposit"
+    "github.com/rocket-pool/smartnode/rocketpool-cli/fee"
+    "github.com/rocket-pool/smartnode/rocketpool-cli/minipool"
+    "github.com/rocket-pool/smartnode/rocketpool-cli/node"
+    cliutils "github.com/rocket-pool/smartnode/shared/utils/cli"
+)
+
+
+// Create a test app
+func NewApp() *cli.App {
+
+    // Create app
+    app := cli.NewApp()
+
+    // Configure
+    cliutils.Configure(app)
+
+    // Register commands
+    deposit.RegisterCommands(app, "deposit", []string{"d"})
+    fee.RegisterCommands(app, "fee", []string{"f"})
+    minipool.RegisterCommands(app, "minipool", []string{"m"})
+    node.RegisterCommands(app, "node", []string{"n"})
+
+    // Return
+    return app
+
+}
+
+
+// Get CLI app args
+func AppArgs(inputPath string, dataPath string) []string {
+    return []string{
+        "rocketpool-cli",
+        "--database", dataPath + "/rocketpool.db",
+        "--password", dataPath + "/password",
+        "--keychainPow", dataPath + "/accounts",
+        "--keychainBeacon", dataPath + "/validators",
+        "--providerPow", POW_PROVIDER_URL,
+        "--providerBeacon", BEACON_PROVIDER_URL,
+        "--storageAddress", ROCKET_STORAGE_ADDRESS,
+        "--input", inputPath,
+    }
+}
+
--- tests/utils/config.go
+++ tests/utils/config.go
@@ -1,8 +1,11 @@
 package utils
 
 
-// Ethereum client config
+// POW client config
 const POW_PROVIDER_URL string = "http://127.0.0.1:8545"
 const ROCKET_STORAGE_ADDRESS string = "0x70a5F2eB9e4C003B105399b471DAeDbC8d00B1c5"
 const OWNER_PRIVATE_KEY string = "c6d2ac9b00bd599c4ce9d3a69c91e496eb9e79781d9dc84c79bafa7618f45f37"
+
+// Beacon client config
+const BEACON_PROVIDER_URL string = "ws://127.0.0.1:9545"
 
