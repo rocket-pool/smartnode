b6dd04f762ddc98db84504d1225023bcf437fdd0
Jake Pospischil <moles1@gmail.com>
2019-08-05 15:30:39 +1000

    added node init command unit tests

--- tests/rocketpool-cli/node/init_test.go
+++ tests/rocketpool-cli/node/init_test.go
@@ -1,8 +1,11 @@
 package node
 
 import (
+    "bufio"
     "io/ioutil"
     "testing"
+    "os"
+    "regexp"
 
     test "github.com/rocket-pool/smartnode/tests/utils"
 )
@@ -34,10 +37,23 @@ func TestInitNode(t *testing.T) {
     // Initialise node
     if err := app.Run(append(args, "node", "init")); err != nil { t.Error(err) }
 
-    // Attempt to initialise when already initialised
+    // Attempt to initialise node again
     if err := app.Run(append(args, "node", "init")); err != nil { t.Error(err) }
 
-    t.Error("!")
+    // Read & check output
+    output, err = os.Open(output.Name())
+    if err != nil { t.Fatal(err) }
+    line := 0
+    for scanner := bufio.NewScanner(output); scanner.Scan(); {
+        switch line {
+            case 0: if !regexp.MustCompile("(?i)^Please enter a node password").MatchString(scanner.Text()) { t.Error("Password prompt message incorrect") }
+            case 1: if !regexp.MustCompile("(?i)^Node password set successfully:").MatchString(scanner.Text()) { t.Error("Node password set message incorrect") }
+            case 2: if !regexp.MustCompile("(?i)^Node account created successfully:").MatchString(scanner.Text()) { t.Error("Node account created message incorrect") }
+            case 3: if !regexp.MustCompile("(?i)^Node password already set.").MatchString(scanner.Text()) { t.Error("Node password already set message incorrect") }
+            case 4: if !regexp.MustCompile("(?i)^Node account already exists:").MatchString(scanner.Text()) { t.Error("Node account already exists message incorrect") }
+        }
+        line++
+    }
 
 }
 
