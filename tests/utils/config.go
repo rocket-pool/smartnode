7e39e7015bdd5126255a4e109ac7c50b20c2ae3b
Jake Pospischil <moles1@gmail.com>
2019-08-15 11:29:19 +1000

    added docker test config

--- tests/utils/config.go
+++ tests/utils/config.go
@@ -1,6 +1,9 @@
 package utils
 
 
+// Docker config
+const IMAGE_VERSION string = "v0.0.1"
+
 // POW client config
 const POW_PROVIDER_URL string = "http://127.0.0.1:8545"
 const ROCKET_STORAGE_ADDRESS string = "0x70a5F2eB9e4C003B105399b471DAeDbC8d00B1c5"
