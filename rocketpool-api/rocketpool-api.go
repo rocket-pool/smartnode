77bb12f6b499140da48fcde13b252fe9b1d206f2
Jake Pospischil <moles1@gmail.com>
2019-10-24 13:24:50 +1000

    initial api app setup

---
+++ rocketpool-api/rocketpool-api.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "os"
+
+    "github.com/urfave/cli"
+
+    "github.com/rocket-pool/smartnode/shared/utils/api"
+    cliutils "github.com/rocket-pool/smartnode/shared/utils/cli"
+)
+
+
+// Run
+func main() {
+
+    // Initialise application
+    app := cli.NewApp()
+
+    // Set application info
+    app.Name = "rocketpool-api"
+    app.Usage = "Rocket Pool node API"
+    app.Version = "0.0.1"
+    app.Authors = []cli.Author{
+        cli.Author{
+            Name:  "David Rugendyke",
+            Email: "david@rocketpool.net",
+        },
+        cli.Author{
+            Name:  "Jake Pospischil",
+            Email: "jake@rocketpool.net",
+        },
+    }
+    app.Copyright = "(c) 2019 Rocket Pool Pty Ltd"
+
+    // Configure application
+    cliutils.Configure(app)
+
+    // Run application
+    if err := app.Run(os.Args); err != nil {
+        api.PrintErrorResponse(nil, err)
+    }
+
+}
+
