3ce30d2464576e1c5bfe746f2aef13f6de48d698
Jake Pospischil <moles1@gmail.com>
2019-10-30 12:29:03 +1000

    moving api condition checks to separate methods

--- shared/api/node/timezone.go
+++ shared/api/node/timezone.go
@@ -23,9 +23,6 @@ type NodeTimezoneResponse struct {
 // Set node timezone
 func SetNodeTimezone(p *services.Provider, timezone string) (*NodeTimezoneResponse, error) {
 
-    // Response
-    response := &NodeTimezoneResponse{}
-
     // Get node account
     nodeAccount, _ := p.AM.GetNodeAccount()
 
@@ -35,8 +32,6 @@ func SetNodeTimezone(p *services.Provider, timezone string) (*NodeTimezoneRespon
     } else {
         if _, err := eth.ExecuteContractTransaction(p.Client, txor, p.CM.Addresses["rocketNodeAPI"], p.CM.Abis["rocketNodeAPI"], "setTimezoneLocation", timezone); err != nil {
             return nil, errors.New("Error setting node timezone: " + err.Error())
-        } else {
-            response.Success = true
         }
     }
 
@@ -44,12 +39,13 @@ func SetNodeTimezone(p *services.Provider, timezone string) (*NodeTimezoneRespon
     nodeTimezone := new(string)
     if err := p.CM.Contracts["rocketNodeAPI"].Call(nil, nodeTimezone, "getTimezoneLocation", nodeAccount.Address); err != nil {
         return nil, errors.New("Error retrieving node timezone: " + err.Error())
-    } else {
-        response.Timezone = *nodeTimezone
     }
 
     // Return response
-    return response, nil
+    return &NodeTimezoneResponse{
+        Success: true,
+        Timezone: *nodeTimezone,
+    }, nil
 
 }
 
