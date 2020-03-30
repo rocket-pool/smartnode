a180f6ef5c1213e2fb2c5beea8e138a098b5a169
Jake Pospischil <moles1@gmail.com>
2020-03-30 16:04:49 +1000

    implementing composer bin

---
+++ rocketpool-composer/rocketpool-composer.go
@@ -0,0 +1,56 @@
+package main
+
+import(
+    "fmt"
+    "log"
+    "os"
+
+    "github.com/urfave/cli"
+
+    "github.com/rocket-pool/smartnode/rocketpool-composer/service"
+)
+
+
+// Run
+func main() {
+
+    // Add logo to application help template
+    cli.AppHelpTemplate = fmt.Sprintf(`
+______           _        _    ______           _ 
+| ___ \         | |      | |   | ___ \         | |
+| |_/ /___   ___| | _____| |_  | |_/ /__   ___ | |
+|    // _ \ / __| |/ / _ \ __| |  __/ _ \ / _ \| |
+| |\ \ (_) | (__|   <  __/ |_  | | | (_) | (_) | |
+\_| \_\___/ \___|_|\_\___|\__| \_|  \___/ \___/|_|
+
+%s`, cli.AppHelpTemplate)
+
+    // Initialise application
+    app := cli.NewApp()
+
+    // Set application info
+    app.Name = "rocketpool"
+    app.Usage = "Rocket Pool CLI"
+    app.Version = "0.0.1"
+    app.Authors = []cli.Author{
+        cli.Author{
+            Name:  "David Rugendyke",
+            Email: "david@rocketpool.net",
+        },
+        cli.Author{
+            Name:  "Jake Pospischil",
+            Email: "jake@rocketpool.net",
+        },
+    }
+    app.Copyright = "(c) 2020 Rocket Pool Pty Ltd"
+
+    // Register commands
+    service.RegisterCommands(app, "service", []string{"s"})
+
+    // Run application
+    if err := app.Run(os.Args); err != nil {
+        log.Fatal(err)
+    }
+
+}
+
