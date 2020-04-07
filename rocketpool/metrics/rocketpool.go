ff61f0c544e56006307a7662678cfc8d3283200e
Jake Pospischil <moles1@gmail.com>
2020-04-07 13:41:40 +1000

    implementing RP metrics

--- rocketpool/metrics/rocketpool.go
+++ rocketpool/metrics/rocketpool.go
@@ -20,6 +20,8 @@ import (
 type RocketPoolMetricsProcess struct {
     p                       *services.Provider
     stakingDurationEnabled  map[string]prometheus.Gauge
+    networkEthCapacity      map[string]prometheus.Gauge
+    networkEthAssigned      map[string]prometheus.Gauge
     networkUtilisation      map[string]prometheus.Gauge
     rplRatio                map[string]prometheus.Gauge
 
@@ -29,6 +31,8 @@ type RocketPoolMetricsProcess struct {
 // Staking duration metrics
 type StakingDurationMetrics struct {
     DurationId string
+    NetworkEthCapacity float64
+    NetworkEthAssigned float64
     NetworkUtilisation float64
     RplRatio float64
 }
@@ -41,6 +45,8 @@ func StartRocketPoolMetricsProcess(p *services.Provider) {
     process := &RocketPoolMetricsProcess{
         p:                      p,
         stakingDurationEnabled: make(map[string]prometheus.Gauge),
+        networkEthCapacity:     make(map[string]prometheus.Gauge),
+        networkEthAssigned:     make(map[string]prometheus.Gauge),
         networkUtilisation:     make(map[string]prometheus.Gauge),
         rplRatio:               make(map[string]prometheus.Gauge),
     }
@@ -123,6 +129,28 @@ func (p *RocketPoolMetricsProcess) update() {
             p.stakingDurationEnabled[duration.Id].Set(0)
         }
 
+        // Network ETH capacity
+        if _, ok := p.networkEthCapacity[duration.Id]; !ok {
+            p.networkEthCapacity[duration.Id] = promauto.NewGauge(prometheus.GaugeOpts{
+                Namespace:  "smartnode",
+                Subsystem:  "rocketpool",
+                Name:       fmt.Sprintf("network_eth_capacity_%s", duration.Id),
+                Help:       fmt.Sprintf("The network ETH capacity for the '%s' queue", duration.Id),
+            })
+        }
+        p.networkEthCapacity[duration.Id].Set(metrics.NetworkEthCapacity)
+
+        // Network ETH assigned
+        if _, ok := p.networkEthAssigned[duration.Id]; !ok {
+            p.networkEthAssigned[duration.Id] = promauto.NewGauge(prometheus.GaugeOpts{
+                Namespace:  "smartnode",
+                Subsystem:  "rocketpool",
+                Name:       fmt.Sprintf("network_eth_assigned_%s", duration.Id),
+                Help:       fmt.Sprintf("The network ETH assigned for the '%s' queue", duration.Id),
+            })
+        }
+        p.networkEthAssigned[duration.Id].Set(metrics.NetworkEthAssigned)
+
         // Network utilisation
         if _, ok := p.networkUtilisation[duration.Id]; !ok {
             p.networkUtilisation[duration.Id] = promauto.NewGauge(prometheus.GaugeOpts{
@@ -154,9 +182,31 @@ func (p *RocketPoolMetricsProcess) update() {
 func getStakingDurationMetrics(cm *rocketpool.ContractManager, durationId string) (*StakingDurationMetrics, error) {
 
     // Data channels
+    networkEthCapacityChannel := make(chan float64)
+    networkEthAssignedChannel := make(chan float64)
     networkUtilisationChannel := make(chan float64)
     rplRatioChannel := make(chan float64)
     errorChannel := make(chan error)
+
+    // Get network ETH capacity
+    go (func() {
+        networkEthCapacity := new(*big.Int)
+        if err := cm.Contracts["rocketPool"].Call(nil, networkEthCapacity, "getTotalEther", "capacity", durationId); err != nil {
+            errorChannel <- errors.New("Error retrieving network ETH capacity: " + err.Error())
+        } else {
+            networkEthCapacityChannel <- eth.WeiToEth(*networkEthCapacity)
+        }
+    })()
+
+    // Get network ETH assigned
+    go (func() {
+        networkEthAssigned := new(*big.Int)
+        if err := cm.Contracts["rocketPool"].Call(nil, networkEthAssigned, "getTotalEther", "assigned", durationId); err != nil {
+            errorChannel <- errors.New("Error retrieving network ETH assigned: " + err.Error())
+        } else {
+            networkEthAssignedChannel <- eth.WeiToEth(*networkEthAssigned)
+        }
+    })()
 
     // Get network utilisation
     go (func() {
@@ -179,10 +229,16 @@ func getStakingDurationMetrics(cm *rocketpool.ContractManager, durationId string
     })()
 
     // Receive data
+    var networkEthCapacity float64
+    var networkEthAssigned float64
     var networkUtilisation float64
     var rplRatio float64
-    for received := 0; received < 2; {
+    for received := 0; received < 4; {
         select {
+            case networkEthCapacity = <-networkEthCapacityChannel:
+                received++
+            case networkEthAssigned = <-networkEthAssignedChannel:
+                received++
             case networkUtilisation = <-networkUtilisationChannel:
                 received++
             case rplRatio = <-rplRatioChannel:
@@ -195,6 +251,8 @@ func getStakingDurationMetrics(cm *rocketpool.ContractManager, durationId string
     // Return
     return &StakingDurationMetrics{
         DurationId: durationId,
+        NetworkEthCapacity: networkEthCapacity,
+        NetworkEthAssigned: networkEthAssigned,
         NetworkUtilisation: networkUtilisation,
         RplRatio: rplRatio,
     }, nil
