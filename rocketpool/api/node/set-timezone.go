844dacd37616f97b9689270dcfca65fc994b0b8c
Jake Pospischil <moles1@gmail.com>
2020-07-16 12:56:39 +1000

    implementing API node commands

--- rocketpool/api/node/set-timezone.go
+++ rocketpool/api/node/set-timezone.go
@@ -3,13 +3,38 @@ package node
 import (
     "github.com/urfave/cli"
 
-    //"github.com/rocket-pool/smartnode/shared/services"
-    //types "github.com/rocket-pool/smartnode/shared/types/api"
-    //"github.com/rocket-pool/smartnode/shared/utils/api"
+    "github.com/rocket-pool/smartnode/shared/services"
+    types "github.com/rocket-pool/smartnode/shared/types/api"
+    "github.com/rocket-pool/smartnode/shared/utils/api"
 )
 
 
 func setTimezoneLocation(c *cli.Context, timezoneLocation string) error {
-    return nil
+
+    // Get services
+    if err := services.RequireNodeRegistered(c); err != nil { return err }
+    am, err := services.GetAccountManager(c)
+    if err != nil { return err }
+    rp, err := services.GetRocketPool(c)
+    if err != nil { return err }
+
+    // Get transactor
+    opts, err := am.GetNodeAccountTransactor()
+    if err != nil {
+        return api.PrintResponse(&types.SetNodeTimezoneResponse{
+            Error: err.Error(),
+        })
+    }
+
+    // Set timezone location
+    if _, err := node.SetTimezoneLocation(rp, timezoneLocation, opts); err != nil {
+        return api.PrintResponse(&types.SetNodeTimezoneResponse{
+            Error: err.Error(),
+        })
+    }
+
+    // Print response
+    return api.PrintResponse(&types.SetNodeTimezoneResponse{})
+
 }
 
