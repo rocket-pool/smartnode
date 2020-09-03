872195125cc19e245f0c63686f51d492ecab376f
Jake Pospischil <moles1@gmail.com>
2020-09-03 13:18:02 +1000

    implemented default random eth2 client selection

--- rocketpool-cli/config/config.go
+++ rocketpool-cli/config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
     "fmt"
+    "math/rand"
+    "time"
 
     "github.com/urfave/cli"
 
@@ -29,10 +31,10 @@ func configureService(c *cli.Context) error {
     userConfig := config.RocketPoolConfig{}
 
     // Configure chains
-    if err := configureChain(&(globalConfig.Chains.Eth1), &(userConfig.Chains.Eth1), "Eth 1.0"); err != nil {
+    if err := configureChain(&(globalConfig.Chains.Eth1), &(userConfig.Chains.Eth1), "Eth 1.0", false); err != nil {
         return err
     }
-    if err := configureChain(&(globalConfig.Chains.Eth2), &(userConfig.Chains.Eth2), "Eth 2.0"); err != nil {
+    if err := configureChain(&(globalConfig.Chains.Eth2), &(userConfig.Chains.Eth2), "Eth 2.0", true); err != nil {
         return err
     }
 
@@ -49,26 +51,38 @@ func configureService(c *cli.Context) error {
 
 
 // Configure a chain
-func configureChain(globalChain, userChain *config.Chain, chainName string) error {
+func configureChain(globalChain, userChain *config.Chain, chainName string, defaultRandomClient bool) error {
 
     // Check client options
     if len(globalChain.Client.Options) == 0 {
         return fmt.Errorf("There are no available %s client options", chainName)
     }
 
-    // Prompt for client
-    clientOptions := make([]string, len(globalChain.Client.Options))
-    for oi, option := range globalChain.Client.Options {
-        clientOptions[oi] = option.Name
+    // Prompt for random client selection
+    var randomClient bool
+    if defaultRandomClient {
+        randomClient = cliutils.Confirm(fmt.Sprintf("Would you like to run a random %s client (recommended)?", chainName))
     }
-    selected, clientName := cliutils.Select(fmt.Sprintf("Which %s client would you like to run?", chainName), clientOptions)
+
+    // Select client
+    var selected int
+    if randomClient {
+        rand.Seed(time.Now().UnixNano())
+        selected = rand.Intn(len(globalChain.Client.Options))
+    } else {
+        clientOptions := make([]string, len(globalChain.Client.Options))
+        for oi, option := range globalChain.Client.Options {
+            clientOptions[oi] = option.Name
+        }
+        selected, _ = cliutils.Select(fmt.Sprintf("Which %s client would you like to run?", chainName), clientOptions)
+    }
 
     // Set selected client
     globalChain.Client.Selected = globalChain.Client.Options[selected].ID
     userChain.Client.Selected = globalChain.Client.Options[selected].ID
 
     // Log
-    fmt.Printf("%s %s client selected.\n", clientName, chainName)
+    fmt.Printf("%s %s client selected.\n", globalChain.GetSelectedClient().Name, chainName)
     fmt.Println("")
 
     // Prompt for params
