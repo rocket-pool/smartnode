32a70f8229b1225d0de765044ba3a39f1bd774e1
Jake Pospischil <moles1@gmail.com>
2020-09-16 10:21:54 +1000

    added restart shell session notice to install command

--- rocketpool-cli/service/service.go
+++ rocketpool-cli/service/service.go
@@ -42,6 +42,9 @@ func installService(c *cli.Context) error {
     // Print success message & return
     fmt.Println("")
     fmt.Printf("The Rocket Pool service was successfully installed %s!\n", location)
+    if c.GlobalString("host") == "" {
+        fmt.Printf("Please restart your shell session to apply updated user permissions.")
+    }
     fmt.Println("Run 'rocketpool config' to configure the service before starting it.")
     return nil
 
