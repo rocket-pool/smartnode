ef2da5796ed7f99885a7926bc58d8954b9eadcdb
Jake Pospischil <moles1@gmail.com>
2020-10-23 17:37:49 +1000

    implementing voluntary exit support

--- rocketpool/api/minipool/exit.go
+++ rocketpool/api/minipool/exit.go
@@ -5,9 +5,11 @@ import (
     "github.com/rocket-pool/rocketpool-go/minipool"
     "github.com/rocket-pool/rocketpool-go/types"
     "github.com/urfave/cli"
+    eth2types "github.com/wealdtech/go-eth2-types/v2"
 
     "github.com/rocket-pool/smartnode/shared/services"
     "github.com/rocket-pool/smartnode/shared/types/api"
+    "github.com/rocket-pool/smartnode/shared/utils/validator"
 )
 
 
@@ -56,12 +58,62 @@ func exitMinipool(c *cli.Context, minipoolAddress common.Address) (*api.ExitMini
 
     // Get services
     if err := services.RequireNodeRegistered(c); err != nil { return nil, err }
+    if err := services.RequireBeaconClientSynced(c); err != nil { return nil, err }
+    w, err := services.GetWallet(c)
+    if err != nil { return nil, err }
+    rp, err := services.GetRocketPool(c)
+    if err != nil { return nil, err }
+    bc, err := services.GetBeaconClient(c)
+    if err != nil { return nil, err }
 
-    // Exit
-    // TODO: implement
+    // Response
+    response := api.ExitMinipoolResponse{}
+
+    // Get minipool validator pubkey
+    validatorPubkey, err := minipool.GetMinipoolPubkey(rp, minipoolAddress, nil)
+    if err != nil {
+        return nil, err
+    }
+
+    // Get validator private key
+    validatorKey, err := w.GetValidatorKeyByPubkey(validatorPubkey)
+    if err != nil {
+        return nil, err
+    }
+
+    // Get beacon head
+    head, err := bc.GetBeaconHead()
+    if err != nil {
+        return nil, err
+    }
+
+    // Get voluntary exit signature domain
+    signatureDomain, err := bc.GetDomainData(eth2types.DomainVoluntaryExit[:], head.Epoch)
+    if err != nil {
+        return nil, err
+    }
+
+    // Get validator index
+    validatorIndex, err := bc.GetValidatorIndex(validatorPubkey)
+    if err != nil {
+        return nil, err
+    }
+
+    // Get signed voluntary exit message
+    signature, err := validator.GetSignedExitMessage(validatorKey, validatorIndex, head.Epoch, signatureDomain)
+    if err != nil {
+        return nil, err
+    }
+
+    /*
+    // Broadcast voluntary exit message
+    if err := bc.ExitValidator(validatorIndex, head.Epoch, signature); err != nil {
+        return nil, err
+    }
+    */
 
     // Return response
-    return &api.ExitMinipoolResponse{}, nil
+    return &response, nil
 
 }
 
