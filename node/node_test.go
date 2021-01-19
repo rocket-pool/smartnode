597a12301511510cd3c9963f7a0f7cc0f06bd0a1
Jake Pospischil <moles1@gmail.com>
2021-01-19 15:01:53 +1000

    implementing node tests

--- node/node_test.go
+++ node/node_test.go
@@ -9,6 +9,7 @@ import (
     "github.com/ethereum/go-ethereum/ethclient"
 
     "github.com/rocket-pool/rocketpool-go/rocketpool"
+    "github.com/rocket-pool/rocketpool-go/utils/eth"
     "github.com/rocket-pool/rocketpool-go/utils/test"
     "github.com/rocket-pool/rocketpool-go/utils/test/accounts"
     "github.com/rocket-pool/rocketpool-go/utils/test/evm"
@@ -44,13 +45,40 @@ func TestMain(m *testing.M) {
 }
 
 
+func TestGetNodeDetails(t *testing.T) {
+
+    // State snapshotting
+    if err := evm.TakeSnapshot(); err != nil { t.Fatal(err) }
+    t.Cleanup(func() { if err := evm.RevertSnapshot(); err != nil { t.Fatal(err) } })
+
+    // Register node
+    timezoneLocation := "Australia/Brisbane"
+    if _, err := RegisterNode(rp, timezoneLocation, nodeAccount.GetTransactor()); err != nil {
+        t.Fatal(err)
+    }
+
+    // Get node details
+    if details, err := GetNodeDetails(rp, nodeAccount.Address, nil); err != nil {
+        t.Error(err)
+    } else {
+        if !details.Exists {
+            t.Error("Incorrect node exists status")
+        }
+        if details.TimezoneLocation != timezoneLocation {
+            t.Errorf("Incorrect node timezone location '%s'", details.TimezoneLocation)
+        }
+    }
+
+}
+
+
 func TestRegisterNode(t *testing.T) {
 
     // State snapshotting
     if err := evm.TakeSnapshot(); err != nil { t.Fatal(err) }
     t.Cleanup(func() { if err := evm.RevertSnapshot(); err != nil { t.Fatal(err) } })
 
-    // Get initial node exists status
+    // Check initial node exists status
     if exists, err := GetNodeExists(rp, nodeAccount.Address, nil); err != nil {
         t.Error(err)
     } else if exists {
@@ -58,24 +86,68 @@ func TestRegisterNode(t *testing.T) {
     }
 
     // Register node
-    nodeTimezoneLocation := "Australia/Brisbane"
-    if _, err := RegisterNode(rp, nodeTimezoneLocation, nodeAccount.GetTransactor()); err != nil {
+    if _, err := RegisterNode(rp, "Australia/Brisbane", nodeAccount.GetTransactor()); err != nil {
         t.Fatal(err)
     }
 
-    // Get updated node exists status
+    // Check updated node exists status
     if exists, err := GetNodeExists(rp, nodeAccount.Address, nil); err != nil {
         t.Error(err)
     } else if !exists {
         t.Error("Node did not exist after registration")
     }
 
-    // Get node timezone location
-    if timezoneLocation, err := GetNodeTimezoneLocation(rp, nodeAccount.Address, nil); err != nil {
-        t.Error(err)
-    } else if timezoneLocation != nodeTimezoneLocation {
-        t.Errorf("Incorrect node timezone location '%s'", timezoneLocation)
+}
+
+
+func TestSetTimezoneLocation(t *testing.T) {
+
+    // State snapshotting
+    if err := evm.TakeSnapshot(); err != nil { t.Fatal(err) }
+    t.Cleanup(func() { if err := evm.RevertSnapshot(); err != nil { t.Fatal(err) } })
+
+    // Register node
+    timezoneLocation := "Australia/Brisbane"
+    if _, err := RegisterNode(rp, timezoneLocation, nodeAccount.GetTransactor()); err != nil {
+        t.Fatal(err)
     }
+
+    // Set timezone
+    timezoneLocation = "Australia/Sydney"
+    if _, err := SetTimezoneLocation(rp, timezoneLocation, nodeAccount.GetTransactor()); err != nil {
+        t.Fatal(err)
+    }
+
+    // Check node timezone location
+    if nodeTimezoneLocation, err := GetNodeTimezoneLocation(rp, nodeAccount.Address, nil); err != nil {
+        t.Error(err)
+    } else if nodeTimezoneLocation != timezoneLocation {
+        t.Errorf("Incorrect node timezone location '%s'", nodeTimezoneLocation)
+    }
+
+}
+
+
+func TestDeposit(t *testing.T) {
+
+    // State snapshotting
+    if err := evm.TakeSnapshot(); err != nil { t.Fatal(err) }
+    t.Cleanup(func() { if err := evm.RevertSnapshot(); err != nil { t.Fatal(err) } })
+
+    // Register node
+    if _, err := RegisterNode(rp, "Australia/Brisbane", nodeAccount.GetTransactor()); err != nil {
+        t.Fatal(err)
+    }
+
+    // Deposit
+    opts := nodeAccount.GetTransactor()
+    opts.Value = eth.EthToWei(16)
+    if _, err := Deposit(rp, 0, opts); err != nil {
+        t.Fatal(err)
+    }
+
+    // Check deposit
+    // TODO: implement
 
 }
 
