26e2f23e6bd76a5cb71880c2de9d443ae5017cfa
Jake Pospischil <moles1@gmail.com>
2021-01-19 11:05:59 +1000

    implementing contract manager tests

--- rocketpool/rocketpool_test.go
+++ rocketpool/rocketpool_test.go
@@ -2,6 +2,7 @@ package rocketpool
 
 import (
     "bytes"
+    "encoding/json"
     "testing"
 
     "github.com/ethereum/go-ethereum/common"
@@ -66,6 +67,70 @@ func TestGetAddresses(t *testing.T) {
         for ai := 0; ai < len(addresses2); ai++ {
             if !bytes.Equal(addresses2[ai].Bytes(), addresses1[ai].Bytes()) {
                 t.Errorf("Cached contract address %d did not match original contract address", ai)
+            }
+        }
+    }
+
+}
+
+
+func TestGetABI(t *testing.T) {
+
+    // Setup
+    client, err := ethclient.Dial(test.Eth1ProviderAddress)
+    if err != nil { t.Fatal(err) }
+    rp, err := NewRocketPool(client, common.HexToAddress(test.RocketStorageAddress))
+    if err != nil { t.Fatal(err) }
+
+    // Get ABI
+    abi1, err := rp.GetABI("rocketDepositPool")
+    if err != nil {
+        t.Errorf("Could not get contract ABI: %s", err)
+    }
+
+    // Get cached ABI
+    abi2, err := rp.GetABI("rocketDepositPool")
+    if err != nil {
+        t.Errorf("Could not get cached contract ABI: %s", err)
+    } else {
+        abi2Json, err := json.Marshal(abi2)
+        if err != nil { t.Fatal(err) }
+        abi1Json, err := json.Marshal(abi1)
+        if err != nil { t.Fatal(err) }
+        if !bytes.Equal(abi2Json, abi1Json) {
+            t.Error("Cached contract ABI did not match original contract ABI")
+        }
+    }
+
+}
+
+
+func TestGetABIs(t *testing.T) {
+
+    // Setup
+    client, err := ethclient.Dial(test.Eth1ProviderAddress)
+    if err != nil { t.Fatal(err) }
+    rp, err := NewRocketPool(client, common.HexToAddress(test.RocketStorageAddress))
+    if err != nil { t.Fatal(err) }
+
+    // Get ABIs
+    abis1, err := rp.GetABIs("rocketNodeManager", "rocketNodeDeposit")
+    if err != nil {
+        t.Errorf("Could not get contract ABIs: %s", err)
+    }
+
+    // Get cached ABIs
+    abis2, err := rp.GetABIs("rocketNodeManager", "rocketNodeDeposit")
+    if err != nil {
+        t.Errorf("Could not get cached contract ABIs: %s", err)
+    } else {
+        for ai := 0; ai < len(abis2); ai++ {
+            abi2Json, err := json.Marshal(abis2[ai])
+            if err != nil { t.Fatal(err) }
+            abi1Json, err := json.Marshal(abis1[ai])
+            if err != nil { t.Fatal(err) }
+            if !bytes.Equal(abi2Json, abi1Json) {
+                t.Errorf("Cached contract ABI %d did not match original contract ABI", ai)
             }
         }
     }
