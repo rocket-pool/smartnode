a41f4cc656dadd209b3def956dd4920f8dc24be2
Jake Pospischil <moles1@gmail.com>
2021-01-19 10:43:25 +1000

    implementing contract manager tests

--- rocketpool/rocketpool_test.go
+++ rocketpool/rocketpool_test.go
@@ -13,11 +13,9 @@ import (
 
 func TestGetAddress(t *testing.T) {
 
-    // Initialize eth client
+    // Setup
     client, err := ethclient.Dial(test.Eth1ProviderAddress)
     if err != nil { t.Fatal(err) }
-
-    // Initialize contract manager
     rp, err := NewRocketPool(client, common.HexToAddress(test.RocketStorageAddress))
     if err != nil { t.Fatal(err) }
 
@@ -35,6 +33,41 @@ func TestGetAddress(t *testing.T) {
         t.Errorf("Could not get cached contract address: %s", err)
     } else if !bytes.Equal(address2.Bytes(), address1.Bytes()) {
         t.Error("Cached contract address did not match original contract address")
+    }
+
+}
+
+
+func TestGetAddresses(t *testing.T) {
+
+    // Setup
+    client, err := ethclient.Dial(test.Eth1ProviderAddress)
+    if err != nil { t.Fatal(err) }
+    rp, err := NewRocketPool(client, common.HexToAddress(test.RocketStorageAddress))
+    if err != nil { t.Fatal(err) }
+
+    // Get contract addresses
+    addresses1, err := rp.GetAddresses("rocketNodeManager", "rocketNodeDeposit")
+    if err != nil {
+        t.Errorf("Could not get contract addresses: %s", err)
+    } else {
+        for ai, address := range addresses1 {
+            if bytes.Equal(address.Bytes(), common.Address{}.Bytes()) {
+                t.Errorf("Contract address %d was not found", ai)
+            }
+        }
+    }
+
+    // Get cached contract addresses
+    addresses2, err := rp.GetAddresses("rocketNodeManager", "rocketNodeDeposit")
+    if err != nil {
+        t.Errorf("Could not get cached contract addresses: %s", err)
+    } else {
+        for ai := 0; ai < len(addresses2); ai++ {
+            if !bytes.Equal(addresses2[ai].Bytes(), addresses1[ai].Bytes()) {
+                t.Errorf("Cached contract address %d did not match original contract address", ai)
+            }
+        }
     }
 
 }
