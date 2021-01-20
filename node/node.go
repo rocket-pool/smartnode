37fed319c107b385f740470e6e33ab24386f9512
Jake Pospischil <moles1@gmail.com>
2021-01-20 16:45:33 +1000

    updating node module

--- node/node.go
+++ node/node.go
@@ -2,6 +2,7 @@ package node
 
 import (
     "fmt"
+    "math/big"
     "sync"
 
     "github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -15,11 +16,181 @@ import (
 )
 
 
+// Settings
+const (
+    NodeAddressBatchSize = 50
+    NodeDetailsBatchSize = 20
+)
+
+
 // Node details
 type NodeDetails struct {
+    Address common.Address      `json:"address"`
     Exists bool                 `json:"exists"`
     Trusted bool                `json:"trusted"`
     TimezoneLocation string     `json:"timezoneLocation"`
+}
+
+
+// Get all node details
+func GetNodes(rp *rocketpool.RocketPool, opts *bind.CallOpts) ([]NodeDetails, error) {
+
+    // Get node addresses
+    nodeAddresses, err := GetNodeAddresses(rp, opts)
+    if err != nil {
+        return []NodeDetails{}, err
+    }
+
+    // Load node details in batches
+    details := make([]NodeDetails, len(nodeAddresses))
+    for bsi := 0; bsi < len(nodeAddresses); bsi += NodeDetailsBatchSize {
+
+        // Get batch start & end index
+        nsi := bsi
+        nei := bsi + NodeDetailsBatchSize
+        if nei > len(nodeAddresses) { nei = len(nodeAddresses) }
+
+        // Load details
+        var wg errgroup.Group
+        for ni := nsi; ni < nei; ni++ {
+            ni := ni
+            wg.Go(func() error {
+                nodeAddress := nodeAddresses[ni]
+                nodeDetails, err := GetNodeDetails(rp, nodeAddress, opts)
+                if err == nil { details[ni] = nodeDetails }
+                return err
+            })
+        }
+        if err := wg.Wait(); err != nil {
+            return []NodeDetails{}, err
+        }
+
+    }
+
+    // Return
+    return details, nil
+
+}
+
+
+// Get all node addresses
+func GetNodeAddresses(rp *rocketpool.RocketPool, opts *bind.CallOpts) ([]common.Address, error) {
+
+    // Get node count
+    nodeCount, err := GetNodeCount(rp, opts)
+    if err != nil {
+        return []common.Address{}, err
+    }
+
+    // Load node addresses in batches
+    addresses := make([]common.Address, nodeCount)
+    for bsi := uint64(0); bsi < nodeCount; bsi += NodeAddressBatchSize {
+
+        // Get batch start & end index
+        nsi := bsi
+        nei := bsi + NodeAddressBatchSize
+        if nei > nodeCount { nei = nodeCount }
+
+        // Load addresses
+        var wg errgroup.Group
+        for ni := nsi; ni < nei; ni++ {
+            ni := ni
+            wg.Go(func() error {
+                address, err := GetNodeAt(rp, ni, opts)
+                if err == nil { addresses[ni] = address }
+                return err
+            })
+        }
+        if err := wg.Wait(); err != nil {
+            return []common.Address{}, err
+        }
+
+    }
+
+    // Return
+    return addresses, nil
+
+}
+
+
+// Get all trusted node details
+func GetTrustedNodes(rp *rocketpool.RocketPool, opts *bind.CallOpts) ([]NodeDetails, error) {
+
+    // Get trusted node addresses
+    trustedNodeAddresses, err := GetTrustedNodeAddresses(rp, opts)
+    if err != nil {
+        return []NodeDetails{}, err
+    }
+
+    // Load node details in batches
+    details := make([]NodeDetails, len(trustedNodeAddresses))
+    for bsi := 0; bsi < len(trustedNodeAddresses); bsi += NodeDetailsBatchSize {
+
+        // Get batch start & end index
+        nsi := bsi
+        nei := bsi + NodeDetailsBatchSize
+        if nei > len(trustedNodeAddresses) { nei = len(trustedNodeAddresses) }
+
+        // Load details
+        var wg errgroup.Group
+        for ni := nsi; ni < nei; ni++ {
+            ni := ni
+            wg.Go(func() error {
+                nodeAddress := trustedNodeAddresses[ni]
+                nodeDetails, err := GetNodeDetails(rp, nodeAddress, opts)
+                if err == nil { details[ni] = nodeDetails }
+                return err
+            })
+        }
+        if err := wg.Wait(); err != nil {
+            return []NodeDetails{}, err
+        }
+
+    }
+
+    // Return
+    return details, nil
+
+}
+
+
+// Get all trusted node addresses
+func GetTrustedNodeAddresses(rp *rocketpool.RocketPool, opts *bind.CallOpts) ([]common.Address, error) {
+
+    // Get trusted node count
+    trustedNodeCount, err := GetTrustedNodeCount(rp, opts)
+    if err != nil {
+        return []common.Address{}, err
+    }
+
+    // Load node addresses in batches
+    addresses := make([]common.Address, trustedNodeCount)
+    for bsi := uint64(0); bsi < trustedNodeCount; bsi += NodeAddressBatchSize {
+
+        // Get batch start & end index
+        nsi := bsi
+        nei := bsi + NodeAddressBatchSize
+        if nei > trustedNodeCount { nei = trustedNodeCount }
+
+        // Load addresses
+        var wg errgroup.Group
+        for ni := nsi; ni < nei; ni++ {
+            ni := ni
+            wg.Go(func() error {
+                address, err := GetTrustedNodeAt(rp, ni, opts)
+                if err == nil { addresses[ni] = address }
+                return err
+            })
+        }
+        if err := wg.Wait(); err != nil {
+            return []common.Address{}, err
+        }
+
+    }
+
+    // Return
+    return addresses, nil
+
 }
 
 
@@ -56,11 +227,68 @@ func GetNodeDetails(rp *rocketpool.RocketPool, nodeAddress common.Address, opts 
 
     // Return
     return NodeDetails{
+        Address: nodeAddress,
         Exists: exists,
         Trusted: trusted,
         TimezoneLocation: timezoneLocation,
     }, nil
 
+}
+
+
+// Get the number of nodes in the network
+func GetNodeCount(rp *rocketpool.RocketPool, opts *bind.CallOpts) (uint64, error) {
+    rocketNodeManager, err := getRocketNodeManager(rp)
+    if err != nil {
+        return 0, err
+    }
+    nodeCount := new(*big.Int)
+    if err := rocketNodeManager.Call(opts, nodeCount, "getNodeCount"); err != nil {
+        return 0, fmt.Errorf("Could not get node count: %w", err)
+    }
+    return (*nodeCount).Uint64(), nil
+}
+
+
+// Get a node address by index
+func GetNodeAt(rp *rocketpool.RocketPool, index uint64, opts *bind.CallOpts) (common.Address, error) {
+    rocketNodeManager, err := getRocketNodeManager(rp)
+    if err != nil {
+        return common.Address{}, err
+    }
+    nodeAddress := new(common.Address)
+    if err := rocketNodeManager.Call(opts, nodeAddress, "getNodeAt", big.NewInt(int64(index))); err != nil {
+        return common.Address{}, fmt.Errorf("Could not get node %d address: %w", index, err)
+    }
+    return *nodeAddress, nil
+}
+
+
+// Get the number of trusted nodes in the network
+func GetTrustedNodeCount(rp *rocketpool.RocketPool, opts *bind.CallOpts) (uint64, error) {
+    rocketNodeManager, err := getRocketNodeManager(rp)
+    if err != nil {
+        return 0, err
+    }
+    trustedNodeCount := new(*big.Int)
+    if err := rocketNodeManager.Call(opts, trustedNodeCount, "getTrustedNodeCount"); err != nil {
+        return 0, fmt.Errorf("Could not get trusted node count: %w", err)
+    }
+    return (*trustedNodeCount).Uint64(), nil
+}
+
+
+// Get a trusted node address by index
+func GetTrustedNodeAt(rp *rocketpool.RocketPool, index uint64, opts *bind.CallOpts) (common.Address, error) {
+    rocketNodeManager, err := getRocketNodeManager(rp)
+    if err != nil {
+        return common.Address{}, err
+    }
+    trustedNodeAddress := new(common.Address)
+    if err := rocketNodeManager.Call(opts, trustedNodeAddress, "getTrustedNodeAt", big.NewInt(int64(index))); err != nil {
+        return common.Address{}, fmt.Errorf("Could not get trusted node %d address: %w", index, err)
+    }
+    return *trustedNodeAddress, nil
 }
 
 
@@ -115,6 +343,20 @@ func RegisterNode(rp *rocketpool.RocketPool, timezoneLocation string, opts *bind
     txReceipt, err := contract.Transact(rp.Client, rocketNodeManager, opts, "registerNode", timezoneLocation)
     if err != nil {
         return nil, fmt.Errorf("Could not register node: %w", err)
+    }
+    return txReceipt, nil
+}
+
+
+// Set a node's trusted status
+func SetNodeTrusted(rp *rocketpool.RocketPool, nodeAddress common.Address, trusted bool, opts *bind.TransactOpts) (*types.Receipt, error) {
+    rocketNodeManager, err := getRocketNodeManager(rp)
+    if err != nil {
+        return nil, err
+    }
+    txReceipt, err := contract.Transact(rp.Client, rocketNodeManager, opts, "setNodeTrusted", nodeAddress, trusted)
+    if err != nil {
+        return nil, fmt.Errorf("Could not set node trusted status: %w", err)
     }
     return txReceipt, nil
 }
