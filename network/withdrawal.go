545867b60765aade5ddfaaa68f23ed7f27c33e18
Jake Pospischil <moles1@gmail.com>
2021-01-21 14:08:49 +1000

    updating network module

--- network/withdrawal.go
+++ network/withdrawal.go
@@ -12,6 +12,7 @@ import (
     "github.com/rocket-pool/rocketpool-go/rocketpool"
     rptypes "github.com/rocket-pool/rocketpool-go/types"
     "github.com/rocket-pool/rocketpool-go/utils/contract"
+    "github.com/rocket-pool/rocketpool-go/utils/eth"
 )
 
 
@@ -43,6 +44,34 @@ func GetWithdrawalCredentials(rp *rocketpool.RocketPool, opts *bind.CallOpts) (c
 }
 
 
+// Set the network validator withdrawal credentials
+func SetWithdrawalCredentials(rp *rocketpool.RocketPool, withdrawalCredentials common.Hash, opts *bind.TransactOpts) (*types.Receipt, error) {
+    rocketNetworkWithdrawal, err := getRocketNetworkWithdrawal(rp)
+    if err != nil {
+        return nil, err
+    }
+    txReceipt, err := contract.Transact(rp.Client, rocketNetworkWithdrawal, opts, "setWithdrawalCredentials", withdrawalCredentials)
+    if err != nil {
+        return nil, fmt.Errorf("Could not set network withdrawal credentials: %w", err)
+    }
+    return txReceipt, nil
+}
+
+
+// Transfer a validator balance to the withdrawal contract
+func TransferWithdrawal(rp *rocketpool.RocketPool, opts *bind.TransactOpts) (*types.Receipt, error) {
+    rocketNetworkWithdrawalAddress, err := getRocketNetworkWithdrawalAddress(rp)
+    if err != nil {
+        return nil, err
+    }
+    txReceipt, err := eth.SendTransaction(rp.Client, *rocketNetworkWithdrawalAddress, opts)
+    if err != nil {
+        return nil, fmt.Errorf("Could not transfer validator balance: %w", err)
+    }
+    return txReceipt, nil
+}
+
+
 // Process a validator withdrawal from the beacon chain
 func ProcessWithdrawal(rp *rocketpool.RocketPool, validatorPubkey rptypes.ValidatorPubkey, opts *bind.TransactOpts) (*types.Receipt, error) {
     rocketNetworkWithdrawal, err := getRocketNetworkWithdrawal(rp)
@@ -63,5 +92,11 @@ func getRocketNetworkWithdrawal(rp *rocketpool.RocketPool) (*bind.BoundContract,
     rocketNetworkWithdrawalLock.Lock()
     defer rocketNetworkWithdrawalLock.Unlock()
     return rp.GetContract("rocketNetworkWithdrawal")
+}
+var rocketNetworkWithdrawalAddressLock sync.Mutex
+func getRocketNetworkWithdrawalAddress(rp *rocketpool.RocketPool) (*common.Address, error) {
+    rocketNetworkWithdrawalAddressLock.Lock()
+    defer rocketNetworkWithdrawalAddressLock.Unlock()
+    return rp.GetAddress("rocketNetworkWithdrawal")
 }
 
