5829985ba0b3f3a96eb764c3231db2d78b9b5552
Jake Pospischil <moles1@gmail.com>
2021-01-22 14:09:38 +1000

    added tests validator utils

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/rocket-pool/rocketpool-go
 go 1.13
 
 require (
+	github.com/dgraph-io/ristretto v0.0.3 // indirect
 	github.com/ethereum/go-ethereum v1.9.15
+	github.com/ferranbt/fastssz v0.0.0-20210120143747-11b9eff30ea9 // indirect
+	github.com/minio/highwayhash v1.0.1 // indirect
+	github.com/protolambda/zssz v0.1.5 // indirect
+	github.com/prysmaticlabs/go-bitfield v0.0.0-20210121075346-fee7b721f342 // indirect
+	github.com/prysmaticlabs/go-ssz v0.0.0-20210121151755-f6208871c388
 	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f
 )
---
+++ tests/utils/validator/deposit-data.go
@@ -0,0 +1,52 @@
+package validator
+
+import (
+    "github.com/ethereum/go-ethereum/common"
+    "github.com/prysmaticlabs/go-ssz"
+
+    "github.com/rocket-pool/rocketpool-go/tests"
+    "github.com/rocket-pool/rocketpool-go/types"
+)
+
+
+// Deposit settings
+const depositAmount = 32000000000 // gwei
+
+
+// Deposit data
+type depositData struct {
+    PublicKey []byte                `ssz-size:"48"`
+    WithdrawalCredentials []byte    `ssz-size:"32"`
+    Amount uint64
+    Signature []byte                `ssz-size:"96"`
+}
+
+
+// Get the validator pubkey
+func GetValidatorPubkey() (types.ValidatorPubkey, error) {
+    return types.HexToValidatorPubkey(tests.ValidatorPubkey)
+}
+
+
+// Get the validator deposit signature
+func GetValidatorSignature() (types.ValidatorSignature, error) {
+    return types.HexToValidatorSignature(tests.ValidatorSignature)
+}
+
+
+// Get the withdrawal credentials
+func GetWithdrawalCredentials() common.Hash {
+    return common.HexToHash(tests.WithdrawalCredentials)
+}
+
+
+// Get the validator deposit depositDataRoot
+func GetDepositDataRoot(validatorPubkey types.ValidatorPubkey, withdrawalCredentials common.Hash, validatorSignature types.ValidatorSignature) (common.Hash, error) {
+    return ssz.HashTreeRoot(depositData{
+        PublicKey: validatorPubkey.Bytes(),
+        WithdrawalCredentials: withdrawalCredentials[:],
+        Amount: depositAmount,
+        Signature: validatorSignature.Bytes(),
+    })
+}
+
