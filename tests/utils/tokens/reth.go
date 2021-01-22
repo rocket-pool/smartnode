09422466dd01021ff9b9e6404e9756779fd292dc
Jake Pospischil <moles1@gmail.com>
2021-01-22 15:02:30 +1000

    implementing token helpers

--- tests/utils/tokens/reth.go
+++ tests/utils/tokens/reth.go
@@ -6,20 +6,21 @@ import (
     "github.com/rocket-pool/rocketpool-go/deposit"
     "github.com/rocket-pool/rocketpool-go/rocketpool"
     "github.com/rocket-pool/rocketpool-go/tests/utils/accounts"
-    //"github.com/rocket-pool/rocketpool-go/tokens"
+    "github.com/rocket-pool/rocketpool-go/tokens"
 )
 
 
 // Mint an amount of rETH to an account
 func MintRETH(rp *rocketpool.RocketPool, toAccount *accounts.Account, amount *big.Int) error {
 
-    // Get rETH exchange rate
-    // TODO: implement
+    // Get ETH value of amount
+    ethValue, err := tokens.GetETHValueOfRETH(rp, amount, nil)
+    if err != nil { return err }
 
     // Deposit from account to mint rETH
     opts := toAccount.GetTransactor()
-    opts.Value = amount
-    _, err := deposit.Deposit(rp, opts)
+    opts.Value = ethValue
+    _, err = deposit.Deposit(rp, opts)
     return err
 
 }
