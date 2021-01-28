3e7f94a02c361689f3398aa1d02d16b8fc2adf9e
Jake Pospischil <moles1@gmail.com>
2021-01-28 16:34:43 +1000

    implementing test helpers

---
+++ tests/utils/minipool/minipool.go
@@ -0,0 +1,68 @@
+package minipool
+
+import (
+    "errors"
+    "math/big"
+
+    "github.com/ethereum/go-ethereum/common"
+
+    "github.com/rocket-pool/rocketpool-go/minipool"
+    "github.com/rocket-pool/rocketpool-go/node"
+    "github.com/rocket-pool/rocketpool-go/rocketpool"
+    "github.com/rocket-pool/rocketpool-go/tests/utils/accounts"
+    "github.com/rocket-pool/rocketpool-go/tests/utils/validator"
+    "github.com/rocket-pool/rocketpool-go/utils/contract"
+)
+
+
+// Minipool created event
+type minipoolCreated struct {
+    Minipool common.Address
+    Node common.Address
+    Time *big.Int
+}
+
+
+// Create a minipool
+func CreateMinipool(rp *rocketpool.RocketPool, nodeAccount *accounts.Account, depositAmount *big.Int) (*minipool.Minipool, error) {
+
+    // Make node deposit
+    opts := nodeAccount.GetTransactor()
+    opts.Value = depositAmount
+    txReceipt, err := node.Deposit(rp, 0, opts)
+    if err != nil { return nil, err }
+
+    // Get minipool manager contract details
+    minipoolManagerAddress, minipoolManagerABI, err := contract.GetDetails(rp, "rocketMinipoolManager")
+    if err != nil { return nil, err }
+
+    // Get created minipool address
+    minipoolCreatedEvents, err := contract.GetTransactionEvents(rp.Client, minipoolManagerAddress, minipoolManagerABI, txReceipt, "MinipoolCreated", minipoolCreated{})
+    if err != nil || len(minipoolCreatedEvents) == 0 {
+        return nil, errors.New("Could not get minipool created event")
+    }
+    minipoolAddress := minipoolCreatedEvents[0].(minipoolCreated).Minipool
+
+    // Return minipool instance
+    return minipool.NewMinipool(rp, minipoolAddress)
+
+}
+
+
+// Stake a minipool
+func StakeMinipool(rp *rocketpool.RocketPool, mp *minipool.Minipool, nodeAccount *accounts.Account) error {
+
+    // Get validator data
+    validatorPubkey, err := validator.GetValidatorPubkey()
+    if err != nil { return err }
+    validatorSignature, err := validator.GetValidatorSignature()
+    if err != nil { return err }
+    depositDataRoot, err := validator.GetDepositDataRoot(validatorPubkey, validator.GetWithdrawalCredentials(), validatorSignature)
+    if err != nil { return err }
+
+    // Stake minipool & return
+    _, err = mp.Stake(validatorPubkey, validatorSignature, depositDataRoot, nodeAccount.GetTransactor())
+    return err
+
+}
+
--- tests/utils/tokens/neth.go
+++ tests/utils/tokens/neth.go
@@ -1,28 +1,16 @@
 package tokens
 
 import (
-    "errors"
     "math/big"
-
-    "github.com/ethereum/go-ethereum/common"
 
     "github.com/rocket-pool/rocketpool-go/minipool"
     "github.com/rocket-pool/rocketpool-go/node"
     "github.com/rocket-pool/rocketpool-go/rocketpool"
     "github.com/rocket-pool/rocketpool-go/settings"
     "github.com/rocket-pool/rocketpool-go/tests/utils/accounts"
-    "github.com/rocket-pool/rocketpool-go/tests/utils/validator"
-    "github.com/rocket-pool/rocketpool-go/utils/contract"
+    minipoolutils "github.com/rocket-pool/rocketpool-go/tests/utils/minipool"
     "github.com/rocket-pool/rocketpool-go/utils/eth"
 )
-
-
-// Minipool created event
-type minipoolCreated struct {
-    Minipool common.Address
-    Node common.Address
-    Time *big.Int
-}
 
 
 // Mint an amount of nETH to an account
@@ -33,31 +21,10 @@ func MintNETH(rp *rocketpool.RocketPool, ownerAccount *accounts.Account, nodeAcc
     if _, err := node.RegisterNode(rp, "Australia/Brisbane", nodeAccount.GetTransactor()); err != nil { return err }
     if _, err := node.SetNodeTrusted(rp, nodeAccount.Address, true, ownerAccount.GetTransactor()); err != nil { return err }
 
-    // Make node deposit
-    opts := toAccount.GetTransactor()
-    opts.Value = eth.EthToWei(32)
-    txReceipt, err := node.Deposit(rp, 0, opts)
+    // Create & stake minipool
+    mp, err := minipoolutils.CreateMinipool(rp, toAccount, eth.EthToWei(32))
     if err != nil { return err }
-
-    // Get created minipool address
-    minipoolManagerAddress, minipoolManagerABI, err := contract.GetDetails(rp, "rocketMinipoolManager")
-    if err != nil { return err }
-    minipoolCreatedEvents, err := contract.GetTransactionEvents(rp.Client, minipoolManagerAddress, minipoolManagerABI, txReceipt, "MinipoolCreated", minipoolCreated{})
-    if err != nil || len(minipoolCreatedEvents) == 0 {
-        return errors.New("Could not get minipool created event")
-    }
-    minipoolAddress := minipoolCreatedEvents[0].(minipoolCreated).Minipool
-
-    // Stake minipool
-    mp, err := minipool.NewMinipool(rp, minipoolAddress)
-    if err != nil { return err }
-    validatorPubkey, err := validator.GetValidatorPubkey()
-    if err != nil { return err }
-    validatorSignature, err := validator.GetValidatorSignature()
-    if err != nil { return err }
-    depositDataRoot, err := validator.GetDepositDataRoot(validatorPubkey, validator.GetWithdrawalCredentials(), validatorSignature)
-    if err != nil { return err }
-    if _, err := mp.Stake(validatorPubkey, validatorSignature, depositDataRoot, toAccount.GetTransactor()); err != nil { return err }
+    if err := minipoolutils.StakeMinipool(rp, mp, toAccount); err != nil { return err }
 
     // Disable minipool withdrawal delay
     withdrawalDelay, err := settings.GetMinipoolWithdrawalDelay(rp, nil)
@@ -65,7 +32,7 @@ func MintNETH(rp *rocketpool.RocketPool, ownerAccount *accounts.Account, nodeAcc
     if _, err := settings.SetMinipoolWithdrawalDelay(rp, 0, ownerAccount.GetTransactor()); err != nil { return err }
 
     // Mark minipool as withdrawable and withdraw
-    if _, err := minipool.SubmitMinipoolWithdrawable(rp, minipoolAddress, eth.EthToWei(32), amount, nodeAccount.GetTransactor()); err != nil { return err }
+    if _, err := minipool.SubmitMinipoolWithdrawable(rp, mp.Address, eth.EthToWei(32), amount, nodeAccount.GetTransactor()); err != nil { return err }
     if _, err := mp.Withdraw(toAccount.GetTransactor()); err != nil { return err }
 
     // Re-enable minipool withdrawal delay
