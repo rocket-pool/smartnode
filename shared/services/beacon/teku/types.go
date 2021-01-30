08f7a2f6459089087a53a36f29abcfb50cf3fb46
Joe Clapis <jclapis@outlook.com>
2021-01-30 17:24:49 -0500

    Initial Teku support

---
+++ shared/services/beacon/teku/types.go
@@ -0,0 +1,130 @@
+package teku
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"strconv"
+
+	hexutil "github.com/rocket-pool/smartnode/shared/utils/hex"
+)
+
+// Request types
+type VoluntaryExitRequest struct {
+	Message   VoluntaryExitMessage `json:"message"`
+	Signature byteArray            `json:"signature"`
+}
+type VoluntaryExitMessage struct {
+	Epoch          uinteger `json:"epoch"`
+	ValidatorIndex uinteger `json:"validator_index"`
+}
+
+// Response types
+type SyncStatusResponse struct {
+	Data struct {
+		HeadSlot     uinteger `json:"head_slot"`
+		SyncDistance uinteger `json:"sync_distance"`
+	} `json:"data"`
+}
+type Eth2ConfigResponse struct {
+	Data struct {
+		SecondsPerSlot uinteger `json:"SECONDS_PER_SLOT"`
+		SlotsPerEpoch  uinteger `json:"SLOTS_PER_EPOCH"`
+	} `json:"data"`
+}
+type GenesisResponse struct {
+	Data struct {
+		GenesisTime           uinteger  `json:"genesis_time"`
+		GenesisForkVersion    byteArray `json:"genesis_fork_version"`
+		GenesisValidatorsRoot byteArray `json:"genesis_validators_root"`
+	} `json:"data"`
+}
+type FinalityCheckpointsResponse struct {
+	Data struct {
+		PreviousJustified struct {
+			Epoch uinteger `json:"epoch"`
+		} `json:"previous_justified"`
+		CurrentJustified struct {
+			Epoch uinteger `json:"epoch"`
+		} `json:"current_justified"`
+		Finalized struct {
+			Epoch uinteger `json:"epoch"`
+		} `json:"finalized"`
+	} `json:"data"`
+}
+type ForkResponse struct {
+	Data struct {
+		PreviousVersion byteArray `json:"previous_version"`
+		CurrentVersion  byteArray `json:"current_version"`
+		Epoch           uinteger  `json:"epoch"`
+	} `json:"data"`
+}
+type ValidatorsResponse struct {
+	Data []Validator `json:"data"`
+}
+type Validator struct {
+	Index     uinteger `json:"index"`
+	Balance   uinteger `json:"balance"`
+	Status    string   `json:"status"`
+	Validator struct {
+		Pubkey                     byteArray `json:"pubkey"`
+		WithdrawalCredentials      byteArray `json:"withdrawal_credentials"`
+		EffectiveBalance           uinteger  `json:"effective_balance"`
+		Slashed                    bool      `json:"slashed"`
+		ActivationEligibilityEpoch uinteger  `json:"activation_eligibility_epoch"`
+		ActivationEpoch            uinteger  `json:"activation_epoch"`
+		ExitEpoch                  uinteger  `json:"exit_epoch"`
+		WithdrawableEpoch          uinteger  `json:"withdrawable_epoch"`
+	} `json:"validator"`
+}
+
+// Unsigned integer type
+type uinteger uint64
+
+func (i uinteger) MarshalJSON() ([]byte, error) {
+	return json.Marshal(strconv.Itoa(int(i)))
+}
+func (i *uinteger) UnmarshalJSON(data []byte) error {
+
+	// Unmarshal string
+	var dataStr string
+	if err := json.Unmarshal(data, &dataStr); err != nil {
+		return err
+	}
+
+	// Parse integer value
+	value, err := strconv.ParseUint(dataStr, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	// Set value and return
+	*i = uinteger(value)
+	return nil
+
+}
+
+// Byte array type
+type byteArray []byte
+
+func (b byteArray) MarshalJSON() ([]byte, error) {
+	return json.Marshal(hexutil.AddPrefix(hex.EncodeToString(b)))
+}
+func (b *byteArray) UnmarshalJSON(data []byte) error {
+
+	// Unmarshal string
+	var dataStr string
+	if err := json.Unmarshal(data, &dataStr); err != nil {
+		return err
+	}
+
+	// Decode hex
+	value, err := hex.DecodeString(hexutil.RemovePrefix(dataStr))
+	if err != nil {
+		return err
+	}
+
+	// Set value and return
+	*b = value
+	return nil
+
+}
--- shared/services/services.go
+++ shared/services/services.go
@@ -19,6 +19,7 @@ import (
     "github.com/rocket-pool/smartnode/shared/services/wallet"
     lhkeystore "github.com/rocket-pool/smartnode/shared/services/wallet/keystore/lighthouse"
     prkeystore "github.com/rocket-pool/smartnode/shared/services/wallet/keystore/prysm"
+    tkkeystore "github.com/rocket-pool/smartnode/shared/services/wallet/keystore/teku"
 )
 
 
@@ -140,8 +141,10 @@ func getWallet(cfg config.RocketPoolConfig, pm *passwords.PasswordManager) (*wal
         if err == nil {
             lighthouseKeystore := lhkeystore.NewKeystore(os.ExpandEnv(cfg.Smartnode.ValidatorKeychainPath), pm)
             prysmKeystore := prkeystore.NewKeystore(os.ExpandEnv(cfg.Smartnode.ValidatorKeychainPath), pm)
+            tekuKeystore := tkkeystore.NewKeystore(os.ExpandEnv(cfg.Smartnode.ValidatorKeychainPath), pm)
             nodeWallet.AddKeystore("lighthouse", lighthouseKeystore)
             nodeWallet.AddKeystore("prysm", prysmKeystore)
+            nodeWallet.AddKeystore("teku", tekuKeystore)
         }
     })
     return nodeWallet, err
@@ -174,6 +177,8 @@ func getBeaconClient(cfg config.RocketPoolConfig) (beacon.Client, error) {
                 beaconClient = lighthouse.NewClient(cfg.Chains.Eth2.Provider)
             case "prysm":
                 beaconClient, err = prysm.NewClient(cfg.Chains.Eth2.Provider)
+            case "teku":
+                beaconClient = teku.NewClient(cfg.Chains.Eth2.Provider)
             default:
                 err = fmt.Errorf("Unknown Eth 2.0 client '%s' selected", cfg.Chains.Eth2.Client.Selected)
         }
