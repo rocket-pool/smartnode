b442cf1135cc87b3b4b1f8c66ca287e3ece80fe1
Jake Pospischil <moles1@gmail.com>
2021-02-01 16:08:39 +1000

    implementing settings module unit tests

--- tests/settings/settings_test.go
+++ tests/settings/settings_test.go
@@ -104,3 +104,32 @@ func TestDepositSettings(t *testing.T) {
 
 }
 
+
+func TestNodeSettings(t *testing.T) {
+
+    // State snapshotting
+    if err := evm.TakeSnapshot(); err != nil { t.Fatal(err) }
+    t.Cleanup(func() { if err := evm.RevertSnapshot(); err != nil { t.Fatal(err) } })
+
+    // Set & get node registrations enabled
+    nodeRegistrationsEnabled := false
+    if _, err := settings.SetNodeRegistrationEnabled(rp, nodeRegistrationsEnabled, ownerAccount.GetTransactor()); err != nil {
+        t.Error(err)
+    } else if value, err := settings.GetNodeRegistrationEnabled(rp, nil); err != nil {
+        t.Error(err)
+    } else if value != nodeRegistrationsEnabled {
+        t.Error("Incorrect node registrations enabled enabled value")
+    }
+
+    // Set & get node deposits enabled
+    nodeDepositsEnabled := false
+    if _, err := settings.SetNodeDepositEnabled(rp, nodeDepositsEnabled, ownerAccount.GetTransactor()); err != nil {
+        t.Error(err)
+    } else if value, err := settings.GetNodeDepositEnabled(rp, nil); err != nil {
+        t.Error(err)
+    } else if value != nodeDepositsEnabled {
+        t.Error("Incorrect node deposits enabled enabled value")
+    }
+
+}
+
