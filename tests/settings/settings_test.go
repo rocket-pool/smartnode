4e6e6c5e40fac1ebc5417be54702cff530a4eac9
Jake Pospischil <moles1@gmail.com>
2021-02-01 15:53:32 +1000

    implementing settings module unit tests

---
+++ tests/settings/settings_test.go
@@ -0,0 +1,65 @@
+package rocketpool
+
+import (
+    "log"
+    "os"
+    "testing"
+
+    "github.com/ethereum/go-ethereum/common"
+    "github.com/ethereum/go-ethereum/ethclient"
+
+    "github.com/rocket-pool/rocketpool-go/rocketpool"
+    "github.com/rocket-pool/rocketpool-go/settings"
+    "github.com/rocket-pool/rocketpool-go/tests"
+    "github.com/rocket-pool/rocketpool-go/tests/utils/accounts"
+    "github.com/rocket-pool/rocketpool-go/tests/utils/evm"
+)
+
+
+var (
+    client *ethclient.Client
+    rp *rocketpool.RocketPool
+
+    ownerAccount *accounts.Account
+)
+
+
+func TestMain(m *testing.M) {
+    var err error
+
+    // Initialize eth client
+    client, err = ethclient.Dial(tests.Eth1ProviderAddress)
+    if err != nil { log.Fatal(err) }
+
+    // Initialize contract manager
+    rp, err = rocketpool.NewRocketPool(client, common.HexToAddress(tests.RocketStorageAddress))
+    if err != nil { log.Fatal(err) }
+
+    // Initialize accounts
+    ownerAccount, err = accounts.GetAccount(0)
+    if err != nil { log.Fatal(err) }
+
+    // Run tests
+    os.Exit(m.Run())
+
+}
+
+
+func TestDepositSettings(t *testing.T) {
+
+    // State snapshotting
+    if err := evm.TakeSnapshot(); err != nil { t.Fatal(err) }
+    t.Cleanup(func() { if err := evm.RevertSnapshot(); err != nil { t.Fatal(err) } })
+
+    // Set & get deposits enabled
+    depositEnabled := false
+    if _, err := settings.SetDepositEnabled(rp, depositEnabled, ownerAccount.GetTransactor()); err != nil {
+        t.Error(err)
+    } else if value, err := settings.GetDepositEnabled(rp, nil); err != nil {
+        t.Error(err)
+    } else if value != depositEnabled {
+        t.Error("Incorrect deposit enabled value")
+    }
+
+}
+
