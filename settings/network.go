065a42e63f80dd6202d5193637a8c3c89ebfdfa8
Jake Pospischil <moles1@gmail.com>
2021-02-01 15:33:06 +1000

    updating settings module

--- settings/network.go
+++ settings/network.go
@@ -6,6 +6,7 @@ import (
     "sync"
 
     "github.com/ethereum/go-ethereum/accounts/abi/bind"
+    "github.com/ethereum/go-ethereum/core/types"
 
     "github.com/rocket-pool/rocketpool-go/rocketpool"
     "github.com/rocket-pool/rocketpool-go/utils/eth"
@@ -24,6 +25,17 @@ func GetNodeConsensusThreshold(rp *rocketpool.RocketPool, opts *bind.CallOpts) (
     }
     return eth.WeiToEth(*value), nil
 }
+func SetNodeConsensusThreshold(rp *rocketpool.RocketPool, value float64, opts *bind.TransactOpts) (*types.Receipt, error) {
+    rocketNetworkSettings, err := getRocketNetworkSettings(rp)
+    if err != nil {
+        return nil, err
+    }
+    txReceipt, err := rocketNetworkSettings.Transact(opts, "setNodeConsensusThreshold", eth.EthToWei(value))
+    if err != nil {
+        return nil, fmt.Errorf("Could not set trusted node consensus threshold: %w", err)
+    }
+    return txReceipt, nil
+}
 
 
 // Network balance submissions currently enabled
@@ -37,6 +49,17 @@ func GetSubmitBalancesEnabled(rp *rocketpool.RocketPool, opts *bind.CallOpts) (b
         return false, fmt.Errorf("Could not get network balance submissions enabled status: %w", err)
     }
     return *value, nil
+}
+func SetSubmitBalancesEnabled(rp *rocketpool.RocketPool, value bool, opts *bind.TransactOpts) (*types.Receipt, error) {
+    rocketNetworkSettings, err := getRocketNetworkSettings(rp)
+    if err != nil {
+        return nil, err
+    }
+    txReceipt, err := rocketNetworkSettings.Transact(opts, "setSubmitBalancesEnabled", value)
+    if err != nil {
+        return nil, fmt.Errorf("Could not set network balance submissions enabled status: %w", err)
+    }
+    return txReceipt, nil
 }
 
 
@@ -52,6 +75,17 @@ func GetSubmitBalancesFrequency(rp *rocketpool.RocketPool, opts *bind.CallOpts) 
     }
     return (*value).Uint64(), nil
 }
+func SetSubmitBalancesFrequency(rp *rocketpool.RocketPool, value uint64, opts *bind.TransactOpts) (*types.Receipt, error) {
+    rocketNetworkSettings, err := getRocketNetworkSettings(rp)
+    if err != nil {
+        return nil, err
+    }
+    txReceipt, err := rocketNetworkSettings.Transact(opts, "setSubmitBalancesFrequency", big.NewInt(value))
+    if err != nil {
+        return nil, fmt.Errorf("Could not set network balance submission frequency: %w", err)
+    }
+    return txReceipt, nil
+}
 
 
 // Processing validator withdrawals currently enabled
@@ -66,9 +100,20 @@ func GetProcessWithdrawalsEnabled(rp *rocketpool.RocketPool, opts *bind.CallOpts
     }
     return *value, nil
 }
+func SetProcessWithdrawalsEnabled(rp *rocketpool.RocketPool, value bool, opts *bind.TransactOpts) (*types.Receipt, error) {
+    rocketNetworkSettings, err := getRocketNetworkSettings(rp)
+    if err != nil {
+        return nil, err
+    }
+    txReceipt, err := rocketNetworkSettings.Transact(opts, "setProcessWithdrawalsEnabled", value)
+    if err != nil {
+        return nil, fmt.Errorf("Could not set processing withdrawals enabled status: %w", err)
+    }
+    return txReceipt, nil
+}
 
 
-// Node commission rate parameters
+// Minimum node commission rate
 func GetMinimumNodeFee(rp *rocketpool.RocketPool, opts *bind.CallOpts) (float64, error) {
     rocketNetworkSettings, err := getRocketNetworkSettings(rp)
     if err != nil {
@@ -80,6 +125,20 @@ func GetMinimumNodeFee(rp *rocketpool.RocketPool, opts *bind.CallOpts) (float64,
     }
     return eth.WeiToEth(*value), nil
 }
+func SetMinimumNodeFee(rp *rocketpool.RocketPool, value float64, opts *bind.TransactOpts) (*types.Receipt, error) {
+    rocketNetworkSettings, err := getRocketNetworkSettings(rp)
+    if err != nil {
+        return nil, err
+    }
+    txReceipt, err := rocketNetworkSettings.Transact(opts, "setMinimumNodeFee", eth.EthToWei(value))
+    if err != nil {
+        return nil, fmt.Errorf("Could not set minimum node fee: %w", err)
+    }
+    return txReceipt, nil
+}
+
+
+// Target node commission rate
 func GetTargetNodeFee(rp *rocketpool.RocketPool, opts *bind.CallOpts) (float64, error) {
     rocketNetworkSettings, err := getRocketNetworkSettings(rp)
     if err != nil {
@@ -91,6 +150,20 @@ func GetTargetNodeFee(rp *rocketpool.RocketPool, opts *bind.CallOpts) (float64, 
     }
     return eth.WeiToEth(*value), nil
 }
+func SetTargetNodeFee(rp *rocketpool.RocketPool, value float64, opts *bind.TransactOpts) (*types.Receipt, error) {
+    rocketNetworkSettings, err := getRocketNetworkSettings(rp)
+    if err != nil {
+        return nil, err
+    }
+    txReceipt, err := rocketNetworkSettings.Transact(opts, "setTargetNodeFee", eth.EthToWei(value))
+    if err != nil {
+        return nil, fmt.Errorf("Could not set target node fee: %w", err)
+    }
+    return txReceipt, nil
+}
+
+
+// Maximum node commission rate
 func GetMaximumNodeFee(rp *rocketpool.RocketPool, opts *bind.CallOpts) (float64, error) {
     rocketNetworkSettings, err := getRocketNetworkSettings(rp)
     if err != nil {
@@ -102,6 +175,20 @@ func GetMaximumNodeFee(rp *rocketpool.RocketPool, opts *bind.CallOpts) (float64,
     }
     return eth.WeiToEth(*value), nil
 }
+func SetMaximumNodeFee(rp *rocketpool.RocketPool, value float64, opts *bind.TransactOpts) (*types.Receipt, error) {
+    rocketNetworkSettings, err := getRocketNetworkSettings(rp)
+    if err != nil {
+        return nil, err
+    }
+    txReceipt, err := rocketNetworkSettings.Transact(opts, "setMaximumNodeFee", eth.EthToWei(value))
+    if err != nil {
+        return nil, fmt.Errorf("Could not set maximum node fee: %w", err)
+    }
+    return txReceipt, nil
+}
+
+
+// The range of node demand values to base fee calculations on
 func GetNodeFeeDemandRange(rp *rocketpool.RocketPool, opts *bind.CallOpts) (*big.Int, error) {
     rocketNetworkSettings, err := getRocketNetworkSettings(rp)
     if err != nil {
@@ -112,6 +199,17 @@ func GetNodeFeeDemandRange(rp *rocketpool.RocketPool, opts *bind.CallOpts) (*big
         return nil, fmt.Errorf("Could not get node fee demand range: %w", err)
     }
     return *value, nil
+}
+func SetNodeFeeDemandRange(rp *rocketpool.RocketPool, value *big.Int, opts *bind.TransactOpts) (*types.Receipt, error) {
+    rocketNetworkSettings, err := getRocketNetworkSettings(rp)
+    if err != nil {
+        return nil, err
+    }
+    txReceipt, err := rocketNetworkSettings.Transact(opts, "setNodeFeeDemandRange", value)
+    if err != nil {
+        return nil, fmt.Errorf("Could not set node fee demand range: %w", err)
+    }
+    return txReceipt, nil
 }
 
 
@@ -126,6 +224,17 @@ func GetTargetRethCollateralRate(rp *rocketpool.RocketPool, opts *bind.CallOpts)
         return 0, fmt.Errorf("Could not get target rETH contract collateralization rate: %w", err)
     }
     return eth.WeiToEth(*value), nil
+}
+func SetTargetRethCollateralRate(rp *rocketpool.RocketPool, value float64, opts *bind.TransactOpts) (*types.Receipt, error) {
+    rocketNetworkSettings, err := getRocketNetworkSettings(rp)
+    if err != nil {
+        return nil, err
+    }
+    txReceipt, err := rocketNetworkSettings.Transact(opts, "setTargetRethCollateralRate", eth.EthToWei(value))
+    if err != nil {
+        return nil, fmt.Errorf("Could not set target rETH contract collateralization rate: %w", err)
+    }
+    return txReceipt, nil
 }
 
 
