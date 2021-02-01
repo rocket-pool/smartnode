ed0280a026a0f33458c8af3842058b57d6220d9d
Jake Pospischil <moles1@gmail.com>
2021-02-01 16:24:54 +1000

    implementing settings module unit tests

--- tests/settings/settings_test.go
+++ tests/settings/settings_test.go
@@ -111,6 +111,46 @@ func TestMinipoolSettings(t *testing.T) {
     if err := evm.TakeSnapshot(); err != nil { t.Fatal(err) }
     t.Cleanup(func() { if err := evm.RevertSnapshot(); err != nil { t.Fatal(err) } })
 
+    // Get & check launch balance and deposit amounts
+    fullMinipoolBalance := eth.EthToWei(32)
+    halfMinipoolBalance := eth.EthToWei(16)
+    emptyMinipoolBalance := eth.EthToWei(0)
+    if value, err := settings.GetMinipoolLaunchBalance(rp, nil); err != nil {
+        t.Error(err)
+    } else if value.Cmp(fullMinipoolBalance) != 0 {
+        t.Error("Incorrect minipool launch balance")
+    }
+    if value, err := settings.GetMinipoolFullDepositNodeAmount(rp, nil); err != nil {
+        t.Error(err)
+    } else if value.Cmp(fullMinipoolBalance) != 0 {
+        t.Error("Incorrect minipool full deposit node amount")
+    }
+    if value, err := settings.GetMinipoolHalfDepositNodeAmount(rp, nil); err != nil {
+        t.Error(err)
+    } else if value.Cmp(halfMinipoolBalance) != 0 {
+        t.Error("Incorrect minipool half deposit node amount")
+    }
+    if value, err := settings.GetMinipoolEmptyDepositNodeAmount(rp, nil); err != nil {
+        t.Error(err)
+    } else if value.Cmp(emptyMinipoolBalance) != 0 {
+        t.Error("Incorrect minipool empty deposit node amount")
+    }
+    if value, err := settings.GetMinipoolFullDepositUserAmount(rp, nil); err != nil {
+        t.Error(err)
+    } else if value.Cmp(halfMinipoolBalance) != 0 {
+        t.Error("Incorrect minipool full deposit user amount")
+    }
+    if value, err := settings.GetMinipoolHalfDepositUserAmount(rp, nil); err != nil {
+        t.Error(err)
+    } else if value.Cmp(halfMinipoolBalance) != 0 {
+        t.Error("Incorrect minipool half deposit user amount")
+    }
+    if value, err := settings.GetMinipoolEmptyDepositUserAmount(rp, nil); err != nil {
+        t.Error(err)
+    } else if value.Cmp(fullMinipoolBalance) != 0 {
+        t.Error("Incorrect minipool empty deposit user amount")
+    }
+
     // Set & get submit withdrawable enabled
     submitWithdrawableEnabled := false
     if _, err := settings.SetMinipoolSubmitWithdrawableEnabled(rp, submitWithdrawableEnabled, ownerAccount.GetTransactor()); err != nil {
