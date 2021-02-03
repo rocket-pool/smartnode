3f6c901992283ba91676dc09c86b6f5c46d732bb
Jake Pospischil <moles1@gmail.com>
2021-02-03 13:16:51 +1000

    implementing tokens module unit tests

--- tests/tokens/reth_test.go
+++ tests/tokens/reth_test.go
@@ -5,7 +5,9 @@ import (
 
     "github.com/ethereum/go-ethereum/common"
 
+    "github.com/rocket-pool/rocketpool-go/network"
     "github.com/rocket-pool/rocketpool-go/tests/utils/evm"
+    nodeutils "github.com/rocket-pool/rocketpool-go/tests/utils/node"
     tokenutils "github.com/rocket-pool/rocketpool-go/tests/utils/tokens"
     "github.com/rocket-pool/rocketpool-go/tokens"
     "github.com/rocket-pool/rocketpool-go/utils/eth"
@@ -104,6 +106,40 @@ func TestBurnRETH(t *testing.T) {
 }
 
 
-// TODO:
-// implement additional rETH unit tests
+func TestRETHExchangeRate(t *testing.T) {
+
+    // State snapshotting
+    if err := evm.TakeSnapshot(); err != nil { t.Fatal(err) }
+    t.Cleanup(func() { if err := evm.RevertSnapshot(); err != nil { t.Fatal(err) } })
+
+    // Register trusted node
+    if err := nodeutils.RegisterTrustedNode(rp, ownerAccount, trustedNodeAccount); err != nil { t.Fatal(err) }
+
+    // Submit network balances
+    if _, err := network.SubmitBalances(rp, 1, eth.EthToWei(100), eth.EthToWei(100), eth.EthToWei(50), trustedNodeAccount.GetTransactor()); err != nil { t.Fatal(err) }
+
+    // Get & check ETH value of rETH amount
+    rethAmount := eth.EthToWei(1)
+    if ethValue, err := tokens.GetETHValueOfRETH(rp, rethAmount, nil); err != nil {
+        t.Error(err)
+    } else if ethValue.Cmp(eth.EthToWei(2)) != 0 {
+        t.Errorf("Incorrect ETH value %s of rETH amount %s", ethValue.String(), rethAmount.String())
+    }
+
+    // Get & check rETH value of ETH amount
+    ethAmount := eth.EthToWei(2)
+    if rethValue, err := tokens.GetRETHValueOfETH(rp, ethAmount, nil); err != nil {
+        t.Error(err)
+    } else if rethValue.Cmp(eth.EthToWei(1)) != 0 {
+        t.Errorf("Incorrect rETH value %s of ETH amount %s", rethValue.String(), ethAmount.String())
+    }
+
+    // Get & check ETH : rETH exchange rate
+    if exchangeRate, err := tokens.GetRETHExchangeRate(rp, nil); err != nil {
+        t.Error(err)
+    } else if exchangeRate != 2 {
+        t.Errorf("Incorrect ETH : rETH exchange rate %f : 1", exchangeRate)
+    }
+
+}
 
