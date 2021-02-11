abc0bfdb8a77d55efe68b1bf74ff4be74e853bd3
Jake Pospischil <moles1@gmail.com>
2021-02-11 12:20:49 +1000

    updating existing unit tests

--- tests/testutils/node/node.go
+++ tests/testutils/node/node.go
@@ -1,6 +1,9 @@
 package node
 
 import (
+    "fmt"
+
+    "github.com/rocket-pool/rocketpool-go/dao/trustednode"
     "github.com/rocket-pool/rocketpool-go/node"
     "github.com/rocket-pool/rocketpool-go/rocketpool"
 
@@ -8,10 +11,15 @@ import (
 )
 
 
+// Trusted node counter
+var trustedNodeIndex = 0
+
+
 // Register a trusted node
 func RegisterTrustedNode(rp *rocketpool.RocketPool, ownerAccount *accounts.Account, trustedNodeAccount *accounts.Account) error {
     if _, err := node.RegisterNode(rp, "Australia/Brisbane", trustedNodeAccount.GetTransactor()); err != nil { return err }
-    if _, err := node.SetNodeTrusted(rp, trustedNodeAccount.Address, true, ownerAccount.GetTransactor()); err != nil { return err }
+    if _, err := trustednode.BootstrapMember(rp, fmt.Sprintf("tn%d", trustedNodeIndex), fmt.Sprintf("tn%d@rocketpool.net", trustedNodeIndex), trustedNodeAccount.Address, ownerAccount.GetTransactor()); err != nil { return err }
+    trustedNodeIndex++
     return nil
 }
 
