45712b744be1734f57a9928d254f0436ec126de4
Jake Pospischil <moles1@gmail.com>
2021-02-15 09:57:04 +1000

    implementing eth1 withdrawals changes

--- network/withdrawal.go
+++ network/withdrawal.go
@@ -2,7 +2,6 @@ package network
 
 import (
     "fmt"
-    "math/big"
     "sync"
 
     "github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -10,75 +9,32 @@ import (
     "github.com/ethereum/go-ethereum/core/types"
 
     "github.com/rocket-pool/rocketpool-go/rocketpool"
-    rptypes "github.com/rocket-pool/rocketpool-go/types"
 )
 
 
-// Get the withdrawal pool balance
-func GetWithdrawalBalance(rp *rocketpool.RocketPool, opts *bind.CallOpts) (*big.Int, error) {
+// Get the system withdrawal contract address
+func GetSystemWithdrawalContractAddress(rp *rocketpool.RocketPool, opts *bind.CallOpts) (common.Address, error) {
+    rocketNetworkWithdrawal, err := getRocketNetworkWithdrawal(rp)
+    if err != nil {
+        return common.Address{}, err
+    }
+    swcAddress := new(common.Address)
+    if err := rocketNetworkWithdrawal.Call(opts, swcAddress, "getSystemWithdrawalContractAddress"); err != nil {
+        return common.Address{}, fmt.Errorf("Could not get system withdrawal contract address: %w", err)
+    }
+    return *swcAddress, nil
+}
+
+
+// Set the system withdrawal contract address
+func SetSystemWithdrawalContractAddress(rp *rocketpool.RocketPool, swcAddress common.Address, opts *bind.TransactOpts) (*types.Receipt, error) {
     rocketNetworkWithdrawal, err := getRocketNetworkWithdrawal(rp)
     if err != nil {
         return nil, err
     }
-    balance := new(*big.Int)
-    if err := rocketNetworkWithdrawal.Call(opts, balance, "getBalance"); err != nil {
-        return nil, fmt.Errorf("Could not get withdrawal pool balance: %w", err)
-    }
-    return *balance, nil
-}
-
-
-// Get the current network validator withdrawal credentials
-func GetWithdrawalCredentials(rp *rocketpool.RocketPool, opts *bind.CallOpts) (common.Hash, error) {
-    rocketNetworkWithdrawal, err := getRocketNetworkWithdrawal(rp)
+    txReceipt, err := rocketNetworkWithdrawal.Transact(opts, "setSystemWithdrawalContractAddress", swcAddress)
     if err != nil {
-        return common.Hash{}, err
-    }
-    withdrawalCredentials := new(common.Hash)
-    if err := rocketNetworkWithdrawal.Call(opts, withdrawalCredentials, "getWithdrawalCredentials"); err != nil {
-        return common.Hash{}, fmt.Errorf("Could not get network withdrawal credentials: %w", err)
-    }
-    return *withdrawalCredentials, nil
-}
-
-
-// Set the network validator withdrawal credentials
-func SetWithdrawalCredentials(rp *rocketpool.RocketPool, withdrawalCredentials common.Hash, opts *bind.TransactOpts) (*types.Receipt, error) {
-    rocketNetworkWithdrawal, err := getRocketNetworkWithdrawal(rp)
-    if err != nil {
-        return nil, err
-    }
-    txReceipt, err := rocketNetworkWithdrawal.Transact(opts, "setWithdrawalCredentials", withdrawalCredentials.Bytes())
-    if err != nil {
-        return nil, fmt.Errorf("Could not set network withdrawal credentials: %w", err)
-    }
-    return txReceipt, nil
-}
-
-
-// Transfer a validator balance to the withdrawal contract
-func TransferWithdrawal(rp *rocketpool.RocketPool, opts *bind.TransactOpts) (*types.Receipt, error) {
-    rocketNetworkWithdrawal, err := getRocketNetworkWithdrawal(rp)
-    if err != nil {
-        return nil, err
-    }
-    txReceipt, err := rocketNetworkWithdrawal.Transfer(opts)
-    if err != nil {
-        return nil, fmt.Errorf("Could not transfer validator balance: %w", err)
-    }
-    return txReceipt, nil
-}
-
-
-// Process a validator withdrawal from the beacon chain
-func ProcessWithdrawal(rp *rocketpool.RocketPool, validatorPubkey rptypes.ValidatorPubkey, opts *bind.TransactOpts) (*types.Receipt, error) {
-    rocketNetworkWithdrawal, err := getRocketNetworkWithdrawal(rp)
-    if err != nil {
-        return nil, err
-    }
-    txReceipt, err := rocketNetworkWithdrawal.Transact(opts, "processWithdrawal", validatorPubkey[:])
-    if err != nil {
-        return nil, fmt.Errorf("Could not process validator %s withdrawal: %w", validatorPubkey.Hex(), err)
+        return nil, fmt.Errorf("Could not set system withdrawal contract address: %w", err)
     }
     return txReceipt, nil
 }
