f2e3ddb4e2b97836bc3040c37001d2a6233aef86
Jake Pospischil <moles1@gmail.com>
2021-02-16 09:24:11 +1000

    updating settings unit tests

--- tests/settings/trustednode/proposals_test.go
+++ tests/settings/trustednode/proposals_test.go
@@ -6,10 +6,12 @@ import (
     "github.com/rocket-pool/rocketpool-go/settings/trustednode"
 
     "github.com/rocket-pool/rocketpool-go/tests/testutils/evm"
+    daoutils "github.com/rocket-pool/rocketpool-go/tests/testutils/dao"
+    nodeutils "github.com/rocket-pool/rocketpool-go/tests/testutils/node"
 )
 
 
-func TestProposalsSettings(t *testing.T) {
+func TestBootstrapProposalsSettings(t *testing.T) {
 
     // State snapshotting
     if err := evm.TakeSnapshot(); err != nil { t.Fatal(err) }
@@ -26,13 +28,33 @@ func TestProposalsSettings(t *testing.T) {
     }
 
     // Set & get vote blocks
-    var voteBlocks uint64 = 1
+    var voteBlocks uint64 = 10
     if _, err := trustednode.BootstrapProposalVoteBlocks(rp, voteBlocks, ownerAccount.GetTransactor()); err != nil {
         t.Error(err)
     } else if value, err := trustednode.GetProposalVoteBlocks(rp, nil); err != nil {
         t.Error(err)
     } else if value != voteBlocks {
         t.Error("Incorrect vote blocks value")
+    }
+
+    // Set & get execute blocks
+    var executeBlocks uint64 = 10
+    if _, err := trustednode.BootstrapProposalExecuteBlocks(rp, executeBlocks, ownerAccount.GetTransactor()); err != nil {
+        t.Error(err)
+    } else if value, err := trustednode.GetProposalExecuteBlocks(rp, nil); err != nil {
+        t.Error(err)
+    } else if value != executeBlocks {
+        t.Error("Incorrect execute blocks value")
+    }
+
+    // Set & get action blocks
+    var actionBlocks uint64 = 10
+    if _, err := trustednode.BootstrapProposalActionBlocks(rp, actionBlocks, ownerAccount.GetTransactor()); err != nil {
+        t.Error(err)
+    } else if value, err := trustednode.GetProposalActionBlocks(rp, nil); err != nil {
+        t.Error(err)
+    } else if value != actionBlocks {
+        t.Error("Incorrect action blocks value")
     }
 
     // Set & get vote delay blocks
@@ -45,9 +67,47 @@ func TestProposalsSettings(t *testing.T) {
         t.Error("Incorrect vote delay blocks value")
     }
 
+}
+
+
+func TestProposeProposalsSettings(t *testing.T) {
+
+    // State snapshotting
+    if err := evm.TakeSnapshot(); err != nil { t.Fatal(err) }
+    t.Cleanup(func() { if err := evm.RevertSnapshot(); err != nil { t.Fatal(err) } })
+
+    // Register trusted node
+    if err := nodeutils.RegisterTrustedNode(rp, ownerAccount, trustedNodeAccount); err != nil { t.Fatal(err) }
+
+    // Set & get cooldown
+    var cooldown uint64 = 1000000
+    if proposalId, _, err := trustednode.ProposeProposalCooldown(rp, cooldown, trustedNodeAccount.GetTransactor()); err != nil {
+        t.Error(err)
+    } else if err := daoutils.PassAndExecuteProposal(rp, proposalId, trustedNodeAccount); err != nil {
+        t.Error(err)
+    } else if value, err := trustednode.GetProposalCooldown(rp, nil); err != nil {
+        t.Error(err)
+    } else if value != cooldown {
+        t.Error("Incorrect cooldown value")
+    }
+
+    // Set & get vote blocks
+    var voteBlocks uint64 = 10
+    if proposalId, _, err := trustednode.ProposeProposalVoteBlocks(rp, voteBlocks, trustedNodeAccount.GetTransactor()); err != nil {
+        t.Error(err)
+    } else if err := daoutils.PassAndExecuteProposal(rp, proposalId, trustedNodeAccount); err != nil {
+        t.Error(err)
+    } else if value, err := trustednode.GetProposalVoteBlocks(rp, nil); err != nil {
+        t.Error(err)
+    } else if value != voteBlocks {
+        t.Error("Incorrect vote blocks value")
+    }
+
     // Set & get execute blocks
-    var executeBlocks uint64 = 1
-    if _, err := trustednode.BootstrapProposalExecuteBlocks(rp, executeBlocks, ownerAccount.GetTransactor()); err != nil {
+    var executeBlocks uint64 = 10
+    if proposalId, _, err := trustednode.ProposeProposalExecuteBlocks(rp, executeBlocks, trustedNodeAccount.GetTransactor()); err != nil {
+        t.Error(err)
+    } else if err := daoutils.PassAndExecuteProposal(rp, proposalId, trustedNodeAccount); err != nil {
         t.Error(err)
     } else if value, err := trustednode.GetProposalExecuteBlocks(rp, nil); err != nil {
         t.Error(err)
@@ -56,13 +116,27 @@ func TestProposalsSettings(t *testing.T) {
     }
 
     // Set & get action blocks
-    var actionBlocks uint64 = 1
-    if _, err := trustednode.BootstrapProposalActionBlocks(rp, actionBlocks, ownerAccount.GetTransactor()); err != nil {
+    var actionBlocks uint64 = 10
+    if proposalId, _, err := trustednode.ProposeProposalActionBlocks(rp, actionBlocks, trustedNodeAccount.GetTransactor()); err != nil {
+        t.Error(err)
+    } else if err := daoutils.PassAndExecuteProposal(rp, proposalId, trustedNodeAccount); err != nil {
         t.Error(err)
     } else if value, err := trustednode.GetProposalActionBlocks(rp, nil); err != nil {
         t.Error(err)
     } else if value != actionBlocks {
         t.Error("Incorrect action blocks value")
+    }
+
+    // Set & get vote delay blocks
+    var voteDelayBlocks uint64 = 1000
+    if proposalId, _, err := trustednode.ProposeProposalVoteDelayBlocks(rp, voteDelayBlocks, trustedNodeAccount.GetTransactor()); err != nil {
+        t.Error(err)
+    } else if err := daoutils.PassAndExecuteProposal(rp, proposalId, trustedNodeAccount); err != nil {
+        t.Error(err)
+    } else if value, err := trustednode.GetProposalVoteDelayBlocks(rp, nil); err != nil {
+        t.Error(err)
+    } else if value != voteDelayBlocks {
+        t.Error("Incorrect vote delay blocks value")
     }
 
 }
--- tests/testutils/dao/proposals.go
+++ tests/testutils/dao/proposals.go
@@ -1,8 +1,9 @@
 package dao
 
 import (
-    "github.com/rocket-pool/rocketpool-go/dao/trustednode"
+    trustednodedao "github.com/rocket-pool/rocketpool-go/dao/trustednode"
     "github.com/rocket-pool/rocketpool-go/rocketpool"
+    trustednodesettings "github.com/rocket-pool/rocketpool-go/settings/trustednode"
 
     "github.com/rocket-pool/rocketpool-go/tests/testutils/accounts"
     "github.com/rocket-pool/rocketpool-go/tests/testutils/evm"
@@ -12,12 +13,16 @@ import (
 // Pass and execute a proposal
 func PassAndExecuteProposal(rp *rocketpool.RocketPool, proposalId uint64, trustedNodeAccount *accounts.Account) error {
 
+    // Get proposal voting delay
+    voteDelayBlocks, err := trustednodesettings.GetProposalVoteDelayBlocks(rp, nil)
+    if err != nil { return err }
+
     // Mine blocks until proposal voting delay has passed
-    if err := evm.MineBlocks(1); err != nil { return err }
+    if err := evm.MineBlocks(int(voteDelayBlocks)); err != nil { return err }
 
     // Vote on & execute proposal
-    if _, err := trustednode.VoteOnProposal(rp, proposalId, true, trustedNodeAccount.GetTransactor()); err != nil { return err }
-    if _, err := trustednode.ExecuteProposal(rp, proposalId, trustedNodeAccount.GetTransactor()); err != nil { return err }
+    if _, err := trustednodedao.VoteOnProposal(rp, proposalId, true, trustedNodeAccount.GetTransactor()); err != nil { return err }
+    if _, err := trustednodedao.ExecuteProposal(rp, proposalId, trustedNodeAccount.GetTransactor()); err != nil { return err }
 
     // Return
     return nil
