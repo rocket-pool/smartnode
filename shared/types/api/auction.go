2e1b958cdc67347218bf8d300d9f9013c193ff7a
Jake Pospischil <moles1@gmail.com>
2021-02-25 12:18:54 +1000

    implementing auction api endpoints

---
+++ shared/types/api/auction.go
@@ -0,0 +1,81 @@
+package api
+
+import (
+    "github.com/rocket-pool/rocketpool-go/auction"
+)
+
+
+type AuctionStatusResponse struct {
+    Status string               `json:"status"`
+    Error string                `json:"error"`
+}
+
+
+type AuctionLotsResponse struct {
+    Status string               `json:"status"`
+    Error string                `json:"error"`
+    Lots []auction.LotDetails   `json:"lots"`
+}
+
+
+type CanCreateLotResponse struct {
+    Status string               `json:"status"`
+    Error string                `json:"error"`
+    CanCreate bool              `json:"canCreate"`
+    InsufficientBalance bool    `json:"insufficientBalance"`
+    CreateLotDisabled bool      `json:"createLotDisabled"`
+}
+type CreateLotResponse struct {
+    Status string               `json:"status"`
+    Error string                `json:"error"`
+    LotID uint64                `json:"lotId"`
+    TxHash common.Hash          `json:"txHash"`
+}
+
+
+type CanBidOnLotResponse struct {
+    Status string               `json:"status"`
+    Error string                `json:"error"`
+    CanBid bool                 `json:"canBid"`
+    DoesNotExist bool           `json:"doesNotExist"`
+    BiddingEnded bool           `json:"biddingEnded"`
+    RPLExhausted bool           `json:"rplExhausted"`
+    BidOnLotDisabled bool       `json:"bidOnLotDisabled"`
+}
+type BidOnLotResponse struct {
+    Status string               `json:"status"`
+    Error string                `json:"error"`
+    TxHash common.Hash          `json:"txHash"`
+}
+
+
+type CanClaimFromLotResponse struct {
+    Status string               `json:"status"`
+    Error string                `json:"error"`
+    CanClaim bool               `json:"canClaim"`
+    DoesNotExist bool           `json:"doesNotExist"`
+    NoBidFromAddress bool       `json:"noBidFromAddress"`
+    NotCleared bool             `json:"notCleared"`
+}
+type ClaimFromLotResponse struct {
+    Status string               `json:"status"`
+    Error string                `json:"error"`
+    TxHash common.Hash          `json:"txHash"`
+}
+
+
+type CanRecoverRPLFromLotResponse struct {
+    Status string               `json:"status"`
+    Error string                `json:"error"`
+    CanRecover bool             `json:"canRecover"`
+    DoesNotExist bool           `json:"doesNotExist"`
+    NotCleared bool             `json:"notCleared"`
+    NoUnclaimedRPL bool         `json:"noUnclaimedRpl"`
+    RPLAlreadyRecovered bool    `json:"rplAlreadyRecovered"`
+}
+type RecoverRPLFromLotResponse struct {
+    Status string               `json:"status"`
+    Error string                `json:"error"`
+    TxHash common.Hash          `json:"txHash"`
+}
+
