84e89891a7fd37d950c532497188e8fcfd801821
Jake Pospischil <moles1@gmail.com>
2021-02-25 14:19:02 +1000

    implementing auction api endpoints

--- rocketpool/api/auction/create-lot.go
+++ rocketpool/api/auction/create-lot.go
@@ -1,8 +1,14 @@
 package auction
 
 import (
+    "math/big"
+
     "github.com/rocket-pool/rocketpool-go/auction"
+    "github.com/rocket-pool/rocketpool-go/network"
+    "github.com/rocket-pool/rocketpool-go/settings/protocol"
+    "github.com/rocket-pool/rocketpool-go/utils/eth"
     "github.com/urfave/cli"
+    "golang.org/x/sync/errgroup"
 
     "github.com/rocket-pool/smartnode/shared/services"
     "github.com/rocket-pool/smartnode/shared/types/api"
@@ -20,7 +26,49 @@ func canCreateLot(c *cli.Context) (*api.CanCreateLotResponse, error) {
     // Response
     response := api.CanCreateLotResponse{}
 
-    _ = rp
+    // Data
+    var wg errgroup.Group
+    var remainingRplBalance *big.Int
+    var lotMinimumEthValue *big.Int
+    var rplPrice *big.Int
+
+    // Check if lot creation is enabled
+    wg.Go(func() error {
+        createLotEnabled, err := protocol.GetCreateLotEnabled(rp, nil)
+        if err == nil {
+            response.CreateLotDisabled = !createLotEnabled
+        }
+        return err
+    })
+
+    // Get data
+    wg.Go(func() error {
+        var err error
+        remainingRplBalance, err = auction.GetRemainingRPLBalance(rp, nil)
+        return err
+    })
+    wg.Go(func() error {
+        var err error
+        lotMinimumEthValue, err = protocol.GetLotMinimumEthValue(rp, nil)
+        return err
+    })
+    wg.Go(func() error {
+        var err error
+        rplPrice, err = network.GetRPLPrice(rp, nil)
+        return err
+    })
+
+    // Wait for data
+    if err := wg.Wait(); err != nil {
+        return nil, err
+    }
+
+    // Check auction contract remaining RPL balance
+    var tmp big.Int
+    var lotMinimumRplAmount big.Int
+    tmp.Mul(lotMinimumEthValue, eth.EthToWei(1))
+    lotMinimumRplAmount.Quo(&tmp, rplPrice)
+    response.InsufficientBalance = (remainingRplBalance.Cmp(&lotMinimumRplAmount) < 0)
 
     // Update & return response
     response.CanCreate = !(response.InsufficientBalance || response.CreateLotDisabled)
