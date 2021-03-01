a0eb979b5fbe460c27d26ad2ebaea611c7dd11e4
Jake Pospischil <moles1@gmail.com>
2021-03-01 15:19:10 +1000

    implementing CLI auction endpoints

---
+++ rocketpool-cli/auction/recover-lot.go
@@ -0,0 +1,25 @@
+package auction
+
+import (
+    //"fmt"
+
+    "github.com/urfave/cli"
+
+    "github.com/rocket-pool/smartnode/shared/services/rocketpool"
+)
+
+
+func recoverRplFromLot(c *cli.Context) error {
+
+    // Get RP client
+    rp, err := rocketpool.NewClientFromCtx(c)
+    if err != nil { return err }
+    defer rp.Close()
+
+    _ = rp
+
+    // Log & return
+    return nil
+
+}
+
---
+++ rocketpool-cli/auction/status.go
@@ -0,0 +1,25 @@
+package auction
+
+import (
+    //"fmt"
+
+    "github.com/urfave/cli"
+
+    "github.com/rocket-pool/smartnode/shared/services/rocketpool"
+)
+
+
+func getStatus(c *cli.Context) error {
+
+    // Get RP client
+    rp, err := rocketpool.NewClientFromCtx(c)
+    if err != nil { return err }
+    defer rp.Close()
+
+    _ = rp
+
+    // Print & return
+    return nil
+
+}
+
