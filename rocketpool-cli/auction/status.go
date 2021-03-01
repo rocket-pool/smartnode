68b6ff612d7037405513cdb7b686e527c1abf3e0
Jake Pospischil <moles1@gmail.com>
2021-03-01 16:02:32 +1000

    implementing CLI auction endpoints

--- rocketpool-cli/auction/status.go
+++ rocketpool-cli/auction/status.go
@@ -1,11 +1,13 @@
 package auction
 
 import (
-    //"fmt"
+    "fmt"
 
+    "github.com/rocket-pool/rocketpool-go/utils/eth"
     "github.com/urfave/cli"
 
     "github.com/rocket-pool/smartnode/shared/services/rocketpool"
+    "github.com/rocket-pool/smartnode/shared/utils/math"
 )
 
 
@@ -16,9 +18,30 @@ func getStatus(c *cli.Context) error {
     if err != nil { return err }
     defer rp.Close()
 
-    _ = rp
+    // Get auction status
+    status, err := rp.AuctionStatus()
+    if err != nil {
+        return err
+    }
 
     // Print & return
+    fmt.Printf(
+        "A total of %.6f RPL is up for auction, with %.6f RPL currently allotted and %.6f RPL remaining.\n",
+        math.RoundDown(eth.WeiToEth(status.TotalRPLBalance), 6),
+        math.RoundDown(eth.WeiToEth(status.AllottedRPLBalance), 6),
+        math.RoundDown(eth.WeiToEth(status.RemainingRPLBalance), 6))
+    if status.LotCounts.ClaimAvailable > 0 {
+        fmt.Printf("%d lots you have bid on have RPL available to claim!\n", status.LotCounts.ClaimAvailable)
+    }
+    if status.LotCounts.BiddingAvailable > 0 {
+        fmt.Printf("%d lots are open for bidding!\n", status.LotCounts.BiddingAvailable)
+    }
+    if status.LotCounts.RPLRecoveryAvailable > 0 {
+        fmt.Printf("%d cleared lots have unclaimed RPL ready to recover!\n", status.LotCounts.RPLRecoveryAvailable)
+    }
+    if status.CanCreateLot {
+        fmt.Println("A new lot can be created with remaining RPL in the auction contract.")
+    }
     return nil
 
 }
