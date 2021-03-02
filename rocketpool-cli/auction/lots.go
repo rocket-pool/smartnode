eabcacaab8fa44d5b692667ca9f884c0d956a1a1
Jake Pospischil <moles1@gmail.com>
2021-03-02 11:31:32 +1000

    implementing CLI auction commands

--- rocketpool-cli/auction/lots.go
+++ rocketpool-cli/auction/lots.go
@@ -1,11 +1,14 @@
 package auction
 
 import (
-    //"fmt"
+    "fmt"
 
+    "github.com/rocket-pool/rocketpool-go/utils/eth"
     "github.com/urfave/cli"
 
     "github.com/rocket-pool/smartnode/shared/services/rocketpool"
+    "github.com/rocket-pool/smartnode/shared/types/api"
+    "github.com/rocket-pool/smartnode/shared/utils/math"
 )
 
 
@@ -16,9 +19,92 @@ func getLots(c *cli.Context) error {
     if err != nil { return err }
     defer rp.Close()
 
-    _ = rp
+    // Get lot details
+    lots, err := rp.AuctionLots()
+    if err != nil {
+        return err
+    }
 
-    // Print & return
+    // Get lots by status
+    openLots := []api.LotDetails{}
+    clearedLots := []api.LotDetails{}
+    claimableLots := []api.LotDetails{}
+    biddableLots := []api.LotDetails{}
+    recoverableLots := []api.LotDetails{}
+    for _, lot := range lots.Lots {
+        if lot.Details.Cleared {
+            clearedLots = append(clearedLots, lot)
+        } else {
+            openLots = append(openLots, lot)
+        }
+        if lot.ClaimAvailable {
+            claimableLots = append(claimableLots, lot)
+        }
+        if lot.BiddingAvailable {
+            biddableLots = append(biddableLots, lot)
+        }
+        if lot.RPLRecoveryAvailable {
+            recoverableLots = append(recoverableLots, lot)
+        }
+    }
+
+    // Print lot details by status
+    if len(lots.Lots) == 0 {
+        fmt.Println("There are no lots for auction yet.")
+    }
+    for status := 0; status < 2; status++ {
+
+        // Get status title format & lot list
+        var statusFormat string
+        var statusLots []api.LotDetails
+        if status == 0 {
+            statusFormat = "%d lot(s) open for bidding:\n"
+            statusLots = openLots
+        } else {
+            statusFormat = "%d cleared lot(s):\n"
+            statusLots = clearedLots
+        }
+        if len(statusLots) == 0 { continue }
+
+        // Print
+        fmt.Printf(statusFormat, len(statusLots))
+        for _, lot := range statusLots {
+
+            fmt.Printf("--------------------\n")
+
+
+
+            fmt.Printf("\n")
+
+        }
+        fmt.Println("")
+
+    }
+
+    // Print actionable lot details
+    if len(claimableLots) > 0 {
+        fmt.Printf("%d lots you have bid on have RPL available to claim:\n", len(claimableLots))
+        for _, lot := range claimableLots {
+            fmt.Printf("- lot %d (%.6f ETH bid @ %.6f ETH per RPL)\n", lot.Details.Index, math.RoundDown(eth.WeiToEth(lot.Details.AddressBidAmount), 6), math.RoundDown(eth.WeiToEth(lot.Details.CurrentPrice), 6))
+        }
+        fmt.Println("")
+    }
+    if len(biddableLots) > 0 {
+        fmt.Printf("%d lots are open for bidding:\n", len(biddableLots))
+        for _, lot := range biddableLots {
+            fmt.Printf("- lot %d (current price %.6f ETH per RPL)\n", lot.Details.Index, math.RoundDown(eth.WeiToEth(lot.Details.CurrentPrice), 6))
+        }
+        fmt.Println("")
+    }
+    if len(recoverableLots) > 0 {
+        fmt.Printf("%d lots have unclaimed RPL ready to recover:\n", len(recoverableLots))
+        for _, lot := range recoverableLots {
+            fmt.Printf("- lot %d (%.6f RPL unclaimed)\n", lot.Details.Index, math.RoundDown(eth.WeiToEth(lot.Details.RemainingRPLAmount), 6))
+        }
+        fmt.Println("")
+    }
+
+    // Return
     return nil
 
 }
