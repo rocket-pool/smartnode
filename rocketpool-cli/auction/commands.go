ba8fd0191e144204cf965c12d9525e4da70463de
Jake Pospischil <moles1@gmail.com>
2021-03-02 12:17:29 +1000

    implementing CLI auction commands

--- rocketpool-cli/auction/commands.go
+++ rocketpool-cli/auction/commands.go
@@ -67,11 +67,29 @@ func RegisterCommands(app *cli.App, name string, aliases []string) {
                 Name:      "bid-lot",
                 Aliases:   []string{"b"},
                 Usage:     "Bid on a lot",
-                UsageText: "rocketpool auction bid-lot amount",
+                UsageText: "rocketpool auction bid-lot [options]",
+                Flags: []cli.Flag{
+                    cli.StringFlag{
+                        Name:  "lot, l",
+                        Usage: "The ID of the lot to bid on",
+                    },
+                    cli.StringFlag{
+                        Name:  "amount, a",
+                        Usage: "The amount of ETH to bid (or 'max')",
+                    },
+                },
                 Action: func(c *cli.Context) error {
 
                     // Validate args
                     if err := cliutils.ValidateArgCount(c, 0); err != nil { return err }
+
+                    // Validate flags
+                    if c.String("lot") != "" {
+                        if _, err := cliutils.ValidateUint("lot ID", c.String("lot")); err != nil { return err }
+                    }
+                    if c.String("amount") != "" && c.String("amount") != "max" {
+                        if _, err := cliutils.ValidatePositiveEthAmount("bid amount", c.String("amount")); err != nil { return err }
+                    }
 
                     // Run
                     return bidOnLot(c)
@@ -83,11 +101,22 @@ func RegisterCommands(app *cli.App, name string, aliases []string) {
                 Name:      "claim-lot",
                 Aliases:   []string{"c"},
                 Usage:     "Claim RPL from a lot",
-                UsageText: "rocketpool auction claim-lot",
+                UsageText: "rocketpool auction claim-lot [options]",
+                Flags: []cli.Flag{
+                    cli.StringFlag{
+                        Name:  "lot, l",
+                        Usage: "The lot to claim RPL from (lot ID or 'all')",
+                    },
+                },
                 Action: func(c *cli.Context) error {
 
                     // Validate args
                     if err := cliutils.ValidateArgCount(c, 0); err != nil { return err }
+
+                    // Validate flags
+                    if c.String("lot") != "" && c.String("lot") != "all" {
+                        if _, err := cliutils.ValidateUint("lot ID", c.String("lot")); err != nil { return err }
+                    }
 
                     // Run
                     return claimFromLot(c)
@@ -99,11 +128,22 @@ func RegisterCommands(app *cli.App, name string, aliases []string) {
                 Name:      "recover-lot",
                 Aliases:   []string{"r"},
                 Usage:     "Recover unclaimed RPL from a lot (returning it to the auction contract)",
-                UsageText: "rocketpool auction recover-lot",
+                UsageText: "rocketpool auction recover-lot [options]",
+                Flags: []cli.Flag{
+                    cli.StringFlag{
+                        Name:  "lot, l",
+                        Usage: "The lot to recover unclaimed RPL from (lot ID or 'all')",
+                    },
+                },
                 Action: func(c *cli.Context) error {
 
                     // Validate args
                     if err := cliutils.ValidateArgCount(c, 0); err != nil { return err }
+
+                    // Validate flags
+                    if c.String("lot") != "" && c.String("lot") != "all" {
+                        if _, err := cliutils.ValidateUint("lot ID", c.String("lot")); err != nil { return err }
+                    }
 
                     // Run
                     return recoverRplFromLot(c)
