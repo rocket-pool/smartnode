0dfe74e1c97c6b4e657c90a89d9ad08a8979a63d
Jake Pospischil <moles1@gmail.com>
2021-03-12 15:13:36 +1000

    updated bins version info

--- rocketpool/rocketpool.go
+++ rocketpool/rocketpool.go
@@ -22,7 +22,7 @@ func main() {
     // Set application info
     app.Name = "rocketpool"
     app.Usage = "Rocket Pool service"
-    app.Version = "0.0.9"
+    app.Version = "1.0.0-beta.0"
     app.Authors = []cli.Author{
         cli.Author{
             Name:  "David Rugendyke",
