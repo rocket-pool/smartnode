8db46042a261d167a7baa781845b65fbfc9b3c0a
Jake Pospischil <moles1@gmail.com>
2021-03-15 12:36:00 +1000

    minor formatting fix

--- shared/services/beacon/nimbus/types.go
+++ shared/services/beacon/nimbus/types.go
@@ -55,7 +55,7 @@ type Validator struct {
         EffectiveBalance           uint64    `json:"effective_balance"`
         Slashed                    bool      `json:"slashed"`
         ActivationEligibilityEpoch uint64    `json:"activation_eligibility_epoch"`
-        ActivationEpoch            int64    `json:"activation_epoch"`    // Nimbus uses -1 for FAR_FUTURE_EPOCH so this has to be a signed int
+        ActivationEpoch            int64     `json:"activation_epoch"`   // Nimbus uses -1 for FAR_FUTURE_EPOCH so this has to be a signed int
         ExitEpoch                  int64     `json:"exit_epoch"`         // Same here
         WithdrawableEpoch          int64     `json:"withdrawable_epoch"` // Same here
     } `json:"validator"`
