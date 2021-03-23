06abb5d76199451aeecf9282e7eb0fc6ea705a59
Jake Pospischil <moles1@gmail.com>
2021-03-23 10:40:34 +1000

    updated binaries version info

--- rocketpool-cli/rocketpool-cli.go
+++ rocketpool-cli/rocketpool-cli.go
@@ -37,7 +37,7 @@ ______           _        _    ______           _
     // Set application info
     app.Name = "rocketpool"
     app.Usage = "Rocket Pool CLI"
-    app.Version = "1.0.0-beta.0"
+    app.Version = "1.0.0-beta.1"
     app.Authors = []cli.Author{
         cli.Author{
             Name:  "David Rugendyke",
