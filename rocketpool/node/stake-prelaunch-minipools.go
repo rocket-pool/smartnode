97dce332974311d6bcd7f16466a706e4b3db0835
Joao Poupino <joao@probely.com>
2021-03-30 22:57:11 +0000

    Make container detection code more general

--- rocketpool/node/stake-prelaunch-minipools.go
+++ rocketpool/node/stake-prelaunch-minipools.go
@@ -4,10 +4,8 @@ import (
     "context"
     "errors"
     "fmt"
-    "io/ioutil"
     "os"
     "os/exec"
-    "strings"
     "time"
 
     "github.com/docker/docker/api/types"
@@ -264,8 +262,8 @@ func (t *stakePrelaunchMinipools) stakeMinipool(mp *minipool.Minipool, withdrawa
 // Restart validator process
 func (t *stakePrelaunchMinipools) restartValidator() error {
 
-    // Restart validator docker container
-    if isInsideDocker() {
+    // Restart validator container
+    if isInsideContainer() {
 
         // Get validator container name & client type label
         var containerName string
@@ -335,18 +333,27 @@ func (t *stakePrelaunchMinipools) restartValidator() error {
 
 }
 
-
-// Check whether process is running inside docker
-func isInsideDocker() bool {
-
-    // Read process control group info; assume non-docker on failure
-    cgroup, err := ioutil.ReadFile("/proc/1/cgroup")
-    if err != nil {
-        return false
+// Check if path exists
+func pathExists(path string) bool {
+    if _, err := os.Stat(path); err == nil {
+        return true;
     }
+    // Assuming that the path does not exist. Note that this may result in false negatives.
+    // E.g. a path may exist, but we have no permission to read it or its ancestors.
+    return false;
+}
 
-    // check whether control group info contains docker references
-    return strings.Contains(string(cgroup), "docker")
-
+// Check whether process is running inside a container
+func isInsideContainer() bool {
+    containerMarkerPaths := []string {
+        "/.dockerenv", // Docker
+        "/run/.containerenv", // Podman
+    }
+    for _, path := range containerMarkerPaths {
+        if pathExists(path) {
+            return true;
+        }
+    }
+    return false;
 }
 
