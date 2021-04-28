3e0b991391ea9f409ab22948229b3938c870f283
Joe Clapis <jclapis@outlook.com>
2021-04-28 00:04:51 -0400

    Refactored the CLI with the new async TX system

---
+++ shared/services/rocketpool/api.go
@@ -0,0 +1,26 @@
+package rocketpool
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/rocket-pool/smartnode/shared/types/api"
+)
+
+// Wait for a transaction
+func (c *Client) WaitForTransaction(txHash common.Hash) (api.APIResponse, error) {
+    responseBytes, err := c.callAPI(fmt.Sprintf("wait %s", txHash.String()))
+    if err != nil {
+        return api.APIResponse{}, fmt.Errorf("Error waiting for tx: %w", err)
+    }
+    var response api.APIResponse
+    if err := json.Unmarshal(responseBytes, &response); err != nil {
+        return api.APIResponse{}, fmt.Errorf("Error decoding wait response: %w", err)
+    }
+    if response.Error != "" {
+        return api.APIResponse{}, fmt.Errorf("Error waiting for tx: %s", response.Error)
+    }
+    return response, nil
+}
