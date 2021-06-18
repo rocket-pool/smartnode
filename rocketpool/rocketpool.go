fa6f1c990bfa6800767025cb11f23bf7d2a181a8
Joe Clapis <jclapis@outlook.com>
2021-06-18 01:17:49 -0400

    Forgot to save a version number

--- rocketpool/rocketpool.go
+++ rocketpool/rocketpool.go
@@ -21,7 +21,7 @@ func main() {
     // Set application info
     app.Name = "rocketpool"
     app.Usage = "Rocket Pool service"
-    app.Version = "1.0.0-rc1"
+    app.Version = "1.0.0-rc3"
     app.Authors = []cli.Author{
         {
             Name:  "David Rugendyke",
