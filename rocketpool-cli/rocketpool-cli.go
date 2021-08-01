c142e15728000a2febdff44e7987718351750302
Joe Clapis <jclapis@outlook.com>
2021-08-01 19:07:55 -0400

    Updated CLI and daemon versions

--- rocketpool-cli/rocketpool-cli.go
+++ rocketpool-cli/rocketpool-cli.go
@@ -37,7 +37,7 @@ ______           _        _    ______           _
     // Set application info
     app.Name = "rocketpool"
     app.Usage = "Rocket Pool CLI"
-    app.Version = "1.0.0-rc3"
+    app.Version = "1.0.0-rc4"
     app.Authors = []cli.Author{
         {
             Name:  "David Rugendyke",
