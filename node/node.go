b925817d36e126e111cd629d68775d6c8eec87e3
Joe Clapis <jclapis@outlook.com>
2021-08-08 14:50:40 -0400

    Added timezone sanitization to node registration

--- node/node.go
+++ node/node.go
@@ -245,6 +245,10 @@ func EstimateRegisterNodeGas(rp *rocketpool.RocketPool, timezoneLocation string,
     if err != nil {
         return rocketpool.GasInfo{}, err
     }
+    _, err = time.LoadLocation(timezoneLocation)
+    if err != nil {
+        return rocketpool.GasInfo{}, fmt.Errorf("Could not verify timezone [%s]: %w", timezoneLocation, err)
+    }
     return rocketNodeManager.GetTransactionGasInfo(opts, "registerNode", timezoneLocation)
 }
 
@@ -254,6 +258,10 @@ func RegisterNode(rp *rocketpool.RocketPool, timezoneLocation string, opts *bind
     rocketNodeManager, err := getRocketNodeManager(rp)
     if err != nil {
         return common.Hash{}, err
+    }
+    _, err = time.LoadLocation(timezoneLocation)
+    if err != nil {
+        return common.Hash{}, fmt.Errorf("Could not verify timezone [%s]: %w", timezoneLocation, err)
     }
     hash, err := rocketNodeManager.Transact(opts, "registerNode", timezoneLocation)
     if err != nil {
@@ -271,7 +279,7 @@ func EstimateSetTimezoneLocationGas(rp *rocketpool.RocketPool, timezoneLocation 
     }
     _, err = time.LoadLocation(timezoneLocation)
     if err != nil {
-        return rocketpool.GasInfo{}, fmt.Errorf("Could not parse timezone [%s]: %w", timezoneLocation, err)
+        return rocketpool.GasInfo{}, fmt.Errorf("Could not verify timezone [%s]: %w", timezoneLocation, err)
     }
     return rocketNodeManager.GetTransactionGasInfo(opts, "setTimezoneLocation", timezoneLocation)
 }
@@ -285,7 +293,7 @@ func SetTimezoneLocation(rp *rocketpool.RocketPool, timezoneLocation string, opt
     }
     _, err = time.LoadLocation(timezoneLocation)
     if err != nil {
-        return common.Hash{}, fmt.Errorf("Could not parse timezone [%s]: %w", timezoneLocation, err)
+        return common.Hash{}, fmt.Errorf("Could not verify timezone [%s]: %w", timezoneLocation, err)
     }
     hash, err := rocketNodeManager.Transact(opts, "setTimezoneLocation", timezoneLocation)
     if err != nil {
