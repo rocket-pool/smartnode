65536c7fd42d8e6a1dee959f49efc37e8193ebf3
Joe Clapis <jclapis@outlook.com>
2021-08-14 02:27:13 -0400

    Updated some minor wording based on Ken's feedback

--- rocketpool-cli/queue/status.go
+++ rocketpool-cli/queue/status.go
@@ -1,13 +1,13 @@
 package queue
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/rocket-pool/rocketpool-go/utils/eth"
-    "github.com/urfave/cli"
+	"github.com/rocket-pool/rocketpool-go/utils/eth"
+	"github.com/urfave/cli"
 
-    "github.com/rocket-pool/smartnode/shared/services/rocketpool"
-    "github.com/rocket-pool/smartnode/shared/utils/math"
+	"github.com/rocket-pool/smartnode/shared/services/rocketpool"
+	"github.com/rocket-pool/smartnode/shared/utils/math"
 )
 
 
@@ -25,7 +25,7 @@ func getStatus(c *cli.Context) error {
     }
 
     // Print & return
-    fmt.Printf("The deposit pool has a balance of %.6f ETH.\n", math.RoundDown(eth.WeiToEth(status.DepositPoolBalance), 6))
+    fmt.Printf("The staking pool has a balance of %.6f ETH.\n", math.RoundDown(eth.WeiToEth(status.DepositPoolBalance), 6))
     fmt.Printf("There are %d available minipools with a total capacity of %.6f ETH.\n", status.MinipoolQueueLength, math.RoundDown(eth.WeiToEth(status.MinipoolQueueCapacity), 6))
     return nil
 
