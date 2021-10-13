3df825e29df9f03d34ee88e610676a61645d0615
Joe Clapis <jclapis@outlook.com>
2021-10-12 21:07:52 -0400

    Fixed a version mismatch

--- rocketpool/build.sh
+++ rocketpool/build.sh
@@ -13,7 +13,7 @@ apt update
 apt dist-upgrade -y
 apt install build-essential git wget -y
 cd /tmp
-wget https://golang.org/dl/go1.17.1.linux-$ARCH.tar.gz
+wget https://golang.org/dl/go1.17.2.linux-$ARCH.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.2.linux-$ARCH.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 cd /smartnode/rocketpool
