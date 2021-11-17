eea4707f325fff117f05291127fb898d32317a4c
Joe Clapis <jclapis@outlook.com>
2021-11-16 19:54:38 -0500

    Updated dependencies

--- rocketpool-cli/build.sh
+++ rocketpool-cli/build.sh
@@ -2,7 +2,7 @@
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o rocketpool-cli-linux-amd64 rocketpool-cli.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o rocketpool-cli-darwin-amd64 rocketpool-cli.go
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o rocketpool-cli-windows-amd64.exe rocketpool-cli.go
+#CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o rocketpool-cli-windows-amd64.exe rocketpool-cli.go
 
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o rocketpool-cli-linux-arm64 rocketpool-cli.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o rocketpool-cli-darwin-arm64 rocketpool-cli.go
