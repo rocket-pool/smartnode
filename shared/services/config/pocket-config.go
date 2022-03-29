affce2033cba626a531c5a24be66a9d47788fafa
Joe Clapis <jclapis@outlook.com>
2022-03-28 22:41:37 -0400

    Added a temp Kiln ID for Pocket

--- shared/services/config/pocket-config.go
+++ shared/services/config/pocket-config.go
@@ -62,6 +62,7 @@ func NewPocketConfig(config *RocketPoolConfig, isFallback bool) *PocketConfig {
 			Default: map[Network]interface{}{
 				Network_Mainnet: defaultPocketGatewayMainnet,
 				Network_Prater:  defaultPocketGatewayPrater,
+				Network_Kiln:    "",
 			},
 			Regex:                "(^$|^(lb\\/)?[0-9a-zA-Z]{24,}$)",
 			AffectsContainers:    []ContainerID{ContainerID_Eth1},
