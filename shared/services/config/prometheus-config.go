3879473bc72ba919c075f8d36d1d741b0dabeb18
Joe Clapis <jclapis@outlook.com>
2022-04-22 03:36:04 -0400

    Updated Grafana and Prometheus versions

--- shared/services/config/prometheus-config.go
+++ shared/services/config/prometheus-config.go
@@ -1,7 +1,7 @@
 package config
 
 // Constants
-const prometheusTag string = "prom/prometheus:v2.31.1"
+const prometheusTag string = "prom/prometheus:v2.35.0"
 
 // Defaults
 const defaultPrometheusPort uint16 = 9091
