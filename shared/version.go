7f28fe9a90cd527729302bf168d461a900a5c374
Joe Clapis <jclapis@outlook.com>
2022-04-25 02:41:59 -0400

    Removed the Prysm supermajority warning

--- shared/version.go
+++ shared/version.go
@@ -1,6 +1,6 @@
 package shared
 
-const RocketPoolVersion string = "1.3.1"
+const RocketPoolVersion string = "1.3.2-dev"
 
 const Logo string = `______           _        _    ______           _
 | ___ \         | |      | |   | ___ \         | |
