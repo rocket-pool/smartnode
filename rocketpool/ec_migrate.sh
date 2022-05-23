967bae7485e008728db3cfd9c620618c3fd08772
Joe Clapis <jclapis@outlook.com>
2022-05-23 02:21:40 -0400

    Updated the ec-migrator script to better support Busybox

--- rocketpool/ec_migrate.sh
+++ rocketpool/ec_migrate.sh
@@ -8,9 +8,16 @@ if [ "$OPERATION" = "size" ]; then
     if [ ! -d $EXTERNAL_DIR ]; then
         echo "Source path is not a directory." 1>&2
         exit 1
-    else 
+    else
         # Get the space used by the external directory
-        du -s -B 1 $EXTERNAL_DIR | awk -F '\\s*' '{print $1}'
+        DIR_SIZE=$(du -s -k $EXTERNAL_DIR | awk -F '\\s*' '{print $1}')
+        if [ ! -z "$DIR_SIZE" ]; then
+            # The size will be in KB because of Busybox, so turn it into bytes
+            expr $DIR_SIZE \* 1024
+        else
+            echo "Failed to get source directory size." 1>&2
+            exit 2
+        fi
     fi
 
 else 
