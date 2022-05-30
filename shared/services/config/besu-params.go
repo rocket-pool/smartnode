ef7aa5354644f5a1a30bfd10711f92db971c3ab8
Joe Clapis <jclapis@outlook.com>
2022-05-29 21:32:36 -0400

    Updated the EC versions

--- shared/services/config/besu-params.go
+++ shared/services/config/besu-params.go
@@ -7,8 +7,8 @@ import (
 
 // Constants
 const (
-	besuTagAmd64         string = "hyperledger/besu:22.4.1-openjdk-latest"
-	besuTagArm64         string = "hyperledger/besu:22.4.1-openjdk-latest"
+	besuTagAmd64         string = "hyperledger/besu:22.4.2-openjdk-latest"
+	besuTagArm64         string = "hyperledger/besu:22.4.2-openjdk-latest"
 	besuEventLogInterval int    = 25000
 	besuMaxPeers         uint16 = 25
 	besuStopSignal       string = "SIGTERM"
--- shared/services/config/nethermind-params.go
+++ shared/services/config/nethermind-params.go
@@ -9,8 +9,8 @@ import (
 
 // Constants
 const (
-	nethermindTagAmd64         string = "nethermind/nethermind:1.13.0"
-	nethermindTagArm64         string = "rocketpool/nethermind:1.13.0-pi"
+	nethermindTagAmd64         string = "nethermind/nethermind:1.13.1"
+	nethermindTagArm64         string = "nethermind/nethermind:1.13.1"
 	nethermindEventLogInterval int    = 25000
 	nethermindStopSignal       string = "SIGINT"
 )
