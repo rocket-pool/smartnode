f04d4e5095f2686500dabe1949e0d532e0c53758
Joe Clapis <jclapis@outlook.com>
2022-06-01 00:23:03 -0400

    Added all of the CCs back in

--- shared/services/config/rocket-pool-config.go
+++ shared/services/config/rocket-pool-config.go
@@ -261,7 +261,7 @@ func NewRocketPoolConfig(rpDir string, isNativeMode bool) *RocketPoolConfig {
 					Value:       ExecutionClient_Pocket,
 				},*/{
 					Name:        "*Dummy",
-					Description: "Ignore moe",
+					Description: "Ignore me",
 					Value:       ExecutionClient_Unknown,
 				}},
 		},
@@ -313,19 +313,19 @@ func NewRocketPoolConfig(rpDir string, isNativeMode bool) *RocketPoolConfig {
 				Name:        "Lighthouse",
 				Description: "Lighthouse is a Consensus client with a heavy focus on speed and security. The team behind it, Sigma Prime, is an information security and software engineering firm who have funded Lighthouse along with the Ethereum Foundation, Consensys, and private individuals. Lighthouse is built in Rust and offered under an Apache 2.0 License.",
 				Value:       ConsensusClient_Lighthouse,
-			}, /*{
-					Name:        "Nimbus",
-					Description: "Nimbus is a Consensus client implementation that strives to be as lightweight as possible in terms of resources used. This allows it to perform well on embedded systems, resource-restricted devices -- including Raspberry Pis and mobile devices -- and multi-purpose servers.",
-					Value:       ConsensusClient_Nimbus,
-				}, {
-					Name:        "Prysm",
-					Description: "Prysm is a Go implementation of Ethereum Consensus protocol with a focus on usability, security, and reliability. Prysm is developed by Prysmatic Labs, a company with the sole focus on the development of their client. Prysm is written in Go and released under a GPL-3.0 license.",
-					Value:       ConsensusClient_Prysm,
-				}, */{
-					Name:        "Teku",
-					Description: "PegaSys Teku (formerly known as Artemis) is a Java-based Ethereum 2.0 client designed & built to meet institutional needs and security requirements. PegaSys is an arm of ConsenSys dedicated to building enterprise-ready clients and tools for interacting with the core Ethereum platform. Teku is Apache 2 licensed and written in Java, a language notable for its maturity & ubiquity.",
-					Value:       ConsensusClient_Teku,
-				}},
+			}, {
+				Name:        "Nimbus",
+				Description: "Nimbus is a Consensus client implementation that strives to be as lightweight as possible in terms of resources used. This allows it to perform well on embedded systems, resource-restricted devices -- including Raspberry Pis and mobile devices -- and multi-purpose servers.",
+				Value:       ConsensusClient_Nimbus,
+			}, {
+				Name:        "Prysm",
+				Description: "Prysm is a Go implementation of Ethereum Consensus protocol with a focus on usability, security, and reliability. Prysm is developed by Prysmatic Labs, a company with the sole focus on the development of their client. Prysm is written in Go and released under a GPL-3.0 license.",
+				Value:       ConsensusClient_Prysm,
+			}, {
+				Name:        "Teku",
+				Description: "PegaSys Teku (formerly known as Artemis) is a Java-based Ethereum 2.0 client designed & built to meet institutional needs and security requirements. PegaSys is an arm of ConsenSys dedicated to building enterprise-ready clients and tools for interacting with the core Ethereum platform. Teku is Apache 2 licensed and written in Java, a language notable for its maturity & ubiquity.",
+				Value:       ConsensusClient_Teku,
+			}},
 		},
 
 		ExternalConsensusClient: Parameter{
