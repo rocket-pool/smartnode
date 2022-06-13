d8779d6fbb7a199e7d04d0a9259362f87ea9dd63
Joe Clapis <jclapis@outlook.com>
2022-06-13 00:33:34 -0400

    Updated to alpha 1

--- shared/version.go
+++ shared/version.go
@@ -1,6 +1,6 @@
 package shared
 
-const RocketPoolVersion string = "1.5.0-dev"
+const RocketPoolVersion string = "1.5.0-a1"
 
 const Logo string = `______           _        _    ______           _
 | ___ \         | |      | |   | ___ \         | |
