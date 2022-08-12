130c6e89ae4543bc5e2181fe6b5ca467918dd2fa
Joe Clapis <jclapis@outlook.com>
2022-08-11 21:10:40 -0400

    The TUI can now access the MEV-Boost settings page on Prater

--- rocketpool-cli/service/config/settings-home.go
+++ rocketpool-cli/service/config/settings-home.go
@@ -124,7 +124,7 @@ func (home *settingsHome) createContent() {
 			}
 		} else if s1 == home.mevBoostPage.page.title {
 			switch home.md.Config.Smartnode.Network.Value.(config.Network) {
-			case config.Network_Mainnet, config.Network_Prater:
+			case config.Network_Mainnet:
 				return
 			}
 		}
