edcb054833d33f980139183497e98e47339af6c3
Joe Clapis <jclapis@outlook.com>
2022-08-15 14:36:03 -0400

    Updated Prysm on the testnet

--- shared/services/config/prysm-config.go
+++ shared/services/config/prysm-config.go
@@ -5,11 +5,12 @@ import (
 	"runtime"
 )
 
-// v2.1.3
 const (
-	prysmBnTagAmd64Test     string = "prysmaticlabs/prysm-beacon-chain:HEAD-4e225f-debug"
-	prysmVcTagAmd64Test     string = "prysmaticlabs/prysm-validator:HEAD-4e225f-debug"
-	prysmTagArm64Test       string = "rocketpool/prysm:v2.1.4"
+	// v2.1.5-dev
+	prysmBnTagAmd64Test string = "prysmaticlabs/prysm-beacon-chain:HEAD-65b5c4-debug"
+	prysmVcTagAmd64Test string = "prysmaticlabs/prysm-validator:HEAD-65b5c4-debug"
+	prysmTagArm64Test   string = "rocketpool/prysm:v2.1.4"
+	// v2.1.4
 	prysmBnTagAmd64Prod     string = "prysmaticlabs/prysm-beacon-chain:HEAD-4e225f-debug"
 	prysmVcTagAmd64Prod     string = "prysmaticlabs/prysm-validator:HEAD-4e225f-debug"
 	prysmTagArm64Prod       string = "rocketpool/prysm:v2.1.4"
