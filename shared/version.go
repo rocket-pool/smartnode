e1800b5bd1d79ef1c996289ae2265c5d2d8f6ef0
Joe Clapis <jclapis@outlook.com>
2022-09-15 05:15:19 -0400

    Added EC_ENGINE_WS_ENDPOINT

--- shared/version.go
+++ shared/version.go
@@ -1,6 +1,6 @@
 package shared
 
-const RocketPoolVersion string = "1.6.3"
+const RocketPoolVersion string = "1.6.4-dev"
 
 const Logo string = `______           _        _    ______           _
 | ___ \         | |      | |   | ___ \         | |
