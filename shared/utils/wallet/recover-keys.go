80217aa3a0e237b84c61584f88d3148c42674210
Jacob Shufro <jacob@shuf.ro>
2022-10-19 16:53:09 -0400

    Remove total order assumptions from recover/rebuild key search

--- shared/utils/wallet/recover-keys.go
+++ shared/utils/wallet/recover-keys.go
@@ -160,14 +160,11 @@ func RecoverMinipoolKeys(c *cli.Context, rp *rocketpool.RocketPool, address comm
 	}
 
 	// Recover remaining validator keys normally
-	index := uint(0)
 	for _, pubkey := range remainingPubkeys {
 		if testOnly {
-			index, err = w.TestRecoverValidatorKey(pubkey, index)
-			index++
+			_, err = w.TestRecoverValidatorKey(pubkey, 0)
 		} else {
-			index, err = w.RecoverValidatorKey(pubkey, index)
-			index++
+			_, err = w.RecoverValidatorKey(pubkey, 0)
 		}
 		if err != nil {
 			return nil, err
