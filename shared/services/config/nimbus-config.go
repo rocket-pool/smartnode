eeb0738e203533ced3bd63914302c7b4f2d196fa
Joe Clapis <jclapis@outlook.com>
2023-01-26 23:59:10 -0500

    Updated Nimbus

--- shared/services/config/nimbus-config.go
+++ shared/services/config/nimbus-config.go
@@ -7,8 +7,8 @@ import (
 )
 
 const (
-	nimbusTagTest            string = "statusim/nimbus-eth2:multiarch-v23.1.0"
-	nimbusTagProd            string = "statusim/nimbus-eth2:multiarch-v23.1.0"
+	nimbusTagTest            string = "statusim/nimbus-eth2:multiarch-v23.1.1"
+	nimbusTagProd            string = "statusim/nimbus-eth2:multiarch-v23.1.1"
 	defaultNimbusMaxPeersArm uint16 = 100
 	defaultNimbusMaxPeersAmd uint16 = 160
 )
