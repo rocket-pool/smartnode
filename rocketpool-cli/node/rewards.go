8f45db8cca26098d05271a05301d72d101ec1a00
Joe Clapis <jclapis@outlook.com>
2023-02-15 03:07:29 -0500

    node-rewards now downloads missing intervals

--- rocketpool-cli/node/rewards.go
+++ rocketpool-cli/node/rewards.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/urfave/cli"
 
+	rprewards "github.com/rocket-pool/smartnode/shared/services/rewards"
 	"github.com/rocket-pool/smartnode/shared/services/rocketpool"
 	cliutils "github.com/rocket-pool/smartnode/shared/utils/cli"
 )
@@ -25,15 +26,77 @@ func getRewards(c *cli.Context) error {
 		return err
 	}
 
+	// Get eligible intervals
+	rewardsInfoResponse, err := rp.GetRewardsInfo()
+	if err != nil {
+		return fmt.Errorf("error getting rewards info: %w", err)
+	}
+
+	if !rewardsInfoResponse.Registered {
+		fmt.Printf("This node is not currently registered.\n")
+		return nil
+	}
+
+	// Check for missing Merkle trees with rewards available
+	missingIntervals := []rprewards.IntervalInfo{}
+	invalidIntervals := []rprewards.IntervalInfo{}
+	for _, intervalInfo := range rewardsInfoResponse.InvalidIntervals {
+		if !intervalInfo.TreeFileExists {
+			fmt.Printf("You are missing the rewards tree file for interval %d.\n", intervalInfo.Index)
+			missingIntervals = append(missingIntervals, intervalInfo)
+		} else if !intervalInfo.MerkleRootValid {
+			fmt.Printf("Your local copy of the rewards tree file for interval %d does not match the canonical one.\n", intervalInfo.Index)
+			invalidIntervals = append(invalidIntervals, intervalInfo)
+		}
+	}
+
+	// Download the Merkle trees for all unclaimed intervals that don't exist
+	if len(missingIntervals) > 0 || len(invalidIntervals) > 0 {
+		fmt.Println()
+		fmt.Printf("%sNOTE: If you would like to regenerate these tree files manually, please answer `n` to the prompt below and run `rocketpool network generate-rewards-tree` before claiming your rewards.%s\n", colorBlue, colorReset)
+		if !cliutils.Confirm("Would you like to download all missing rewards tree files now?") {
+			fmt.Println("Cancelled.")
+			return nil
+		}
+
+		// Load the config file
+		cfg, _, err := rp.LoadConfig()
+		if err != nil {
+			return fmt.Errorf("error loading config: %w", err)
+		}
+
+		// Download the files
+		for _, missingInterval := range missingIntervals {
+			fmt.Printf("Downloading interval %d file... ", missingInterval.Index)
+			err := rprewards.DownloadRewardsFile(cfg, missingInterval.Index, missingInterval.CID, false)
+			if err != nil {
+				fmt.Println()
+				return err
+			}
+			fmt.Println("done!")
+		}
+		for _, invalidInterval := range invalidIntervals {
+			fmt.Printf("Downloading interval %d file... ", invalidInterval.Index)
+			err := rprewards.DownloadRewardsFile(cfg, invalidInterval.Index, invalidInterval.CID, false)
+			if err != nil {
+				fmt.Println()
+				return err
+			}
+			fmt.Println("done!")
+		}
+		fmt.Println()
+
+		// Reload rewards now that the files are in place
+		rewardsInfoResponse, err = rp.GetRewardsInfo()
+		if err != nil {
+			return fmt.Errorf("error getting rewards info: %w", err)
+		}
+	}
+
 	// Get node RPL rewards status
 	rewards, err := rp.NodeRewards()
 	if err != nil {
 		return err
-	}
-
-	if !rewards.Registered {
-		fmt.Printf("This node is not currently registered.\n")
-		return nil
 	}
 
 	fmt.Printf("%sNOTE: Legacy rewards from pre-Redstone are temporarily not being included in the below figures. They will be added back in a future release. We apologize for the inconvenience!%s\n\n", colorYellow, colorReset)
