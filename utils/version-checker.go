2f49aad985077d2aa506bbd1d65ceb3b38f51d94
Joe Clapis <jclapis@outlook.com>
2023-02-15 16:46:56 -0500

    Added call opts to GetCurrentVersion

--- utils/version-checker.go
+++ utils/version-checker.go
@@ -3,15 +3,16 @@ package utils
 import (
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/hashicorp/go-version"
 	"github.com/rocket-pool/rocketpool-go/node"
 	"github.com/rocket-pool/rocketpool-go/rocketpool"
 )
 
-func GetCurrentVersion(rp *rocketpool.RocketPool) (*version.Version, error) {
+func GetCurrentVersion(rp *rocketpool.RocketPool, opts *bind.CallOpts) (*version.Version, error) {
 
 	// Check for v1.2
-	nodeStakingVersion, err := node.GetNodeStakingVersion(rp, nil)
+	nodeStakingVersion, err := node.GetNodeStakingVersion(rp, opts)
 	if err != nil {
 		return nil, fmt.Errorf("error checking node staking version: %w", err)
 	}
@@ -20,7 +21,7 @@ func GetCurrentVersion(rp *rocketpool.RocketPool) (*version.Version, error) {
 	}
 
 	// Check for v1.1
-	nodeMgrVersion, err := node.GetNodeManagerVersion(rp, nil)
+	nodeMgrVersion, err := node.GetNodeManagerVersion(rp, opts)
 	if err != nil {
 		return nil, fmt.Errorf("error checking node manager version: %w", err)
 	}
