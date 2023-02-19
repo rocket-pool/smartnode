9e3015ea559d7b65c2bae739d20ba14c20565a02
Joe Clapis <jclapis@outlook.com>
2023-02-19 15:40:14 -0500

    Added missing mnemonic flag checks to import-key and set-withdrawal-creds

--- rocketpool-cli/minipool/import-key.go
+++ rocketpool-cli/minipool/import-key.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/rocket-pool/smartnode/rocketpool-cli/wallet"
 	"github.com/rocket-pool/smartnode/shared/services/rocketpool"
 	cliutils "github.com/rocket-pool/smartnode/shared/utils/cli"
 	"github.com/rocket-pool/smartnode/shared/utils/cli/migration"
@@ -37,7 +38,15 @@ func importKey(c *cli.Context, minipoolAddress common.Address) error {
 
 	fmt.Printf("This will allow you to import the externally-created private key for the validator associated with minipool %s so it can be managed by the Smartnode's Validator Client instead of your externally-managed Validator Client.\n\n", minipoolAddress.Hex())
 
-	success := migration.ImportKey(c, rp, minipoolAddress, c.String("mnemonic"))
+	// Get the mnemonic
+	mnemonic := ""
+	if c.IsSet("mnemonic") {
+		mnemonic = c.String("mnemonic")
+	} else {
+		mnemonic = wallet.PromptMnemonic()
+	}
+
+	success := migration.ImportKey(c, rp, minipoolAddress, mnemonic)
 	if !success {
 		fmt.Println("Importing the key failed.\nYou can try again later by using `rocketpool minipool import-key`.")
 		return nil
--- rocketpool-cli/minipool/set-withdrawal-creds.go
+++ rocketpool-cli/minipool/set-withdrawal-creds.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/rocket-pool/smartnode/rocketpool-cli/wallet"
 	"github.com/rocket-pool/smartnode/shared/services/rocketpool"
 	cliutils "github.com/rocket-pool/smartnode/shared/utils/cli"
 	"github.com/rocket-pool/smartnode/shared/utils/cli/migration"
@@ -37,7 +38,15 @@ func setWithdrawalCreds(c *cli.Context, minipoolAddress common.Address) error {
 
 	fmt.Printf("This will convert the withdrawal credentials for minipool %s's validator from the old 0x00 (BLS) value to the minipool address. This is meant for solo validator conversion **only**.\n\n", minipoolAddress.Hex())
 
-	success := migration.ChangeWithdrawalCreds(rp, minipoolAddress, "")
+	// Get the mnemonic
+	mnemonic := ""
+	if c.IsSet("mnemonic") {
+		mnemonic = c.String("mnemonic")
+	} else {
+		mnemonic = wallet.PromptMnemonic()
+	}
+
+	success := migration.ChangeWithdrawalCreds(rp, minipoolAddress, mnemonic)
 	if !success {
 		fmt.Println("Your withdrawal credentials cannot be automatically changed at this time. Import aborted.\nYou can try again later by using `rocketpool minipool set-withdrawal-creds`.")
 	}
