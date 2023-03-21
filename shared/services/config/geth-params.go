54203f773f9ff7b5e01fecd5aec42510317a9b42
Joe Clapis <jclapis@outlook.com>
2023-03-21 04:09:39 -0400

    Updated Geth

--- shared/services/config/geth-params.go
+++ shared/services/config/geth-params.go
@@ -9,8 +9,8 @@ import (
 
 // Constants
 const (
-	gethTagProd          string = "ethereum/client-go:v1.11.4"
-	gethTagTest          string = "ethereum/client-go:v1.11.4"
+	gethTagProd          string = "ethereum/client-go:v1.11.5"
+	gethTagTest          string = "ethereum/client-go:v1.11.5"
 	gethEventLogInterval int    = 1000
 	gethStopSignal       string = "SIGTERM"
 )
