ac616d2678da4a4b738c86fcc9bfc1174fb3a4b6
Joe Clapis <jclapis@outlook.com>
2023-03-23 21:36:42 -0400

    Updated LH, Nethermind, and Teku

--- shared/services/config/nethermind-params.go
+++ shared/services/config/nethermind-params.go
@@ -9,8 +9,8 @@ import (
 
 // Constants
 const (
-	nethermindTagProd          string = "nethermind/nethermind:1.17.2"
-	nethermindTagTest          string = "nethermind/nethermind:1.17.2"
+	nethermindTagProd          string = "nethermind/nethermind:1.17.3"
+	nethermindTagTest          string = "nethermind/nethermind:1.17.3"
 	nethermindEventLogInterval int    = 1000
 	nethermindStopSignal       string = "SIGTERM"
 )
