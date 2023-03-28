d26d22069fe5fe258eca098add42cdbc5bfa75c7
Joe Clapis <jclapis@outlook.com>
2023-03-28 00:32:10 -0400

    Updated Lodestar

--- shared/services/config/lodestar-config.go
+++ shared/services/config/lodestar-config.go
@@ -5,8 +5,8 @@ import (
 )
 
 const (
-	lodestarTagTest         string = "chainsafe/lodestar:v1.6.0"
-	lodestarTagProd         string = "chainsafe/lodestar:v1.6.0"
+	lodestarTagTest         string = "chainsafe/lodestar:v1.7.0"
+	lodestarTagProd         string = "chainsafe/lodestar:v1.7.0"
 	defaultLodestarMaxPeers uint16 = 50
 )
 
