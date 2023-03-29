9372a5a3b328c2b400d5874b64515967e6f877f2
Joe Clapis <jclapis@outlook.com>
2023-03-29 00:11:21 -0400

    Updated to RC3

--- shared/version.go
+++ shared/version.go
@@ -1,6 +1,6 @@
 package shared
 
-const RocketPoolVersion string = "1.9.0-dev"
+const RocketPoolVersion string = "1.9.0-rc3"
 
 const Logo string = `______           _        _    ______           _
 | ___ \         | |      | |   | ___ \         | |
