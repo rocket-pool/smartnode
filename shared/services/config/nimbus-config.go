b5c4d1245a6720476c4840c856c7df0841cd4072
Joe Clapis <jclapis@outlook.com>
2023-05-04 21:50:22 -0400

    Updated Besu and Lodestar, downgraded Nimbus

--- shared/services/config/nimbus-config.go
+++ shared/services/config/nimbus-config.go
@@ -8,12 +8,12 @@ import (
 
 const (
 	// Prater
-	nimbusBnTagTest string = "statusim/nimbus-eth2:multiarch-v23.4.0"
-	nimbusVcTagTest string = "statusim/nimbus-validator-client:multiarch-v23.4.0"
+	nimbusBnTagTest string = "statusim/nimbus-eth2:multiarch-v23.3.2"
+	nimbusVcTagTest string = "statusim/nimbus-validator-client:multiarch-v23.3.2"
 
 	// Mainnet
-	nimbusBnTagProd string = "statusim/nimbus-eth2:multiarch-v23.4.0"
-	nimbusVcTagProd string = "statusim/nimbus-validator-client:multiarch-v23.4.0"
+	nimbusBnTagProd string = "statusim/nimbus-eth2:multiarch-v23.3.2"
+	nimbusVcTagProd string = "statusim/nimbus-validator-client:multiarch-v23.3.2"
 
 	defaultNimbusMaxPeersArm uint16 = 100
 	defaultNimbusMaxPeersAmd uint16 = 160
