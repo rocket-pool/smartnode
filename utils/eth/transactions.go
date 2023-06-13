3cc32640687800904fbbff3fe40539bdaf84c260
Joe Clapis <jclapis@outlook.com>
2023-06-12 21:35:17 -0400

    SendTransaction now accepts a data payload

--- utils/eth/transactions.go
+++ utils/eth/transactions.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Estimate the gas of SendTransaction
-func EstimateSendTransactionGas(client rocketpool.ExecutionClient, toAddress common.Address, opts *bind.TransactOpts) (rocketpool.GasInfo, error) {
+func EstimateSendTransactionGas(client rocketpool.ExecutionClient, toAddress common.Address, data []byte, useSafeGasLimit bool, opts *bind.TransactOpts) (rocketpool.GasInfo, error) {
 
 	// User-defined settings
 	response := rocketpool.GasInfo{}
@@ -23,24 +23,36 @@ func EstimateSendTransactionGas(client rocketpool.ExecutionClient, toAddress com
 		value = big.NewInt(0)
 	}
 
+	// Set default data
+	if data == nil {
+		data = []byte{}
+	}
+
 	// Estimate gas limit
 	gasLimit, err := client.EstimateGas(context.Background(), ethereum.CallMsg{
 		From:     opts.From,
 		To:       &toAddress,
 		GasPrice: big.NewInt(0), // set to 0 for simulation
+		Data:     data,
 		Value:    value,
 	})
 	if err != nil {
 		return rocketpool.GasInfo{}, err
 	}
 	response.EstGasLimit = gasLimit
-	response.SafeGasLimit = gasLimit
+
+	if useSafeGasLimit {
+		response.SafeGasLimit = uint64(float64(gasLimit) * rocketpool.GasLimitMultiplier)
+	} else {
+		response.SafeGasLimit = gasLimit
+	}
 
 	return response, err
 }
 
 // Send a transaction to an address
-func SendTransaction(client rocketpool.ExecutionClient, toAddress common.Address, chainID *big.Int, opts *bind.TransactOpts) (common.Hash, error) {
+// useSafeGasLimit will amplify the estimated gas limit to by 50% for safety (no effect if the gas limit in opts is already set).
+func SendTransaction(client rocketpool.ExecutionClient, toAddress common.Address, chainID *big.Int, data []byte, useSafeGasLimit bool, opts *bind.TransactOpts) (common.Hash, error) {
 	var err error
 
 	// Get from address nonce
@@ -60,6 +72,11 @@ func SendTransaction(client rocketpool.ExecutionClient, toAddress common.Address
 		value = big.NewInt(0)
 	}
 
+	// Set default data
+	if data == nil {
+		data = []byte{}
+	}
+
 	// Estimate gas limit
 	gasLimit := opts.GasLimit
 	if gasLimit == 0 {
@@ -67,10 +84,15 @@ func SendTransaction(client rocketpool.ExecutionClient, toAddress common.Address
 			From:     opts.From,
 			To:       &toAddress,
 			GasPrice: big.NewInt(0), // use 0 gwei for simulation
+			Data:     data,
 			Value:    value,
 		})
 		if err != nil {
 			return common.Hash{}, err
+		}
+
+		if useSafeGasLimit {
+			gasLimit = uint64(float64(gasLimit) * rocketpool.GasLimitMultiplier)
 		}
 	}
 
@@ -83,7 +105,7 @@ func SendTransaction(client rocketpool.ExecutionClient, toAddress common.Address
 		Gas:        gasLimit,
 		To:         &toAddress,
 		Value:      value,
-		Data:       []byte{},
+		Data:       data,
 		AccessList: []types.AccessTuple{},
 	})
 
