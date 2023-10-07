8267c4ae7d01b778e373a0959f0f317f21e6989a
Joe Clapis <jclapis@outlook.com>
2023-10-06 22:55:57 -0400

    Added GetDeployBlock()

--- storage/rocket-storage.go
+++ storage/rocket-storage.go
@@ -2,9 +2,11 @@ package storage
 
 import (
 	"fmt"
+	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/rocket-pool/rocketpool-go/rocketpool"
 )
 
@@ -52,4 +54,15 @@ func ConfirmWithdrawalAddress(rp *rocketpool.RocketPool, nodeAddress common.Addr
 		return common.Hash{}, fmt.Errorf("Could not confirm node withdrawal address: %w", err)
 	}
 	return tx.Hash(), nil
+}
+
+// Get the number of the block that Rocket Pool was deployed on
+func GetDeployBlock(rp *rocketpool.RocketPool) (*big.Int, error) {
+	deployBlockHash := crypto.Keccak256Hash([]byte("deploy.block"))
+	deployBlock, err := rp.RocketStorage.GetUint(nil, deployBlockHash)
+	if err != nil {
+		return nil, fmt.Errorf("error getting Rocket Pool deployment block: %w", err)
+	}
+
+	return deployBlock, nil
 }
