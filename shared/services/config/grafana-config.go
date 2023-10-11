c7de275a956123fa01d74bd057c69cb68d67c621
Joe Clapis <jclapis@outlook.com>
2023-10-11 00:25:34 -0400

    Updated Grafana and Prometheus

--- shared/services/config/grafana-config.go
+++ shared/services/config/grafana-config.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Constants
-const grafanaTag string = "grafana/grafana:9.4.13"
+const grafanaTag string = "grafana/grafana:9.4.15"
 
 // Defaults
 const defaultGrafanaPort uint16 = 3100
