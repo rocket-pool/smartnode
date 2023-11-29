af3931a7571fa2729381d01b9c208df7699f1240
Fornax <23104993+fornax2@users.noreply.github.com>
2023-11-29 00:52:15 -0300

    Update multiple client versions

--- shared/services/config/lodestar-config.go
+++ shared/services/config/lodestar-config.go
@@ -5,8 +5,8 @@ import (
 )
 
 const (
-	lodestarTagTest         string = "chainsafe/lodestar:v1.11.3"
-	lodestarTagProd         string = "chainsafe/lodestar:v1.11.3"
+	lodestarTagTest         string = "chainsafe/lodestar:v1.12.0"
+	lodestarTagProd         string = "chainsafe/lodestar:v1.12.0"
 	defaultLodestarMaxPeers uint16 = 50
 )
 
