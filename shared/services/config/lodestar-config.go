f375db65fd2284f93c55aba3d6875e7f143a6b4d
Fornax <23104993+fornax2@users.noreply.github.com>
2023-12-03 18:59:30 -0300

    Rolling back lodestar considering a bug reported

--- shared/services/config/lodestar-config.go
+++ shared/services/config/lodestar-config.go
@@ -5,8 +5,8 @@ import (
 )
 
 const (
-	lodestarTagTest         string = "chainsafe/lodestar:v1.12.0"
-	lodestarTagProd         string = "chainsafe/lodestar:v1.12.0"
+	lodestarTagTest         string = "chainsafe/lodestar:v1.11.3"
+	lodestarTagProd         string = "chainsafe/lodestar:v1.11.3"
 	defaultLodestarMaxPeers uint16 = 50
 )
 
