75d5d98809eee5e84b7635b22212c3317bbf62f5
Joe Clapis <jclapis@outlook.com>
2023-12-20 14:12:33 -0500

    Made DepthPerRound dynamic, updated the go lib

--- shared/services/proposals/node-tree-manager.go
+++ shared/services/proposals/node-tree-manager.go
@@ -93,7 +93,7 @@ func NewNodeTreeManager(log *log.ColorLogger, cfg *config.RocketPoolConfig) (*No
 }
 
 // Create a node voting tree from a voting info snapshot and the node's index
-func (m *NodeTreeManager) CreateNodeVotingTree(snapshot *VotingInfoSnapshot, rpNodeIndex uint64, networkTreeNodeIndex uint64) *NodeVotingTree {
+func (m *NodeTreeManager) CreateNodeVotingTree(snapshot *VotingInfoSnapshot, rpNodeIndex uint64, networkTreeNodeIndex uint64, depthPerRound uint64) *NodeVotingTree {
 	address := &snapshot.Info[rpNodeIndex].NodeAddress
 	leaves := make([]*types.VotingTreeNode, len(snapshot.Info))
 	zeroHash := getHashForBalance(common.Big0)
@@ -113,7 +113,7 @@ func (m *NodeTreeManager) CreateNodeVotingTree(snapshot *VotingInfoSnapshot, rpN
 
 	// Make the tree
 	network := m.cfg.Smartnode.Network.Value.(cfgtypes.Network)
-	tree := CreateTreeFromLeaves(snapshot.BlockNumber, network, leaves, networkTreeNodeIndex)
+	tree := CreateTreeFromLeaves(snapshot.BlockNumber, network, leaves, networkTreeNodeIndex, depthPerRound)
 	return &NodeVotingTree{
 		Address:    *address,
 		NodeIndex:  rpNodeIndex,
