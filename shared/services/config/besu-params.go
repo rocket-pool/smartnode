0f0673b035a4f4130a498808e38d626ab6a85aa6
Fornax <23104993+fornax2@users.noreply.github.com>
2024-01-07 10:14:00 -0300

    Add Besu hotfix

--- shared/services/config/besu-params.go
+++ shared/services/config/besu-params.go
@@ -6,8 +6,8 @@ import (
 
 // Constants
 const (
-	besuTagTest          string = "hyperledger/besu:23.10.3"
-	besuTagProd          string = "hyperledger/besu:23.10.3"
+	besuTagTest          string = "hyperledger/besu:23.10.3-hotfix"
+	besuTagProd          string = "hyperledger/besu:23.10.3-hotfix"
 	besuEventLogInterval int    = 1000
 	besuMaxPeers         uint16 = 25
 	besuStopSignal       string = "SIGTERM"
