da8317085aa573ac27f5569ae500c22ab1735943
Fornax <23104993+0xfornax@users.noreply.github.com>
2024-01-13 23:07:23 -0300

    Fix typo

--- shared/services/rewards/types.go
+++ shared/services/rewards/types.go
@@ -73,7 +73,7 @@ type TotalRewards struct {
 	TotalSmoothingPoolEth        *QuotedBigInt `json:"totalSmoothingPoolEth"`
 	PoolStakerSmoothingPoolEth   *QuotedBigInt `json:"poolStakerSmoothingPoolEth"`
 	NodeOperatorSmoothingPoolEth *QuotedBigInt `json:"nodeOperatorSmoothingPoolEth"`
-	TotalNodeWeight              *QuotedBigInt `json:"totalNodeWeighti,omitempty"`
+	TotalNodeWeight              *QuotedBigInt `json:"totalNodeWeight,omitempty"`
 }
 
 // Minipool stats
