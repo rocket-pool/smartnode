d3fda372ba7f4cf9a33ddd35f1a2a8b81b04b2fa
Fornax <23104993+0xfornax@users.noreply.github.com>
2024-01-30 18:31:19 -0300

    New prysm version.

--- shared/services/config/prysm-config.go
+++ shared/services/config/prysm-config.go
@@ -5,10 +5,10 @@ import (
 )
 
 const (
-	prysmBnTest             string = "rocketpool/prysm:v4.2.0"
-	prysmBnProd             string = "rocketpool/prysm:v4.2.0"
-	prysmVcTest             string = "rocketpool/prysm:v4.2.0"
-	prysmVcProd             string = "rocketpool/prysm:v4.2.0"
+	prysmBnTest             string = "rocketpool/prysm:v4.2.1"
+	prysmBnProd             string = "rocketpool/prysm:v4.2.1"
+	prysmVcTest             string = "rocketpool/prysm:v4.2.1"
+	prysmVcProd             string = "rocketpool/prysm:v4.2.1"
 	defaultPrysmRpcPort     uint16 = 5053
 	defaultPrysmOpenRpcPort string = string(config.RPC_Closed)
 	defaultPrysmMaxPeers    uint16 = 70
