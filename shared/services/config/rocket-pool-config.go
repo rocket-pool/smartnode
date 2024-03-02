e55e433e52e08ad450beac57e65e832c9b312c05
Scott Willeke <scott@willeke.com>
2024-03-02 15:10:16 -0800

    fix: add alertmanager port to duplicate port check

--- shared/services/config/rocket-pool-config.go
+++ shared/services/config/rocket-pool-config.go
@@ -1520,6 +1520,7 @@ func (cfg *RocketPoolConfig) Validate() []string {
 	portMap, errors = addAndCheckForDuplicate(portMap, cfg.Grafana.Port, errors)
 	portMap, errors = addAndCheckForDuplicate(portMap, cfg.MevBoost.Port, errors)
 	portMap, errors = addAndCheckForDuplicate(portMap, cfg.Prometheus.Port, errors)
+	portMap, errors = addAndCheckForDuplicate(portMap, cfg.Alertmanager.Port, errors)
 	_, errors = addAndCheckForDuplicate(portMap, cfg.Lighthouse.P2pQuicPort, errors)
 
 	return errors
