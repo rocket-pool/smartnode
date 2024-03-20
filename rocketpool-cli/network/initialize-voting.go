aba2b8ab22b41dbdefbc3e3303aba82789f51884
Joe Clapis <jclapis@outlook.com>
2024-03-19 22:16:34 -0400

    Added confirmation to init-voting

--- rocketpool-cli/network/initialize-voting.go
+++ rocketpool-cli/network/initialize-voting.go
@@ -3,6 +3,7 @@ package network
 import (
 	"fmt"
 
+	"github.com/rocket-pool/smartnode/shared/services/gas"
 	"github.com/rocket-pool/smartnode/shared/services/rocketpool"
 	cliutils "github.com/rocket-pool/smartnode/shared/utils/cli"
 	"github.com/urfave/cli"
@@ -36,18 +37,31 @@ func initializeVoting(c *cli.Context) error {
 		return nil
 	}
 
+	// Assign max fees
+	err = gas.AssignMaxFeeAndLimit(resp.GasInfo, rp, c.Bool("yes"))
+	if err != nil {
+		return err
+	}
+
+	// Prompt for confirmation
+	if !(c.Bool("yes") || cliutils.Confirm("Are you sure you want to initialize voting?")) {
+		fmt.Println("Cancelled.")
+		return nil
+	}
+
+	// Initialize voting
 	response, err := rp.InitializeVoting()
 	if err != nil {
 		return fmt.Errorf("error calling initialize-voting: %w", err)
 	}
 
-	fmt.Printf("Calling initialize voting...\n")
+	fmt.Printf("Initializing voting...\n")
 	cliutils.PrintTransactionHash(rp, response.TxHash)
 	if _, err = rp.WaitForTransaction(response.TxHash); err != nil {
-		fmt.Printf("Could not call initialize voting: %s.\n", err)
-	} else {
-		fmt.Println("Successfully called initialize voting.")
+		return fmt.Errorf("error initializing voting: %w", err)
 	}
-	return nil
 
+	// Log & return
+	fmt.Println("Successfully initialized voting.")
+	return nil
 }
