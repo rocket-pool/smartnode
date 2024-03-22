244e21395d3c5358eb23d25eeb1ba824b5a920f3
Fornax <23104993+fornax2@users.noreply.github.com>
2024-03-21 22:21:39 -0300

    New LH version.

--- shared/services/config/lighthouse-config.go
+++ shared/services/config/lighthouse-config.go
@@ -6,10 +6,10 @@ import (
 )
 
 const (
-	lighthouseTagPortableTest string = "sigp/lighthouse:v5.1.0"
-	lighthouseTagPortableProd string = "sigp/lighthouse:v5.1.0"
-	lighthouseTagModernTest   string = "sigp/lighthouse:v5.1.0-modern"
-	lighthouseTagModernProd   string = "sigp/lighthouse:v5.1.0-modern"
+	lighthouseTagPortableTest string = "sigp/lighthouse:v5.1.2"
+	lighthouseTagPortableProd string = "sigp/lighthouse:v5.1.2"
+	lighthouseTagModernTest   string = "sigp/lighthouse:v5.1.2-modern"
+	lighthouseTagModernProd   string = "sigp/lighthouse:v5.1.2-modern"
 	defaultLhMaxPeers         uint16 = 100
 )
 
