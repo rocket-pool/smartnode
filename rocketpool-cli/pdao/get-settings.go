69c15fe615d0d449fa458da80d579a968c467275
Fornax <23104993+fornax2@users.noreply.github.com>
2024-04-11 00:09:01 -0300

    Add missing Houston check

--- rocketpool-cli/pdao/get-settings.go
+++ rocketpool-cli/pdao/get-settings.go
@@ -17,6 +17,16 @@ func getSettings(c *cli.Context) error {
 	}
 	defer rp.Close()
 
+	// Check for Houston
+	houston, err := rp.IsHoustonDeployed()
+	if err != nil {
+		return fmt.Errorf("error checking if Houston has been deployed: %w", err)
+	}
+	if !houston.IsHoustonDeployed {
+		fmt.Println("This command cannot be used until Houston has been deployed.")
+		return nil
+	}
+
 	// Get all PDAO settings
 	response, err := rp.PDAOGetSettings()
 	if err != nil {
