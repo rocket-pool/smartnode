51ee9eef14bc3b5ba7e885c48cf2321829462f6b
Jacob Shufro <jacob@shuf.ro>
2024-04-18 22:21:18 -0400

    Remove sszgen.sh

--- sszgen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Generates the ssz encoding methods for eth2 types with fastssz
-# Install sszgen with `go get github.com/ferranbt/fastssz/sszgen`
-rm -f ./shared/types/eth2/types_encoding.go
-sszgen --path ./shared/types/eth2
