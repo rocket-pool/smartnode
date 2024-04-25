14460482e9765a295d6cdb7aa30a3ebce468e61d
Fornax <23104993+fornax2@users.noreply.github.com>
2024-04-25 17:07:50 -0300

    Remove Geth's enable PBSS param.

--- shared/services/config/geth-params.go
+++ shared/services/config/geth-params.go
@@ -27,9 +27,6 @@ type GethConfig struct {
 	// The max number of events to query in a single event log query
 	EventLogInterval int `yaml:"-"`
 
-	// The flag for enabling PBSS
-	EnablePbss config.Parameter `yaml:"enablePbss,omitempty"`
-
 	// Max number of P2P peers to connect to
 	MaxPeers config.Parameter `yaml:"maxPeers,omitempty"`
 
@@ -59,17 +56,6 @@ func NewGethConfig(cfg *RocketPoolConfig) *GethConfig {
 		},
 
 		EventLogInterval: gethEventLogInterval,
-
-		EnablePbss: config.Parameter{
-			ID:                 "enablePbss",
-			Name:               "Enable PBSS",
-			Description:        "Enable Geth's new path-based state scheme. With this enabled, you will no longer need to manually prune Geth; it will automatically prune its database in real-time.\n\n[orange]NOTE:\nEnabling this will require you to remove and resync your Geth DB using `rocketpool service resync-eth1`.\nYou will need a synced fallback node configured before doing this, or you will no longer be able to attest until it has finished resyncing!",
-			Type:               config.ParameterType_Bool,
-			Default:            map[config.Network]interface{}{config.Network_All: true},
-			AffectsContainers:  []config.ContainerID{config.ContainerID_Eth1},
-			CanBeBlank:         false,
-			OverwriteOnUpgrade: false,
-		},
 
 		MaxPeers: config.Parameter{
 			ID:                 "maxPeers",
@@ -132,7 +118,6 @@ func calculateGethPeers() uint16 {
 // Get the config.Parameters for this config
 func (cfg *GethConfig) GetParameters() []*config.Parameter {
 	return []*config.Parameter{
-		&cfg.EnablePbss,
 		&cfg.MaxPeers,
 		&cfg.EvmTimeout,
 		&cfg.ContainerTag,
