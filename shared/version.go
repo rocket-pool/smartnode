17e55175ca070201926717188ceeb3e034718bb7
Joe Clapis <jclapis@outlook.com>
2024-05-01 13:37:06 -0400

    Switched versions back to dev builds

--- shared/version.go
+++ shared/version.go
@@ -1,6 +1,6 @@
 package shared
 
-const RocketPoolVersion string = "2.0.0-b1"
+const RocketPoolVersion string = "2.0.0-dev"
 
 const Logo string = `______           _        _    ______           _
 | ___ \         | |      | |   | ___ \         | |
