b7eff7bb9dfc65eaf8ea25210694867bb61c6d2e
Jacob Shufro <jacob@shuf.ro>
2024-05-22 20:16:40 -0400

    Switch underlying ssz bigint type to math/big.Int and only convert to uint256.Int when serializing or parsing

--- shared/services/rewards/ssz_types/big/uint256.go
+++ shared/services/rewards/ssz_types/big/uint256.go
@@ -1,34 +1,51 @@
 package big
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"math/big"
 
 	ssz "github.com/ferranbt/fastssz"
 	"github.com/holiman/uint256"
 )
 
+var Overflow = errors.New("uint256 overflow")
+var Negative = errors.New("uint256 can't be negative before serializing")
+
+// Wraps big.Int but will be checked for sign/overflow when serializing SSZ
 type Uint256 struct {
-	repr *uint256.Int `ssz:"-"`
+	*big.Int
 }
 
-func NewUint256(i uint64) Uint256 {
-	out := Uint256{}
-	out.repr = uint256.NewInt(i)
-	return out
-}
-
-func (u *Uint256) Unwrap() *uint256.Int {
-	out := uint256.NewInt(0)
-	out.Set(u.repr)
-	return out
+func NewUint256(i int64) Uint256 {
+	return Uint256{big.NewInt(i)}
 }
 
 func (u *Uint256) SizeSSZ() (size int) {
 	return 32
 }
 
+func (u *Uint256) ToUint256() (*uint256.Int, error) {
+	// Check sign
+	if u.Sign() < 0 {
+		return nil, Negative
+	}
+
+	s, overflow := uint256.FromBig(u.Int)
+	if overflow {
+		return nil, Overflow
+	}
+	return s, nil
+}
+
 func (u *Uint256) MarshalSSZTo(buf []byte) ([]byte, error) {
-	bytes, err := u.repr.MarshalSSZ()
+	s, err := u.ToUint256()
+	if err != nil {
+		return nil, err
+	}
+
+	bytes, err := s.MarshalSSZ()
 	if err != nil {
 		return nil, err
 	}
@@ -47,23 +64,42 @@ func (u *Uint256) HashTreeRootWith(hh ssz.HashWalker) (err error) {
 }
 
 func (u *Uint256) UnmarshalSSZ(buf []byte) error {
-	u.repr = uint256.NewInt(0)
-	return u.repr.UnmarshalSSZ(buf)
+	repr := uint256.NewInt(0)
+	err := repr.UnmarshalSSZ(buf)
+	if err != nil {
+		return err
+	}
+	u.Int = repr.ToBig()
+	return nil
 }
 
 func (u *Uint256) String() string {
-	return u.repr.String()
+	return u.Int.String()
 }
 
 func (u *Uint256) UnmarshalJSON(data []byte) error {
-	u.repr = uint256.NewInt(0)
-	return u.repr.UnmarshalJSON(data)
+	var s string
+	err := json.Unmarshal(data, &s)
+	if err != nil {
+		return err
+	}
+	u.Int = big.NewInt(0)
+	return u.Int.UnmarshalJSON([]byte(s))
 }
 
-func (u Uint256) MarshalJSON() ([]byte, error) {
-	return u.repr.MarshalJSON()
+func (u *Uint256) MarshalJSON() ([]byte, error) {
+	s, err := u.Int.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+	return []byte(fmt.Sprintf("\"%s\"", s)), nil
 }
 
-func (u Uint256) ToBig() *big.Int {
-	return u.repr.ToBig()
+func (u *Uint256) Bytes32() ([32]byte, error) {
+	s, err := u.ToUint256()
+	if err != nil {
+		return [32]byte{}, err
+	}
+
+	return s.Bytes32(), nil
 }
