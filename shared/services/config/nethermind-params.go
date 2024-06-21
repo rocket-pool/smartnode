cfea57582c9afaf260df3e7a13abf8fc1659aaac
Fornax <23104993+fornax2@users.noreply.github.com>
2024-06-21 12:02:21 -0300

    NM v1.27.0

--- shared/services/config/nethermind-params.go
+++ shared/services/config/nethermind-params.go
@@ -9,8 +9,8 @@ import (
 
 // Constants
 const (
-	nethermindTagProd          string = "nethermind/nethermind:1.26.0"
-	nethermindTagTest          string = "nethermind/nethermind:1.26.0"
+	nethermindTagProd          string = "nethermind/nethermind:1.27.0"
+	nethermindTagTest          string = "nethermind/nethermind:1.27.0"
 	nethermindEventLogInterval int    = 1000
 	nethermindStopSignal       string = "SIGTERM"
 )
