10d391691b2c38b17cd2bed7045c64bfba85484d
Fornax <23104993+fornax2@users.noreply.github.com>
2024-07-10 20:20:43 -0300

    Re-add clear-voting and set-voting with instructions pointing to new cmds.

--- rocketpool-cli/node/commands.go
+++ rocketpool-cli/node/commands.go
@@ -1,6 +1,8 @@
 package node
 
 import (
+	"fmt"
+
 	"github.com/urfave/cli"
 
 	cliutils "github.com/rocket-pool/smartnode/shared/utils/cli"
@@ -638,6 +640,35 @@ func RegisterCommands(app *cli.App, name string, aliases []string) {
 					// Run
 					return nodeSend(c, amount, token, c.Args().Get(2))
 
+				},
+			},
+
+			{
+				Name:      "set-voting-delegate",
+				Aliases:   []string{"sv"},
+				Usage:     "(DEPRECATED) Use `rocketpool pdao set-signalling-address` instead",
+				UsageText: "rocketpool node set-voting-delegate",
+				Action: func(c *cli.Context) error {
+
+					// Run
+					fmt.Println("(DEPRECATED) Use `rocketpool pdao set-signalling-address` instead.")
+					fmt.Println("For more information please refer to the Governance article on Medium (https://medium.com/rocket-pool/rocket-pool-protocol-dao-governance-a3c3e92904e0).")
+					return nil
+
+				},
+			},
+
+			{
+				Name:      "clear-voting-delegate",
+				Aliases:   []string{"cv"},
+				Usage:     "(DEPRECATED) Use `rocketpool pdao clear-signalling-address` instead",
+				UsageText: "rocketpool node clear-voting-delegate",
+				Action: func(c *cli.Context) error {
+
+					// Run
+					fmt.Println("(DEPRECATED) Use `rocketpool pdao clear-signalling-address` instead.")
+					fmt.Println("For more information please refer to the Governance article on Medium (https://medium.com/rocket-pool/rocket-pool-protocol-dao-governance-a3c3e92904e0).")
+					return nil
 				},
 			},
 
