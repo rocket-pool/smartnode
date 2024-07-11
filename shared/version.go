a85f6f99c19cb508f60a0ba2b8ff0111d7267097
Fornax <23104993+fornax2@users.noreply.github.com>
2024-07-11 11:09:35 -0300

    v1.13.7

--- shared/version.go
+++ shared/version.go
@@ -1,6 +1,6 @@
 package shared
 
-const RocketPoolVersion string = "1.13.7-dev"
+const RocketPoolVersion string = "1.13.7"
 
 const Logo string = `______           _        _    ______           _
 | ___ \         | |      | |   | ___ \         | |
