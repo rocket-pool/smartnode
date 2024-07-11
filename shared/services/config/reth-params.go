eb9c6cdfcc47acfe1031dd530e0ef101f780bea0
Fornax <23104993+fornax2@users.noreply.github.com>
2024-07-11 10:58:08 -0300

    Reth v1.0.1

--- shared/services/config/reth-params.go
+++ shared/services/config/reth-params.go
@@ -9,8 +9,8 @@ import (
 
 // Constants
 const (
-	rethTagProd          string = "ghcr.io/paradigmxyz/reth:v1.0.0"
-	rethTagTest          string = "ghcr.io/paradigmxyz/reth:v1.0.0"
+	rethTagProd          string = "ghcr.io/paradigmxyz/reth:v1.0.1"
+	rethTagTest          string = "ghcr.io/paradigmxyz/reth:v1.0.1"
 	rethEventLogInterval int    = 1000
 	rethStopSignal       string = "SIGTERM"
 )
