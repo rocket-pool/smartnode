04b72c29826531b9837fc2912a841f106201c103
Jacob Shufro <jacob@shuf.ro>
2024-07-21 17:56:10 -0400

    Move command registration to commands package

--- rocketpool-cli/rocketpool-cli.go
+++ rocketpool-cli/rocketpool-cli.go
@@ -7,16 +7,7 @@ import (
 	"github.com/urfave/cli/v2"
 
 	"github.com/rocket-pool/smartnode/v2/assets"
-	"github.com/rocket-pool/smartnode/v2/rocketpool-cli/commands/auction"
-	"github.com/rocket-pool/smartnode/v2/rocketpool-cli/commands/minipool"
-	"github.com/rocket-pool/smartnode/v2/rocketpool-cli/commands/network"
-	"github.com/rocket-pool/smartnode/v2/rocketpool-cli/commands/node"
-	"github.com/rocket-pool/smartnode/v2/rocketpool-cli/commands/odao"
-	"github.com/rocket-pool/smartnode/v2/rocketpool-cli/commands/pdao"
-	"github.com/rocket-pool/smartnode/v2/rocketpool-cli/commands/queue"
-	"github.com/rocket-pool/smartnode/v2/rocketpool-cli/commands/security"
-	"github.com/rocket-pool/smartnode/v2/rocketpool-cli/commands/service"
-	"github.com/rocket-pool/smartnode/v2/rocketpool-cli/commands/wallet"
+	"github.com/rocket-pool/smartnode/v2/rocketpool-cli/commands"
 	"github.com/rocket-pool/smartnode/v2/rocketpool-cli/settings"
 	"github.com/rocket-pool/smartnode/v2/rocketpool-cli/utils"
 )
@@ -98,16 +89,7 @@ func newCliApp() *cli.App {
 	app.Flags = utils.AppendFlags(app.Flags)
 
 	// Register commands
-	auction.RegisterCommands(app, "auction", []string{"a"})
-	minipool.RegisterCommands(app, "minipool", []string{"m"})
-	network.RegisterCommands(app, "network", []string{"e"})
-	node.RegisterCommands(app, "node", []string{"n"})
-	odao.RegisterCommands(app, "odao", []string{"o"})
-	pdao.RegisterCommands(app, "pdao", []string{"p"})
-	queue.RegisterCommands(app, "queue", []string{"q"})
-	security.RegisterCommands(app, "security", []string{"c"})
-	service.RegisterCommands(app, "service", []string{"s"})
-	wallet.RegisterCommands(app, "wallet", []string{"w"})
+	commands.RegisterCommands(app)
 
 	var snSettings *settings.SmartNodeSettings
 	app.Before = func(c *cli.Context) error {
