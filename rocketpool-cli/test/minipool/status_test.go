4410ce3183bc73c9242ae51a3cb47c5ff7134f89
Jacob Shufro <jacob@shuf.ro>
2024-07-21 16:04:37 -0400

    Add rocketpool minipool status test with only finalised minipools

--- rocketpool-cli/test/minipool/status_test.go
+++ rocketpool-cli/test/minipool/status_test.go
@@ -1,6 +1,7 @@
 package minipool
 
 import (
+	"math/big"
 	"strings"
 	"testing"
 
@@ -34,6 +35,52 @@ func TestMinipoolStatusNoMinipools(t *testing.T) {
 
 	// Cli output should mention no minipools available
 	expectedResponse := "The node does not have any minipools yet."
+	if !strings.Contains(result.Stdout, expectedResponse) {
+		t.Fatalf(`expected message "%s", got "%s"`, expectedResponse, result.Stdout)
+	}
+}
+
+func newMinipoolDetails() *api.MinipoolDetails {
+	out := new(api.MinipoolDetails)
+
+	// Avoid segfaults by initializing.
+	// TODO: we shouldn't use big.Int pointers in api request/response types.
+	out.Node.DepositBalance = big.NewInt(0)
+	out.Node.RefundBalance = big.NewInt(0)
+	out.User.DepositBalance = big.NewInt(0)
+
+	out.Balances.Eth = big.NewInt(0)
+	out.Balances.Reth = big.NewInt(0)
+	out.Balances.Rpl = big.NewInt(0)
+	out.Balances.FixedSupplyRpl = big.NewInt(0)
+
+	out.NodeShareOfEthBalance = big.NewInt(0)
+
+	out.Validator.Balance = big.NewInt(0)
+	out.Validator.NodeBalance = big.NewInt(0)
+
+	return out
+}
+
+func TestMinipoolStatusOnlyFinalized(t *testing.T) {
+	cliTest := newMinipoolTest(t)
+	response := types.ApiResponse[api.MinipoolStatusData]{
+		Data: &api.MinipoolStatusData{
+			Minipools: []api.MinipoolDetails{},
+		},
+	}
+	minipool1 := newMinipoolDetails()
+	minipool1.Finalised = true
+	response.Data.Minipools = append(response.Data.Minipools, *minipool1)
+	minipool2 := newMinipoolDetails()
+	minipool2.Finalised = true
+	response.Data.Minipools = append(response.Data.Minipools, *minipool2)
+	result := cliTest.RepliesWith(response).Run("minipool", "status")
+	if result.Error != nil {
+		t.Fatal(result.Error)
+	}
+
+	expectedResponse := "All of this node's minipools have been finalized."
 	if !strings.Contains(result.Stdout, expectedResponse) {
 		t.Fatalf(`expected message "%s", got "%s"`, expectedResponse, result.Stdout)
 	}
