60cdbc0c67cf282607e5a259f22548734b064a42
thomaspanf <thomaspanf@gmail.com>
2024-08-26 14:44:42 -0400

    Fixed Printlns

--- rocketpool-cli/commands/pdao/initialize-voting.go
+++ rocketpool-cli/commands/pdao/initialize-voting.go
@@ -14,22 +14,22 @@ import (
 
 func initializeVotingPrompt(c *cli.Context) error {
 	fmt.Println("Thanks for initializing your voting power!")
-	fmt.Println("")
+	fmt.Println()
 	fmt.Println("You have two options:")
-	fmt.Println("")
+	fmt.Println()
 	fmt.Println("1. Vote directly (delegate vote power to yourself)")
 	fmt.Println("   This will allow you to vote on proposals directly,")
 	fmt.Println("   allowing you to personally shape the direction of the protocol.")
-	fmt.Println("")
+	fmt.Println()
 	fmt.Println("2. Delegate your vote")
 	fmt.Println("   This will delegate your vote power to someone you trust,")
 	fmt.Println("   giving them the power to vote on your behalf. You will have the option to override.")
-	fmt.Println("")
+	fmt.Println()
 	fmt.Printf("You can see a list of existing public delegates at %s,\n", "https://delegates.rocketpool.net")
 	fmt.Println("however, you can delegate to any node address.")
-	fmt.Println("")
+	fmt.Println()
 	fmt.Printf("Learn more about how this all works via: %s\n", "https://docs.rocketpool.net/guides/houston/participate#participating-in-on-chain-pdao-proposals")
-	fmt.Println("")
+	fmt.Println()
 
 	inputString := cliutils.Prompt("Please type `direct` or `delegate` to continue:", "^(?i)(direct|delegate)$", "Please type `direct` or `delegate` to continue:")
 	switch strings.ToLower(inputString) {
