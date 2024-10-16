e1c7e4145fdcbeefc805d828b48ba123a5e93861
thomaspanf <thomaspanf@gmail.com>
2024-10-16 14:44:54 -0700

    Edit welcome message

--- rocketpool-cli/rocketpool-cli.go
+++ rocketpool-cli/rocketpool-cli.go
@@ -32,14 +32,6 @@ ______           _        _    ______           _
 |    // _ \ / __| |/ / _ \ __| |  __/ _ \ / _ \| |
 | |\ \ (_) | (__|   <  __/ |_  | | | (_) | (_) | |
 \_| \_\___/ \___|_|\_\___|\__| \_|  \___/ \___/|_|
-+---------------------------------------------------+
-|    DECENTRALISED STAKING PROTOCOL FOR ETHEREUM    |
-+---------------------------------------------------+
-
-Rocket Pool is a first-of-its-kind Ethereum staking pool protocol, designed to
-be community-owned, decentralised, permissionless, & trustless.
-
-For more information about Rocket Pool, visit https://rocketpool.net
 
 Authored by the Rocket Pool Core Team
 A special thanks to the Rocket Pool community for all their contributions.
