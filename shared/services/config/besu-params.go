69a922f39f3fdea605d60181c6bc900958849342
Fornax <23104993+fornax2@users.noreply.github.com>
2024-10-20 19:41:01 -0300

    besu-v24.10.0

--- shared/services/config/besu-params.go
+++ shared/services/config/besu-params.go
@@ -6,8 +6,8 @@ import (
 
 // Constants
 const (
-	besuTagTest          string = "hyperledger/besu:24.9.1"
-	besuTagProd          string = "hyperledger/besu:24.9.1"
+	besuTagTest          string = "hyperledger/besu:24.10.0"
+	besuTagProd          string = "hyperledger/besu:24.10.0"
 	besuEventLogInterval int    = 1000
 	besuMaxPeers         uint16 = 25
 	besuStopSignal       string = "SIGTERM"
