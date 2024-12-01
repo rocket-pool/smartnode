d273012b0b12c7ac6b6cdfa67e39abffb999737c
Fornax <23104993+fornax2@users.noreply.github.com>
2024-12-01 20:34:53 -0300

    lodestar-v1.23.1

--- shared/services/config/lodestar-config.go
+++ shared/services/config/lodestar-config.go
@@ -5,8 +5,8 @@ import (
 )
 
 const (
-	lodestarTagTest         string = "chainsafe/lodestar:v1.23.0"
-	lodestarTagProd         string = "chainsafe/lodestar:v1.23.0"
+	lodestarTagTest         string = "chainsafe/lodestar:v1.23.1"
+	lodestarTagProd         string = "chainsafe/lodestar:v1.23.1"
 	defaultLodestarMaxPeers uint16 = 100
 )
 
