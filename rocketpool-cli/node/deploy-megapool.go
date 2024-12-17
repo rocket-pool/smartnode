fab21a39c141e3deca1545044e8776335962234d
Fornax <23104993+0xfornax@users.noreply.github.com>
2024-12-17 14:01:17 -0300

    Deploy megapool

---
+++ rocketpool-cli/node/deploy-megapool.go
@@ -0,0 +1,58 @@
+package node
+
+import (
+	"fmt"
+
+	"github.com/rocket-pool/smartnode/shared/services/gas"
+	"github.com/rocket-pool/smartnode/shared/services/rocketpool"
+	cliutils "github.com/rocket-pool/smartnode/shared/utils/cli"
+	"github.com/urfave/cli"
+)
+
+func deployMegapool(c *cli.Context) error {
+	// Get RP client
+	rp, err := rocketpool.NewClientFromCtx(c).WithReady()
+	if err != nil {
+		return err
+	}
+	defer rp.Close()
+
+	// Check if the node can deploy a megapool
+	canDeploy, err := rp.CanDeployMegapool()
+	if err != nil {
+		return err
+	}
+
+	if !canDeploy.CanDeploy {
+		if canDeploy.AlreadyDeployed {
+			fmt.Println("The node already has a Megapool deployed.")
+		}
+		return nil
+	}
+
+	// Assign max fees
+	err = gas.AssignMaxFeeAndLimit(canDeploy.GasInfo, rp, c.Bool("yes"))
+	if err != nil {
+		return err
+	}
+
+	// Prompt for confirmation
+	if !(c.Bool("yes") || cliutils.Confirm("Are you sure you want to deploy a Megapool?")) {
+		fmt.Println("Cancelled.")
+		return nil
+	}
+
+	response, err := rp.DeployMegapool()
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Deploying Megapool...\n")
+	cliutils.PrintTransactionHash(rp, response.TxHash)
+	if _, err = rp.WaitForTransaction(response.TxHash); err != nil {
+		return err
+	}
+
+	fmt.Println("Megapool deployed successfully!")
+	return nil
+}
---
+++ rocketpool/api/node/deploy-megapool.go
@@ -0,0 +1,76 @@
+package node
+
+import (
+	"github.com/rocket-pool/rocketpool-go/megapool"
+	"github.com/rocket-pool/rocketpool-go/node"
+	"github.com/rocket-pool/smartnode/shared/services"
+	"github.com/rocket-pool/smartnode/shared/types/api"
+	"github.com/urfave/cli"
+)
+
+func canDeployMegapool(c *cli.Context) (*api.CanDeployMegapoolResponse, error) {
+	// Get services
+	if err := services.RequireNodeRegistered(c); err != nil {
+		return nil, err
+	}
+	w, err := services.GetWallet(c)
+	if err != nil {
+		return nil, err
+	}
+	rp, err := services.GetRocketPool(c)
+	if err != nil {
+		return nil, err
+	}
+
+	// Response
+	response := api.CanDeployMegapoolResponse{}
+
+	// Get node account
+	nodeAccount, err := w.GetNodeAccount()
+	if err != nil {
+		return nil, err
+	}
+
+	// Check if the megapool is already deployed
+	alreadyDeployed, err := megapool.GetMegapoolDeployed(rp, nodeAccount.Address, nil)
+	if err != nil {
+		return nil, err
+	}
+	response.AlreadyDeployed = alreadyDeployed
+
+	// Check if the node can deploy a megapool
+	gasInfo, err := node.EstimateDeployMegapool(rp, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return response
+	response.CanDeploy = !alreadyDeployed
+	response.GasInfo = gasInfo
+	return &response, nil
+}
+
+func deployMegapool(c *cli.Context) (*api.DeployMegapoolResponse, error) {
+	// Get services
+	if err := services.RequireNodeRegistered(c); err != nil {
+		return nil, err
+	}
+
+	rp, err := services.GetRocketPool(c)
+	if err != nil {
+		return nil, err
+	}
+
+	// Response
+	response := api.DeployMegapoolResponse{}
+
+	// Deploy megapool
+	txHash, err := node.DeployMegapool(rp, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return response
+	response.TxHash = txHash
+	return &response, nil
+}
