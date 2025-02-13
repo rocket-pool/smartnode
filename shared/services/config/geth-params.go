4825e8b3bfd8f566c00f918d632d64720f132d97
Fornax <23104993+0xfornax@users.noreply.github.com>
2025-02-13 13:43:56 -0300

    geth-v1.15.1

--- shared/services/config/geth-params.go
+++ shared/services/config/geth-params.go
@@ -8,8 +8,8 @@ import (
 
 // Constants
 const (
-	gethTagProd          string = "ethereum/client-go:v1.14.13"
-	gethTagTest          string = "ethereum/client-go:v1.14.13"
+	gethTagProd          string = "ethereum/client-go:v1.15.1"
+	gethTagTest          string = "ethereum/client-go:v1.15.1"
 	gethEventLogInterval int    = 1000
 	gethStopSignal       string = "SIGTERM"
 )
