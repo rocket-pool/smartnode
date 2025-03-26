bca1db9ce8fc4f1a7815fdff207eb5ca65225e91
Fornax <23104993+fornax2@users.noreply.github.com>
2025-03-26 09:19:48 -0300

    Bump version

--- shared/version.go
+++ shared/version.go
@@ -1,6 +1,6 @@
 package shared
 
-const RocketPoolVersion string = "1.15.3"
+const RocketPoolVersion string = "1.15.4"
 
 const Logo string = `______           _        _    ______           _
 | ___ \         | |      | |   | ___ \         | |
