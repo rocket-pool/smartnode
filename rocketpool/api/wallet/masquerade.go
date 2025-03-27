eb41853df5c57334819307027f387bc4bfce11d8
thomaspanf <thomaspanf@gmail.com>
2025-03-27 14:38:43 -0700

    Add masquerade calls to wallet interface

--- rocketpool/api/wallet/masquerade.go
+++ rocketpool/api/wallet/masquerade.go
@@ -1,6 +1,8 @@
 package wallet
 
 import (
+	"fmt"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/rocket-pool/smartnode/shared/services"
 	"github.com/rocket-pool/smartnode/shared/types/api"
@@ -14,16 +16,15 @@ func masquerade(c *cli.Context, address common.Address) (*api.MasqueradeResponse
 		return nil, err
 	}
 
-	//TODO leaving this out for now
-	// w, err := services.GetWallet(c)
-	// if err != nil {
-	// 	return nil, err
-	// }
+	w, err := services.GetWallet(c)
+	if err != nil {
+		return nil, err
+	}
 
-	// err = w.MasqueradeAsAddress(address)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("Error masquerading as address %s", address.Hex())
-	// }
+	err = w.MasqueradeAsAddress(address)
+	if err != nil {
+		return nil, fmt.Errorf("Error masquerading as address %s", address.Hex())
+	}
 
 	response := api.MasqueradeResponse{}
 
