d258fa0990374621ebd49599e65be05ca984b46d
thomaspanf <thomaspanf@gmail.com>
2025-03-31 22:41:44 -0700

    Restore address deletes the address file, wallet status adjustments

--- rocketpool-cli/wallet/status.go
+++ rocketpool-cli/wallet/status.go
@@ -3,6 +3,7 @@ package wallet
 import (
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/urfave/cli"
 
 	"github.com/rocket-pool/smartnode/shared/services/rocketpool"
@@ -34,14 +35,19 @@ func getStatus(c *cli.Context) error {
 	}
 
 	// Print status & return
-	if status.WalletInitialized {
-		fmt.Println("The node wallet is initialized.")
+	blankAddress := common.Address{}
+	if status.WalletInitialized && status.MasqueradeAddress != blankAddress {
+		fmt.Printf("The node wallet is initialized, but you are currently masquerading as %s%s%s\n", colorBlue, status.MasqueradeAddress.Hex(), colorReset)
 		fmt.Printf("Wallet Address: %s\n", status.AccountAddress.Hex())
-		fmt.Printf("Node Address: %s\n", status.NodeAddress.Hex())
-
+		fmt.Printf("%sDue to this mismatch, the node cannot submit transactions. Use the command 'rocketpool wallet restore-address' to end masquerading and restore your wallet address.%s", colorYellow, colorReset)
 	} else {
-		fmt.Println("The node wallet has not been initialized.")
+		fmt.Println("The node wallet is initialized")
+		fmt.Printf("Wallet Address: %s", status.AccountAddress.Hex())
 	}
+	if !status.WalletInitialized {
+		fmt.Print("The node wallet has not been initialized.")
+	}
+	fmt.Println()
 	return nil
 
 }
