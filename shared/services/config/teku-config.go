c3eb0bc57e7bdf4e6272b9b69a811fe8dc74c8b4
Fornax <23104993+fornax2@users.noreply.github.com>
2025-04-02 11:04:48 -0300

    teku-v25.4.0

--- shared/services/config/teku-config.go
+++ shared/services/config/teku-config.go
@@ -6,8 +6,8 @@ import (
 )
 
 const (
-	tekuTagTest         string = "consensys/teku:25.3.0"
-	tekuTagProd         string = "consensys/teku:25.3.0"
+	tekuTagTest         string = "consensys/teku:25.4.0"
+	tekuTagProd         string = "consensys/teku:25.4.0"
 	defaultTekuMaxPeers uint16 = 100
 )
 
