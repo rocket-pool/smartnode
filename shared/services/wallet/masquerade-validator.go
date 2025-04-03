f786577f99ac12b43e000a198ecee4c45e79ab2b
thomaspanf <thomaspanf@gmail.com>
2025-04-03 12:17:47 -0700

    Return errors for unsupported methods in masqueradeWallet

--- shared/services/wallet/masquerade-validator.go
+++ shared/services/wallet/masquerade-validator.go
@@ -1,317 +1,78 @@
 package wallet
 
 import (
-	"bytes"
-	"errors"
-	"fmt"
-	"os"
-	"strings"
-
 	"github.com/rocket-pool/rocketpool-go/types"
 	rptypes "github.com/rocket-pool/rocketpool-go/types"
-	"github.com/rocket-pool/smartnode/shared/utils/validator"
 	eth2types "github.com/wealdtech/go-eth2-types/v2"
-	eth2util "github.com/wealdtech/go-eth2-util"
 )
-
-//TODO receivers that should be inaccessible while masquerading should return an error
 
 // Get the number of validator keys recorded in the wallet
 func (w *masqueradeWallet) GetValidatorKeyCount() (uint, error) {
-
-	// Check wallet is initialized
-	if !w.IsInitialized() {
-		return 0, errors.New("Wallet is not initialized")
-	}
-
-	// Return validator key count
-	return w.ws.NextAccount, nil
+	return 0, ErrIsMasquerading
 
 }
 
 // Get a validator key by index
 func (w *masqueradeWallet) GetValidatorKeyAt(index uint) (*eth2types.BLSPrivateKey, error) {
-
-	// Check wallet is initialized
-	if !w.IsInitialized() {
-		return nil, errors.New("Wallet is not initialized")
-	}
-
-	// Return validator key
-	key, _, err := w.getValidatorPrivateKey(index)
-	return key, err
+	return nil, ErrIsMasquerading
 
 }
 
 // Get a validator key by public key
 func (w *masqueradeWallet) GetValidatorKeyByPubkey(pubkey rptypes.ValidatorPubkey) (*eth2types.BLSPrivateKey, error) {
-
-	// Check wallet is initialized
-	if !w.IsInitialized() {
-		return nil, errors.New("Wallet is not initialized")
-	}
-
-	// Load the key from the wallet's keystores
-	return w.LoadValidatorKey(pubkey)
+	return nil, ErrIsMasquerading
 
 }
 
 // Create a new validator key
 func (w *masqueradeWallet) CreateValidatorKey() (*eth2types.BLSPrivateKey, error) {
-
-	// Check wallet is initialized
-	if !w.IsInitialized() {
-		return nil, errors.New("Wallet is not initialized")
-	}
-
-	// Get & increment account index
-	index := w.ws.NextAccount
-	w.ws.NextAccount++
-
-	// Get validator key
-	key, path, err := w.getValidatorPrivateKey(index)
-	if err != nil {
-		return nil, err
-	}
-
-	// Update keystores
-	err = w.StoreValidatorKey(key, path)
-	if err != nil {
-		return nil, err
-	}
-
-	// Return validator key
-	return key, nil
+	return nil, ErrIsMasquerading
 
 }
 
 // Stores a validator key into all of the wallet's keystores
 func (w *masqueradeWallet) StoreValidatorKey(key *eth2types.BLSPrivateKey, path string) error {
-
-	for name := range w.keystores {
-		// Update the keystore in the wallet - using an iterator variable only runs it on the local copy
-		if err := w.keystores[name].StoreValidatorKey(key, path); err != nil {
-			return fmt.Errorf("Could not store %s validator key: %w", name, err)
-		}
-	}
-
-	// Return validator key
-	return nil
-
+	return ErrIsMasquerading
 }
 
 // Loads a validator key from the wallet's keystores
 func (w *masqueradeWallet) LoadValidatorKey(pubkey types.ValidatorPubkey) (*eth2types.BLSPrivateKey, error) {
-
-	errors := []string{}
-	// Try loading the key from all of the keystores, caching errors but not breaking on them
-	for name := range w.keystores {
-		key, err := w.keystores[name].LoadValidatorKey(pubkey)
-		if err != nil {
-			errors = append(errors, err.Error())
-		}
-		if key != nil {
-			return key, nil
-		}
-	}
-
-	if len(errors) > 0 {
-		// If there were errors, return them
-		return nil, fmt.Errorf("encountered the following errors while trying to load the key for validator %s:\n%s", pubkey.Hex(), strings.Join(errors, "\n"))
-	} else {
-		// If there were no errors, the key just didn't exist
-		return nil, fmt.Errorf("couldn't find the key for validator %s in any of the wallet's keystores", pubkey.Hex())
-	}
+	return nil, ErrIsMasquerading
 
 }
 
 // Deletes all of the keystore directories and persistent VC storage
 func (w *masqueradeWallet) DeleteValidatorStores() error {
-
-	for name := range w.keystores {
-		keystorePath := w.keystores[name].GetKeystoreDir()
-		err := os.RemoveAll(keystorePath)
-		if err != nil {
-			return fmt.Errorf("error deleting validator directory for %s: %w", name, err)
-		}
-	}
-
-	return nil
+	return ErrIsMasquerading
 
 }
 
 // Returns the next validator key that will be generated without saving it
 func (w *masqueradeWallet) GetNextValidatorKey() (*eth2types.BLSPrivateKey, error) {
-
-	// Check wallet is initialized
-	if !w.IsInitialized() {
-		return nil, errors.New("Wallet is not initialized")
-	}
-
-	// Get account index
-	index := w.ws.NextAccount
-
-	// Get validator key
-	key, _, err := w.getValidatorPrivateKey(index)
-	if err != nil {
-		return nil, err
-	}
-
-	// Return validator key
-	return key, nil
+	return nil, ErrIsMasquerading
 
 }
 
 // Recover a set of validator keys by their public key
 func (w *masqueradeWallet) GetValidatorKeys(startIndex uint, length uint) ([]ValidatorKey, error) {
-
-	// Check wallet is initialized
-	if !w.IsInitialized() {
-		return nil, errors.New("Wallet is not initialized")
-	}
-
-	validatorKeys := make([]ValidatorKey, 0, length)
-	for index := startIndex; index < startIndex+length; index++ {
-		key, path, err := w.getValidatorPrivateKey(index)
-		if err != nil {
-			return nil, fmt.Errorf("error getting validator key for index %d: %w", index, err)
-		}
-		validatorKey := ValidatorKey{
-			PublicKey:      types.BytesToValidatorPubkey(key.PublicKey().Marshal()),
-			PrivateKey:     key,
-			DerivationPath: path,
-			WalletIndex:    index,
-		}
-		validatorKeys = append(validatorKeys, validatorKey)
-	}
-
-	return validatorKeys, nil
+	return nil, ErrIsMasquerading
 
 }
 
 // Save a validator key
 func (w *masqueradeWallet) SaveValidatorKey(key ValidatorKey) error {
-
-	// Update account index
-	if key.WalletIndex >= w.ws.NextAccount {
-		w.ws.NextAccount = key.WalletIndex + 1
-	}
-
-	// Update keystores
-	for name := range w.keystores {
-		// Update the keystore in the wallet - using an iterator variable only runs it on the local copy
-		if err := w.keystores[name].StoreValidatorKey(key.PrivateKey, key.DerivationPath); err != nil {
-			return fmt.Errorf("could not store validator key %s in %s keystore: %w", key.PublicKey.Hex(), name, err)
-		}
-	}
-
-	// Return
-	return nil
+	return ErrIsMasquerading
 
 }
 
 // Recover a validator key by public key
 func (w *masqueradeWallet) RecoverValidatorKey(pubkey rptypes.ValidatorPubkey, startIndex uint) (uint, error) {
-
-	// Check wallet is initialized
-	if !w.IsInitialized() {
-		return 0, errors.New("Wallet is not initialized")
-	}
-
-	// Find matching validator key
-	var index uint
-	var validatorKey *eth2types.BLSPrivateKey
-	var derivationPath string
-	for index = 0; index < MaxValidatorKeyRecoverAttempts; index++ {
-		if key, path, err := w.getValidatorPrivateKey(index + startIndex); err != nil {
-			return 0, err
-		} else if bytes.Equal(pubkey.Bytes(), key.PublicKey().Marshal()) {
-			validatorKey = key
-			derivationPath = path
-			break
-		}
-	}
-
-	// Check validator key
-	if validatorKey == nil {
-		return 0, fmt.Errorf("Validator %s key not found", pubkey.Hex())
-	}
-
-	// Update account index
-	nextIndex := index + startIndex + 1
-	if nextIndex > w.ws.NextAccount {
-		w.ws.NextAccount = nextIndex
-	}
-
-	// Update keystores
-	for name := range w.keystores {
-		// Update the keystore in the wallet - using an iterator variable only runs it on the local copy
-		if err := w.keystores[name].StoreValidatorKey(validatorKey, derivationPath); err != nil {
-			return 0, fmt.Errorf("Could not store %s validator key: %w", name, err)
-		}
-	}
-
-	// Return
-	return index + startIndex, nil
+	return 0, ErrIsMasquerading
 
 }
 
 // Test recovery of a validator key by public key
 func (w *masqueradeWallet) TestRecoverValidatorKey(pubkey rptypes.ValidatorPubkey, startIndex uint) (uint, error) {
-
-	// Check wallet is initialized
-	if !w.IsInitialized() {
-		return 0, errors.New("Wallet is not initialized")
-	}
-
-	// Find matching validator key
-	var index uint
-	var validatorKey *eth2types.BLSPrivateKey
-	for index = 0; index < MaxValidatorKeyRecoverAttempts; index++ {
-		if key, _, err := w.getValidatorPrivateKey(index + startIndex); err != nil {
-			return 0, err
-		} else if bytes.Equal(pubkey.Bytes(), key.PublicKey().Marshal()) {
-			validatorKey = key
-			break
-		}
-	}
-
-	// Check validator key
-	if validatorKey == nil {
-		return 0, fmt.Errorf("Validator %s key not found", pubkey.Hex())
-	}
-
-	// Return
-	return index + startIndex, nil
-
-}
-
-// Get a validator private key by index
-func (w *masqueradeWallet) getValidatorPrivateKey(index uint) (*eth2types.BLSPrivateKey, string, error) {
-
-	// Get derivation path
-	derivationPath := fmt.Sprintf(validator.ValidatorKeyPath, index)
-
-	// Check for cached validator key
-	if validatorKey, ok := w.validatorKeys[index]; ok {
-		return validatorKey, derivationPath, nil
-	}
-
-	// Initialize BLS support
-	if err := validator.InitializeBLS(); err != nil {
-		return nil, "", fmt.Errorf("Could not initialize BLS library: %w", err)
-	}
-
-	// Get private key
-	privateKey, err := eth2util.PrivateKeyFromSeedAndPath(w.seed, derivationPath)
-	if err != nil {
-		return nil, "", fmt.Errorf("Could not get validator %d private key: %w", index, err)
-	}
-
-	// Cache validator key
-	w.validatorKeys[index] = privateKey
-
-	// Return
-	return privateKey, derivationPath, nil
+	return 0, ErrIsMasquerading
 
 }
--- shared/services/wallet/masquerade-wallet.go
+++ shared/services/wallet/masquerade-wallet.go
@@ -9,20 +9,17 @@ import (
 
 	"github.com/btcsuite/btcd/btcutil/hdkeychain"
 	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/goccy/go-json"
 	"github.com/google/uuid"
 	"github.com/rocket-pool/smartnode/shared/services/passwords"
 	"github.com/rocket-pool/smartnode/shared/services/wallet/keystore"
 	"github.com/tyler-smith/go-bip39"
-	eth2types "github.com/wealdtech/go-eth2-types/v2"
 	eth2ks "github.com/wealdtech/go-eth2-wallet-encryptor-keystorev4"
 )
 
-//TODO receivers that should be inaccessible while masquerading should return an error
+var ErrIsMasquerading = errors.New("The node is currently masquerading. Restore the node wallet using 'rocketpool wallet restore-address'")
 
 // masqueradeWallet
 type masqueradeWallet struct {
@@ -44,17 +41,6 @@ type masqueradeWallet struct {
 	// Node key cache
 	nodeKey     *ecdsa.PrivateKey
 	nodeKeyPath string
-
-	// Validator key caches
-	validatorKeys map[uint]*eth2types.BLSPrivateKey
-
-	// Keystores
-	keystores map[string]keystore.Keystore
-
-	// Desired gas price & limit from config
-	maxFee         *big.Int
-	maxPriorityFee *big.Int
-	gasLimit       uint64
 }
 
 // Gets the derived wallet address, if one is loaded
@@ -102,7 +88,7 @@ func (w *masqueradeWallet) GetChainID() *big.Int {
 
 // Add a keystore to the wallet
 func (w *masqueradeWallet) AddKeystore(name string, ks keystore.Keystore) {
-	w.keystores[name] = ks
+	return
 }
 
 // Check if the wallet has been initialized
@@ -120,73 +106,19 @@ func (w *masqueradeWallet) GetInitialized() (bool, error) {
 
 // Serialize the wallet to a JSON string
 func (w *masqueradeWallet) String() (string, error) {
-
-	// Check wallet is initialized
-	if !w.IsInitialized() {
-		return "", errors.New("Wallet is not initialized")
-	}
-
-	// Encode wallet store
-	wsBytes, err := json.Marshal(w.ws)
-	if err != nil {
-		return "", fmt.Errorf("Could not encode wallet: %w", err)
-	}
-
-	// Return
-	return string(wsBytes), nil
+	return "", ErrIsMasquerading
 
 }
 
 // Initialize the wallet from a random seed
 func (w *masqueradeWallet) Initialize(derivationPath string, walletIndex uint) (string, error) {
-
-	// Check wallet is not initialized
-	if w.IsInitialized() {
-		return "", errors.New("Wallet is already initialized")
-	}
-
-	// Generate mnemonic entropy
-	entropy, err := bip39.NewEntropy(EntropyBits)
-	if err != nil {
-		return "", fmt.Errorf("Could not generate wallet mnemonic entropy bytes: %w", err)
-	}
-
-	// Generate mnemonic
-	mnemonic, err := bip39.NewMnemonic(entropy)
-	if err != nil {
-		return "", fmt.Errorf("Could not generate wallet mnemonic: %w", err)
-	}
-
-	// Initialize wallet store
-	if err := w.initializeStore(derivationPath, walletIndex, mnemonic); err != nil {
-		return "", err
-	}
-
-	// Return
-	return mnemonic, nil
+	return "", ErrIsMasquerading
 
 }
 
 // Recover a wallet from a mnemonic
 func (w *masqueradeWallet) Recover(derivationPath string, walletIndex uint, mnemonic string) error {
-
-	// Check wallet is not initialized
-	if w.IsInitialized() {
-		return errors.New("Wallet is already initialized")
-	}
-
-	// Check mnemonic
-	if !bip39.IsMnemonicValid(mnemonic) {
-		return fmt.Errorf("Invalid mnemonic '%s'", mnemonic)
-	}
-
-	// Initialize wallet store
-	if err := w.initializeStore(derivationPath, walletIndex, mnemonic); err != nil {
-		return err
-	}
-
-	// Return
-	return nil
+	return ErrIsMasquerading
 
 }
 
@@ -225,90 +157,22 @@ func (w *masqueradeWallet) TestRecovery(derivationPath string, walletIndex uint,
 
 // Save the wallet store to disk
 func (w *masqueradeWallet) Save() error {
-
-	// Check wallet is initialized
-	if !w.IsInitialized() {
-		return errors.New("Wallet is not initialized")
-	}
-
-	// Encode wallet store
-	wsBytes, err := json.Marshal(w.ws)
-	if err != nil {
-		return fmt.Errorf("Could not encode wallet: %w", err)
-	}
-
-	// Write wallet store to disk
-	if err := os.WriteFile(w.walletPath, wsBytes, FileMode); err != nil {
-		return fmt.Errorf("Could not write wallet to disk: %w", err)
-	}
-
-	// Return
-	return nil
-
+	return ErrIsMasquerading
 }
 
 // Delete the wallet store from disk
 func (w *masqueradeWallet) Delete() error {
-
-	// Check if it exists
-	_, err := os.Stat(w.walletPath)
-	if os.IsNotExist(err) {
-		return nil
-	} else if err != nil {
-		return fmt.Errorf("error checking wallet file path: %w", err)
-	}
-
-	// Write wallet store to disk
-	err = os.Remove(w.walletPath)
-	return err
-
+	return ErrIsMasquerading
 }
 
 // Signs a serialized TX using the wallet's private key
 func (w *masqueradeWallet) Sign(serializedTx []byte) ([]byte, error) {
-	// Get private key
-	privateKey, _, err := w.getNodePrivateKey()
-	if err != nil {
-		return nil, err
-	}
-
-	tx := types.Transaction{}
-	err = tx.UnmarshalBinary(serializedTx)
-	if err != nil {
-		return nil, fmt.Errorf("Error unmarshalling TX: %w", err)
-	}
-
-	signer := types.NewLondonSigner(w.chainID)
-	signedTx, err := types.SignTx(&tx, signer, privateKey)
-	if err != nil {
-		return nil, fmt.Errorf("Error signing TX: %w", err)
-	}
-
-	signedData, err := signedTx.MarshalBinary()
-	if err != nil {
-		return nil, fmt.Errorf("Error marshalling signed TX to binary: %w", err)
-	}
-
-	return signedData, nil
+	return nil, ErrIsMasquerading
 }
 
 // Signs an arbitrary message using the wallet's private key
 func (w *masqueradeWallet) SignMessage(message string) ([]byte, error) {
-	// Get the wallet's private key
-	privateKey, _, err := w.getNodePrivateKey()
-	if err != nil {
-		return nil, err
-	}
-
-	messageHash := accounts.TextHash([]byte(message))
-	signedMessage, err := crypto.Sign(messageHash, privateKey)
-	if err != nil {
-		return nil, fmt.Errorf("Error signing message: %w", err)
-	}
-
-	// fix the ECDSA 'v' (see https://medium.com/mycrypto/the-magic-of-digital-signatures-on-ethereum-98fe184dc9c7#:~:text=The%20version%20number,2%E2%80%9D%20was%20introduced)
-	signedMessage[crypto.RecoveryIDOffset] += 27
-	return signedMessage, nil
+	return nil, ErrIsMasquerading
 }
 
 // Reloads wallet from disk
@@ -367,44 +231,8 @@ func (w *masqueradeWallet) loadStore() (bool, error) {
 }
 
 // Initialize the encrypted wallet store from a mnemonic
-func (w *masqueradeWallet) initializeStore(derivationPath string, walletIndex uint, mnemonic string) error {
-
-	// Generate seed
-	w.seed = bip39.NewSeed(mnemonic, "")
-
-	// Create master key
-	var err error
-	w.mk, err = hdkeychain.NewMaster(w.seed, &chaincfg.MainNetParams)
-	if err != nil {
-		return fmt.Errorf("Could not create wallet master key: %w", err)
-	}
-
-	// Get wallet password
-	password, err := w.pm.GetPassword()
-	if err != nil {
-		return fmt.Errorf("Could not get wallet password: %w", err)
-	}
-
-	// Encrypt seed
-	encryptedSeed, err := w.encryptor.Encrypt(w.seed, password)
-	if err != nil {
-		return fmt.Errorf("Could not encrypt wallet seed: %w", err)
-	}
-
-	// Create wallet store
-	w.ws = &walletStore{
-		Crypto:         encryptedSeed,
-		Name:           w.encryptor.Name(),
-		Version:        w.encryptor.Version(),
-		UUID:           uuid.New(),
-		DerivationPath: derivationPath,
-		WalletIndex:    walletIndex,
-		NextAccount:    0,
-	}
-
-	// Return
-	return nil
-
+func (w *masqueradeWallet) initializeStore(_ string, _ uint, _ string) error {
+	return ErrIsMasquerading
 }
 
 // Masquerade as another node address, running all node functions as that address (in read only mode)
