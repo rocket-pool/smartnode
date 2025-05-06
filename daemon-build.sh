05a4864a00b730f241058adbc33c0aa98560b933
Jacob Shufro <jacob@shuf.ro>
2025-05-06 14:58:02 -0400

    Add Makefile, update github actions, fix build cache

--- daemon-build.sh
+++ daemon-build.sh
@@ -3,7 +3,7 @@
 # Get the platform type and run the build script if possible
 PLATFORM=$(uname -s)
 if [ "$PLATFORM" = "Linux" ]; then
-    docker run --rm -v $PWD:/smartnode rocketpool/smartnode-builder:latest /smartnode/rocketpool/build.sh
+    docker run --rm -v $PWD:/src rocketpool/smartnode-builder:latest /smartnode/rocketpool/build.sh
 else
     echo "Platform ${PLATFORM} is not supported by this script, please build the daemon manually."
     exit 1
