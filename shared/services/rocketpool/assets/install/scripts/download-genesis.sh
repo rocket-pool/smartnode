26a6cddd0ae5c66d2d15c61d2eb0a952f8c6c403
Jacob Shufro <jacob@shuf.ro>
2025-05-15 15:09:21 -0400

    Download genesis from prysm if required

---
+++ shared/services/rocketpool/assets/install/scripts/download-genesis.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+# Grab the Testnet genesis state if needed
+if [ "$NETWORK" = "testnet" ]; then
+    echo "Prysm is configured to use Hoodi, genesis state required."
+    if [ ! -f "/ethclient/hoodi-genesis.ssz" ]; then
+        echo "Downloading from Github..."
+        wget https://github.com/eth-clients/hoodi/blob/main/metadata/genesis.ssz -O /ethclient/hoodi-genesis.ssz
+        echo "Download complete."
+    else
+        echo "Genesis state already downloaded, continuing."
+    fi
+elif [ "$NETWORK" = "devnet" ]; then
+    echo "Prysm is configured to use Hoodi, genesis state required."
+    if [ ! -f "/ethclient/hoodi-genesis.ssz" ]; then
+        echo "Downloading from Github..."
+        wget https://github.com/eth-clients/hoodi/blob/main/metadata/genesis.ssz -O /ethclient/hoodi-genesis.ssz
+        echo "Download complete."
+    else
+        echo "Genesis state already downloaded, continuing."
+    fi
+else
+    echo "Genesis download not required for $NETWORK"
+fi
