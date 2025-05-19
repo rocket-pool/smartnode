d31a28d5cdc76a05c1f088beb5c8b440583518a8
Jacob Shufro <jacob@shuf.ro>
2025-05-19 14:43:13 -0400

    Pin sszgen version

--- sszgen.sh
+++ sszgen.sh
@@ -3,4 +3,4 @@
 # Generates the ssz encoding methods for eth2 types with fastssz
 # Install sszgen with `go get github.com/ferranbt/fastssz/sszgen`
 rm -f ./shared/types/eth2/types_encoding.go
-sszgen --path ./shared/types/eth2
+go run github.com/ferranbt/fastssz/sszgen@v0.1.2 --path ./shared/types/eth2
