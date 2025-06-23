bc867f1476b7ab282d5cd02c7f815e723eaeac57
Fornax <23104993+0xfornax@users.noreply.github.com>
2025-06-23 17:46:00 -0300

    New devnet addresses

--- shared/services/config/smartnode-config.go
+++ shared/services/config/smartnode-config.go
@@ -431,7 +431,7 @@ func NewSmartnodeConfig(cfg *RocketPoolConfig) *SmartnodeConfig {
 
 		storageAddress: map[config.Network]string{
 			config.Network_Mainnet: "0x1d8f8f00cfa6758d7bE78336684788Fb0ee0Fa46",
-			config.Network_Devnet:  "0xF1ab701bDbc5e3628e97d5416aA8BCc1eB4838c1",
+			config.Network_Devnet:  "0x7d1AE15CfAf8972a4C58C3DD69819463d6e244EB",
 			config.Network_Testnet: "0x594Fb75D3dc2DFa0150Ad03F99F97817747dd4E1",
 		},
 
@@ -443,13 +443,13 @@ func NewSmartnodeConfig(cfg *RocketPoolConfig) *SmartnodeConfig {
 
 		rplTokenAddress: map[config.Network]string{
 			config.Network_Mainnet: "0xD33526068D116cE69F19A9ee46F0bd304F21A51f",
-			config.Network_Devnet:  "0x28b3C69dC1A5839C353AdF83126Ca610aa6a9321",
+			config.Network_Devnet:  "0x3A922808724555f57dd2A1FEa615543Ed28f03d9",
 			config.Network_Testnet: "0x1Cc9cF5586522c6F483E84A19c3C2B0B6d027bF0",
 		},
 
 		rethAddress: map[config.Network]string{
 			config.Network_Mainnet: "0xae78736Cd615f374D3085123A210448E74Fc6393",
-			config.Network_Devnet:  "0xb318B1aacD3c6C7B717C5e4cB5d2F07F51370Ff8",
+			config.Network_Devnet:  "0xdc7227AAC9F6a7474f8fc1Ccbf9441d70BFFa6D7",
 			config.Network_Testnet: "0x7322c24752f79c05FFD1E2a6FCB97020C1C264F1",
 		},
 
