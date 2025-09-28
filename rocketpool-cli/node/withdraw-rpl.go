4dd8c79cd326a7acbfb8be2c6d05bbdc89669e06
thomaspanf <thomaspanf@gmail.com>
2025-09-28 15:27:33 -0700

    Fix maxAmount calc in withdraw-rpl cli

--- rocketpool-cli/node/withdraw-rpl.go
+++ rocketpool-cli/node/withdraw-rpl.go
@@ -223,19 +223,23 @@ func nodeWithdrawRpl(c *cli.Context) error {
 				fmt.Println("You have no legacy RPL eligible to be unstaked.")
 				return nil
 			}
-
-			// Get maximum withdrawable amount
-			var maxAmount big.Int
-			var amountWei *big.Int
-
 			fmt.Println("Unstaking legacy RPL follows the same 2-step process as unstaking megapool staked RPL.")
 			fmt.Printf("Unstaked legacy RPL can be withdrawn after an unstaking period of %s%s%s.\n", colorYellow, status.UnstakingPeriodDuration, colorReset)
 			fmt.Println()
 
-			maxAmount.Sub(status.RplStakeLegacy, status.MaximumRplStake)
-			maxAmount.Sub(&maxAmount, status.NodeRPLLocked)
-			if maxAmount.Sign() == 1 {
+			// Get the maximum withdrawable amount based on constraints
+			// defined by decreaseNodeLegacyRPLStake in RocketNodeStaking.sol
+			var maxAmount big.Int
+			var amountWei *big.Int
+			withdrawableFromLegacy := new(big.Int).Sub(status.RplStakeLegacy, status.MaximumRplStake)
+			withdrawableFromTotal := new(big.Int).Sub(status.RplStake, status.NodeRPLLocked)
+			if withdrawableFromLegacy.Cmp(withdrawableFromTotal) < 0 {
+				maxAmount.Set(withdrawableFromLegacy)
+			} else {
+				maxAmount.Set(withdrawableFromTotal)
+			}
 
+			if maxAmount.Sign() == 1 {
 				// Print warning messages if applicable
 				notifyUnstakingRPLStatus()
 
