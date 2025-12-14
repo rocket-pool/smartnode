d2e637d3d584b11ba1774213548d9b39d3cc8328
Fornax <23104993+fornax2@users.noreply.github.com>
2025-12-14 15:23:50 -0300

    prysm-v7.1.0

--- shared/services/config/prysm-config.go
+++ shared/services/config/prysm-config.go
@@ -5,10 +5,10 @@ import (
 )
 
 const (
-	prysmBnTest             string = "gcr.io/offchainlabs/prysm/beacon-chain:v7.0.1"
-	prysmBnProd             string = "gcr.io/offchainlabs/prysm/beacon-chain:v7.0.1"
-	prysmVcTest             string = "gcr.io/offchainlabs/prysm/validator:v7.0.1"
-	prysmVcProd             string = "gcr.io/offchainlabs/prysm/validator:v7.0.1"
+	prysmBnTest             string = "gcr.io/offchainlabs/prysm/beacon-chain:v7.1.0"
+	prysmBnProd             string = "gcr.io/offchainlabs/prysm/beacon-chain:v7.1.0"
+	prysmVcTest             string = "gcr.io/offchainlabs/prysm/validator:v7.1.0"
+	prysmVcProd             string = "gcr.io/offchainlabs/prysm/validator:v7.1.0"
 	defaultPrysmRpcPort     uint16 = 5053
 	defaultPrysmOpenRpcPort string = string(config.RPC_Closed)
 	defaultPrysmMaxPeers    uint16 = 70
