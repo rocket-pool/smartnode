5a93a32399d4df7371f5645e28433f104efcb8f4
Fornax <23104993+0xfornax@users.noreply.github.com>
2025-12-23 18:33:36 -0300

    nimbus v25.12.0

--- shared/services/config/nimbus-config.go
+++ shared/services/config/nimbus-config.go
@@ -8,12 +8,12 @@ import (
 
 const (
 	// Testnet
-	nimbusBnTagTest string = "statusim/nimbus-eth2:multiarch-v25.11.1"
-	nimbusVcTagTest string = "statusim/nimbus-validator-client:multiarch-v25.11.1"
+	nimbusBnTagTest string = "statusim/nimbus-eth2:multiarch-v25.12.0"
+	nimbusVcTagTest string = "statusim/nimbus-validator-client:multiarch-v25.12.0"
 
 	// Mainnet
-	nimbusBnTagProd string = "statusim/nimbus-eth2:multiarch-v25.11.1"
-	nimbusVcTagProd string = "statusim/nimbus-validator-client:multiarch-v25.11.1"
+	nimbusBnTagProd string = "statusim/nimbus-eth2:multiarch-v25.12.0"
+	nimbusVcTagProd string = "statusim/nimbus-validator-client:multiarch-v25.12.0"
 
 	defaultNimbusMaxPeersArm uint16 = 100
 	defaultNimbusMaxPeersAmd uint16 = 160
