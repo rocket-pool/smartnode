ce4d0177c02ec4de31e26f461c7483407fbda1ee
Fornax <23104993+0xfornax@users.noreply.github.com>
2026-01-27 23:46:31 -0300

    Fix lint error

--- rocketpool-cli/node/deposit.go
+++ rocketpool-cli/node/deposit.go
@@ -57,7 +57,7 @@ func nodeDeposit(c *cli.Context) error {
 
 	fmt.Println("The minipool queue is closed in anticipation of Saturn 1 (launching Feb 18, 2026), when users will be able to create Megapools. See details here: https://saturn.rocketpool.net/")
 	return nil
-	
+
 	fmt.Println("Your eth2 client is on the correct network.")
 	fmt.Println()
 
