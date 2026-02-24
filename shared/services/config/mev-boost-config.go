434e154135ab80c532a5a70e828db266810adf43
Fornax <23104993+fornax2@users.noreply.github.com>
2026-02-24 09:39:07 -0300

    mev-boost-v1.12.0

--- shared/services/config/mev-boost-config.go
+++ shared/services/config/mev-boost-config.go
@@ -9,8 +9,8 @@ import (
 
 // Constants
 const (
-	mevBoostTagProd             string = "flashbots/mev-boost:1.11.0"
-	mevBoostTagTest             string = "flashbots/mev-boost:1.11.0"
+	mevBoostTagProd             string = "flashbots/mev-boost:1.12.0"
+	mevBoostTagTest             string = "flashbots/mev-boost:1.12.0"
 	mevDocsUrl                  string = "https://docs.rocketpool.net/node-staking/mev"
 	RegulatedRelayDescription   string = "Select this to enable the relays that comply with government regulations (e.g. OFAC sanctions), "
 	UnregulatedRelayDescription string = "Select this to enable the relays that do not follow any sanctions lists (do not censor transactions), "
