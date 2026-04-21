702857fca70ba3ea1dbe54c6ec687ccf57e52802
Jacob Shufro <jacob@shuf.ro>
2026-04-21 19:26:22 -0400

    Fix govet (unreachable wg)

--- shared/services/connectivity/check-port-connectivity.go
+++ shared/services/connectivity/check-port-connectivity.go
@@ -178,7 +178,7 @@ func getPublicIP() (string, error) {
 // isPortReachableNATReflection attempts a TCP connection to host:port and returns true if
 // the connection succeeds within portCheckTimeout.
 func isPortReachableNATReflection(host string, port uint16) bool {
-	address := fmt.Sprintf("%s:%d", host, port)
+	address := net.JoinHostPort(host, strconv.FormatUint(uint64(port), 10))
 	conn, err := net.DialTimeout("tcp", address, portCheckTimeout)
 	if err != nil {
 		return false
