0dec00dde0a2556ab8fc032142d905c1838a7b8f
Fornax <23104993+0xfornax@users.noreply.github.com>
2026-04-21 18:34:08 -0300

    enable-S1007

--- shared/utils/net/net.go
+++ shared/utils/net/net.go
@@ -7,7 +7,7 @@ import (
 
 // Add a default port to a host address
 func DefaultPort(host string, port string) string {
-	if !regexp.MustCompile(":\\d+$").MatchString(host) {
+	if !regexp.MustCompile(`:\d+$`).MatchString(host) {
 		return fmt.Sprintf("%s:%s", host, port)
 	}
 	return host
