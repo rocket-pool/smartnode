42b13bca66eb0c732f05499b1814c54f90766dc5
thomaspanf <thomaspanf@gmail.com>
2026-04-22 15:59:55 -0700

    Fix Teku crash on IPv6-only hosts

--- shared/services/rocketpool/assets/install/scripts/start-bn.sh
+++ shared/services/rocketpool/assets/install/scripts/start-bn.sh
@@ -327,7 +327,11 @@ if [ "$CC_CLIENT" = "teku" ]; then
     if [ "$ENABLE_IPV6" = "true" ]; then
         CMD="$CMD --p2p-interface=0.0.0.0,:: --p2p-port-ipv6=$BN_IPV6_P2P_PORT"
         if [ ! -z "$EXTERNAL_IP6" ]; then
-            CMD="$CMD --p2p-advertised-ips $EXTERNAL_IP,$EXTERNAL_IP6 --p2p-advertised-port-ipv6=$BN_IPV6_P2P_PORT"
+            if [ ! -z "$EXTERNAL_IP" ] && ! expr "$EXTERNAL_IP" : '.*:' >/dev/null; then
+                CMD="$CMD --p2p-advertised-ips $EXTERNAL_IP,$EXTERNAL_IP6 --p2p-advertised-port-ipv6=$BN_IPV6_P2P_PORT"
+            else
+                CMD="$CMD --p2p-advertised-ips $EXTERNAL_IP6 --p2p-advertised-port-ipv6=$BN_IPV6_P2P_PORT"
+            fi
         fi
     fi
 
