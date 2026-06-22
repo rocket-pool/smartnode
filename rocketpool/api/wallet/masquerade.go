f6eac3ea932288d9beb4a9a6232faca74f40e6b6
thomaspanf <thomaspanf@gmail.com>
2026-06-22 16:10:56 -0700

    Block oDAO members from using --observe

--- rocketpool/api/wallet/masquerade.go
+++ rocketpool/api/wallet/masquerade.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/urfave/cli/v3"
 
+	"github.com/rocket-pool/smartnode/bindings/dao/trustednode"
 	"github.com/rocket-pool/smartnode/shared/services"
 	"github.com/rocket-pool/smartnode/shared/types/api"
 )
@@ -16,6 +17,28 @@ func masquerade(c *cli.Command, address common.Address, observe bool) (*api.Masq
 	w, err := services.GetWallet(c)
 	if err != nil {
 		return nil, err
+	}
+
+	if observe {
+		hdw, err := services.GetHdWallet(c)
+		if err != nil {
+			return nil, err
+		}
+		rp, err := services.GetRocketPool(c)
+		if err != nil {
+			return nil, err
+		}
+		nodeAccount, err := hdw.GetNodeAccount()
+		if err != nil {
+			return nil, err
+		}
+		isMember, err := trustednode.GetMemberExists(rp, nodeAccount.Address, nil)
+		if err != nil {
+			return nil, fmt.Errorf("error checking Oracle DAO membership: %w", err)
+		}
+		if isMember {
+			return nil, fmt.Errorf("Observe mode is not available for Oracle DAO nodes: oDAO duties would stop running while observing")
+		}
 	}
 
 	if err := w.MasqueradeAsAddress(address, observe); err != nil {
