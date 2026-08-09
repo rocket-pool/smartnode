b66d469a7787faab51cacb845bde095815aa6750
Fornax <23104993+fornax2@users.noreply.github.com>
2026-08-09 18:30:58 -0300

    Wire the delegate-upgrade command

--- rocketpool-cli/megapool/delegate.go
+++ rocketpool-cli/megapool/delegate.go
@@ -4,10 +4,92 @@ import (
 	"fmt"
 
 	cliutils "github.com/rocket-pool/smartnode/rocketpool-cli/cli"
+	"github.com/rocket-pool/smartnode/rocketpool-cli/cli/color"
 	"github.com/rocket-pool/smartnode/rocketpool-cli/cli/prompt"
 	"github.com/rocket-pool/smartnode/shared/services/gas"
 	"github.com/rocket-pool/smartnode/shared/services/rocketpool"
 )
+
+func delegateUpgradeMegapool(yes bool) error {
+	// Get RP client
+	rp, err := rocketpool.NewClient().WithReady()
+	if err != nil {
+		return err
+	}
+	defer rp.Close()
+
+	// Get megapool status
+	status, err := rp.MegapoolStatus(false)
+	if err != nil {
+		return err
+	}
+
+	// Return if megapool isn't deployed
+	if !status.Megapool.Deployed {
+		fmt.Println("The node does not have a megapool.")
+		return nil
+	}
+
+	megapoolAddress := status.Megapool.Address
+	currentDelegate := status.Megapool.DelegateAddress
+	latestDelegate := status.LatestDelegate
+
+	// Already on the latest stored delegate
+	if currentDelegate == latestDelegate {
+		fmt.Println("The megapool is already using the latest delegate.")
+		return nil
+	}
+
+	if status.Megapool.UseLatestDelegate {
+		fmt.Println("The megapool is set to automatically use the latest delegate.")
+		fmt.Printf("Its stored delegate is still %s and can be upgraded to %s.\n",
+			color.LightBlue(currentDelegate.Hex()),
+			color.LightBlue(latestDelegate.Hex()),
+		)
+	} else {
+		fmt.Printf("The megapool is using delegate %s and can be upgraded to %s.\n",
+			color.LightBlue(currentDelegate.Hex()),
+			color.LightBlue(latestDelegate.Hex()),
+		)
+	}
+
+	// Get the gas estimate
+	canResponse, err := rp.CanDelegateUpgradeMegapool(megapoolAddress)
+	if err != nil {
+		return fmt.Errorf("error checking if megapool %s can upgrade its delegate: %w", megapoolAddress.Hex(), err)
+	}
+	if canResponse.GasLimits.IsBlank() {
+		return fmt.Errorf("could not estimate gas for megapool delegate upgrade (the upgrade may not be possible right now)")
+	}
+
+	// Assign max fees
+	err = gas.AssignMaxFeeAndLimit(canResponse.GasLimits, rp, yes)
+	if err != nil {
+		return err
+	}
+
+	// Prompt for confirmation
+	if prompt.Declined(yes, "Are you sure you want to upgrade the megapool delegate?") {
+		fmt.Println("Cancelled.")
+		return nil
+	}
+
+	// Upgrade
+	response, err := rp.DelegateUpgradeMegapool(megapoolAddress)
+	if err != nil {
+		return fmt.Errorf("could not upgrade megapool %s delegate: %w", megapoolAddress.Hex(), err)
+	}
+
+	// Log and wait for the transaction
+	fmt.Printf("Upgrading megapool %s delegate...\n", megapoolAddress.Hex())
+	cliutils.PrintTransactionHash(rp, response.TxHash)
+	if _, err = rp.WaitForTransaction(response.TxHash); err != nil {
+		return err
+	}
+
+	fmt.Printf("Successfully upgraded megapool %s to delegate %s.\n", megapoolAddress.Hex(), latestDelegate.Hex())
+	return nil
+}
 
 func setUseLatestDelegateMegapool(setting *bool, yes bool) error {
 	// Get RP client
