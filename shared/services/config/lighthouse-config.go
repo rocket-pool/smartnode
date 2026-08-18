7bfcfe58fa9bf881833ee93ddc01ec6ad7c256a0
Fornax <23104993+fornax2@users.noreply.github.com>
2026-08-17 22:16:49 -0300

    lighthouse:v8.2.2

--- shared/services/config/lighthouse-config.go
+++ shared/services/config/lighthouse-config.go
@@ -5,8 +5,8 @@ import (
 )
 
 const (
-	lighthouseTagPortableTest string = "sigp/lighthouse:v8.2.1"
-	lighthouseTagPortableProd string = "sigp/lighthouse:v8.2.1"
+	lighthouseTagPortableTest string = "sigp/lighthouse:v8.2.2"
+	lighthouseTagPortableProd string = "sigp/lighthouse:v8.2.2"
 	defaultLhMaxPeers         uint16 = 100
 )
 
